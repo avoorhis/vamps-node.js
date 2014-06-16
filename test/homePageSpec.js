ae6c07a9168653f9541933e316bdfaee65c865bb
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-16 10:34:21 -0400

    tests
    Former-commit-id: 610fe080de315c3a5485646ff4368c3c6951b6a3

--- test/homePageSpec.js
+++ test/homePageSpec.js
@@ -1,32 +1,15 @@
 var async = require('async'),
   request = require('supertest'),
   should = require('should'),
-  app = require('../app'),
-  connection = require('../database');
+  app = require('../app')
+  
 
 // 1.1 Home Page Requirements:
 // 1.2 Should contain certain text:  'Visualization'
 // 1.3 Should have links: login,Community Visualization,....
 
 describe('HomePage', function(){
-  before(function (done) {
-	this.timeout(5000);
-	async.series([
-		function (cb) {
-			connection.query('INSERT INTO mocha_test_table '+
-				'VALUE("TEST","TEST","","");',function(err){
-					done();
-				});
-		},
-		function (cb) {
-		connection.query('SELECT * FROM mocha_test_table WHERE user_name="TEST"'+
-			' AND email="TEST";',function(err,results){
-				results.length.should.not.equal(0);
-				done();
-			});
-		}
-	], done);
-  });
+  
   it('1.1 Text of home page', function(done){
 	request(app)
 	.get('/')
