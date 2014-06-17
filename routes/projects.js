40f8d479fcb99e7463a6a7072dba4b9c62d99265
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-17 15:53:56 -0400

    figuring out routing
    Former-commit-id: a039c1968e5553a4d6b61658f3bafae97d8c00e1

--- routes/projects.js
+++ routes/projects.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Userlist page. */
+// These are all under /user
+/* GET New User page. */
 router.get('/', function(req, res) {
     var db = req.db;
-    var qSelect = "SELECT * from users"
+    var qSelect = "SELECT project,title,project_description from projects"
     console.log(qSelect)
     var collection = db.query(qSelect, function (err, rows, fields){
     	if(err)	{
 			throw err;
 		}else{
-
-	    	res.render('userlist', { "rows" : rows  });
+	    	res.render('projectlist', { "rows" : rows  });
     	}
     });
     
