9849e25f956620995aa2a695e46754bf529b1c3c
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-18 16:03:45 -0400

    figuring out routing and post
    Former-commit-id: 32c4fcff06b820c57fd31195c08648812043db33 [formerly afca9eb11961746ce125d7531d81c5e80a5fd70c]
    
    Former-commit-id: d838b0b5355e89cc91ab88e0a75b5ccb4110e875

--- test/authSpec.js
+++ test/authSpec.js
@@ -7,14 +7,14 @@ var async = require('async'),
 describe('Authentication', function() {
  
   it('errors if wrong basic auth', function(done) {
-    api.get('/blog')
+    request(app).get('/')
     .set('x-api-key', '123myapikey')
     .auth('incorrect', 'credentials')
     .expect(401, done)
   });
  
   it('errors if bad x-api-key header', function(done) {
-    api.get('/blog')
+    request(app).get('/')
     .auth('correct', 'credentials')
     .expect(401)
     .expect({error:"Bad or missing app identification header"}, done);
---
+++ test/visualsSpec.js
@@ -0,0 +1,17 @@
+//visualsSpec.js
+
+// 1:Dataset selection page:
+// should be logged in (guest ok)
+// Should show project/dataset selection tree
+
+// 2: units selection page:
+// should be logged in (guest ok)
+// should show selected datasets
+// should show available counts units
+
+// 3: heatmap
+// 4: countsTable
+// 5: barCharts
+// 6: alphaDiversity
+// 7: communityDendrograms
+// more .....
