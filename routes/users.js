ffd0796e9bfff8abfbab8609382ff6bd156d8101
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-20 11:44:27 -0400

    authentication
    Former-commit-id: 857c9f159df8a6f30efd47daa65353ee940d9443

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var session   = require('express-session')
-var flash    = require('connect-flash');
+//var session   = require('express-session')
+//var flash    = require('connect-flash');
 //var LocalStrategy = require('passport-local').Strategy;
 
-// required for passport
-router.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-router.use(passport.initialize());
-router.use(passport.session()); // persistent login sessions
-router.use(flash()); // use connect-flash for flash messages stored in session
+
 
 // These are all under /user
 /* GET User List (index) page. */
@@ -22,7 +18,7 @@ router.get('/', function(req, res) {
 			throw err;
 		}else{
 
-	    	res.render('userlist', { "rows" : rows  });
+	    	res.render('user_admin/index', { "rows" : rows  });
     	}
     });
     
@@ -32,12 +28,12 @@ router.get('/', function(req, res) {
 // =====================================
 // show the login form
 router.get('/login', function(req, res) {
-    res.render('login', { message: req.flash('loginMessage') })
+    res.render('user_admin/login', { message: req.flash('loginMessage') })
 });
 
 router.post('/login',
     passport.authenticate('local-login', { successRedirect: '/',
-                                   failureRedirect: '/users/login',
+                                   failureRedirect: 'login',
                                    failureFlash: true })
 );
 
@@ -48,13 +44,12 @@ router.post('/login',
 router.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup', { message: req.flash('signupMessage') });
+        res.render('user_admin/signup', { message: req.flash('signupMessage') });
 });
 
 // process the signup form
 router.post('/signup', 
-    passport.authenticate('local-signup', {
-                                    successRedirect : 'profile', // redirect to the secure profile section
+    passport.authenticate('local-signup', { successRedirect : '/users/profile', // redirect to the secure profile section
                                     failureRedirect : 'signup', // redirect back to the signup page if there is an error
                                     failureFlash : true // allow flash messages
 }));
@@ -65,7 +60,7 @@ router.post('/signup',
 // we will want this protected so you have to be logged in to visit
 // we will use route middleware to verify this (the isLoggedIn function)
 router.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile', {
+    res.render('user_admin/profile', {
         user : req.user // get the user out of session and pass to template
     });
 });
@@ -78,8 +73,6 @@ router.get('/logout', function(req, res) {
     res.redirect('/');
 });
 
-
-
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
 
@@ -90,6 +83,7 @@ function isLoggedIn(req, res, next) {
     // if they aren't redirect them to the home page
     res.redirect('/');
 }
+
 
 
 
