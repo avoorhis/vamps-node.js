bc4918bc0387e10b75fc25baa363083b360481d4
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-20 14:30:11 -0400

    user cred

--- routes/projects.js
+++ routes/projects.js
@@ -5,30 +5,36 @@ var router = express.Router();
 /* GET New User page. */
 router.get('/', function(req, res) {
     var db = req.db;
-    var qSelect = "SELECT project,title,project_description from projects"
+    var qSelect = "SELECT id,project,title,project_description from projects"
     console.log(qSelect)
     var collection = db.query(qSelect, function (err, rows, fields){
     	if(err)	{
 			throw err;
 		}else{
-	    	res.render('projects/index', { "rows" : rows  });
+	    	res.render('projects/index', { "rows" : rows, "user": get_user(req)  });
     	}
     });
     
 });
 
-router.get('/project/:id', function(req, res) {
+router.get('/:id', function(req, res) {
     var db = req.db;
-    var qSelect = "SELECT project,title,project_description from projects where id='"+ req.params.id +"'"
+    var qSelect = "SELECT id,project,title,project_description from projects where id='"+ req.params.id +"'"
     console.log(qSelect)
     var collection = db.query(qSelect, function (err, rows, fields){
     	if(err)	{
 			throw err;
 		}else{
-	    	res.render('projects/index', { "rows" : rows  });
+	    	res.render('projects/profile', { "rows" : rows, "user": get_user(req)  });
     	}
     });
     
 });
+
+function get_user(req){
+    if(!req.user || req.user==undefined)
+        return ''
+    return req.user         
+}
 
 module.exports = router;
