52fce3d4d7430f30463cb96b5914b22c24782faa
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-22 19:11:36 -0400

    from home
    Former-commit-id: 7682669a3bd28dbfba53115fc3936ddbbc51a8f6

--- test/userLoginSpec.js
+++ test/userLoginSpec.js
@@ -1,8 +1,35 @@
+// userLoginSpec.js
+// test authentication of users
+
 var should = require("should");
 var mysql = require('mysql');
 //var Account = require("../models/account.js");
+var async = require('async'),
+  request = require('supertest'),
+  should  = require('should'),
+  app     = require('../app')
 var User = {}
 var db = require('../config/database-test');
+// https://github.com/jedwood/api-testing-with-node
+
+// describe('Authentication', function() {
+ 
+//   it('errors if wrong basic auth', function(done) {
+//     request(app).get('/')
+//     .set('x-api-key', '123myapikey')
+//     .auth('incorrect', 'credentials')
+//     .expect(401, done)
+//   });
+ 
+//   it('errors if bad x-api-key header', function(done) {
+//     request(app).get('/')
+//     .auth('correct', 'credentials')
+//     .expect(401)
+//     .expect({error:"Bad or missing app identification header"}, done);
+//   });
+
+ 
+// });
 
 describe('User Account', function() {
 
