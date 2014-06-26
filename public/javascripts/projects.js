abf279c0436f798df92bac0c14e7064814be7f8e
annaship <tetya-loshad@yandex.ru>
2014-06-26 13:58:32 -0400

    missing semicolons etc
    Former-commit-id: 6276f5ed440acb6e03b73bf97b53f6e26ce717bd

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -2,26 +2,27 @@
 
 function load_projectslist(rows)
 {
-  rows = JSON.parse(rows)
+  rows = JSON.parse(rows);
+  var html = '<ul>';
+  var i;
   
-  var html = '<ul>'
-  for(i in rows){
-    html += "<li><a href='/projects/"+rows[i].id+"'>"+rows[i].project+"</a>"
-    html += ' -- '+rows[i].title
-    html += ' '+rows[i].project_description
+  for (i in rows){
+    html += "<li><a href='/projects/"+rows[i].id+"'>"+rows[i].project+"</a>";
+    html += ' -- '+rows[i].title;
+    html += ' '+rows[i].project_description;
   }
-  html += '</ul>'
-  var div = document.getElementById('project_list_div').innerHTML = html
+  html += '</ul>';
+  var div = document.getElementById('project_list_div').innerHTML = html;
 
 }
 
 function load_project(p){
-  project = JSON.parse(p)
-  var html = '<ul> '
-  html += '<li>'+project[0].id+'</li>'
-  html += '<li>'+project[0].project+'</li>'
-  html += '<li>'+project[0].title+'</li>'
-  html += '<li>'+project[0].project_description+'</li>'
-  html += '</ul>'
-  var div = document.getElementById('project_info_div').innerHTML = html
+  var project = JSON.parse(p);
+  var html = '<ul> ';
+  html += '<li>'+project[0].id+'</li>';
+  html += '<li>'+project[0].project+'</li>';
+  html += '<li>'+project[0].title+'</li>';
+  html += '<li>'+project[0].project_description+'</li>';
+  html += '</ul>';
+  var div = document.getElementById('project_info_div').innerHTML = html;
 }
