cb00e2f3f808b9d064d8b11635939c2ecfeb3d92
annaship <tetya-loshad@yandex.ru>
2014-06-26 11:29:08 -0400

    spaces and semicolons, part1

--- config/constants.js
+++ config/constants.js
@@ -8,14 +8,14 @@ config.DOMAINS = {"domains":[ {id:1,name:"Archaea"},
                                   {id:4,name:"Unknown"}
                                 ]}
 
-config.UNITSELECT = {"units":[ 
-		{id : 'tax_silva116_simple',file:'unit_selectors/taxa_sil106_simple.html', name : "Taxonomy Silva116 (Simple)"},
-    {id : 'tax_silva116_custom',	file:'unit_selectors/taxa_sil106_cust.html', name : "Taxonomy Silva116 (Custom)"},
-    {id : 'tax_gg_simple',		file:'unit_selectors/taxa_gg_simple.html', name : "Taxonomy Greengenes (Simple)"},
-    {id : 'tax_gg_custom',		file:'unit_selectors/taxa_gg_cust.html', name : "Taxonomy Greengenes (Custom)"},
-    {id : 'tax_rdp',				  file:'unit_selectors/taxa_rdp.html', name : "Taxonomy RDP"},
-    {id : 'otus',					    file:'unit_selectors/otus.html', name : "OTUs"},
-    {id : 'med_nodes',			  file:'unit_selectors/med_nodes.html', name : "MED Nodes"}
+config.UNITSELECT = {"units":[  
+    {id : 'tax_silva116_simple',file:'unit_selectors/taxa_sil106_simple.html', name : "Taxonomy Silva116 (Simple)"},
+    {id : 'tax_silva116_custom',  file:'unit_selectors/taxa_sil106_cust.html', name : "Taxonomy Silva116 (Custom)"},
+    {id : 'tax_gg_simple',    file:'unit_selectors/taxa_gg_simple.html', name : "Taxonomy Greengenes (Simple)"},
+    {id : 'tax_gg_custom',    file:'unit_selectors/taxa_gg_cust.html', name : "Taxonomy Greengenes (Custom)"},
+    {id : 'tax_rdp',          file:'unit_selectors/taxa_rdp.html', name : "Taxonomy RDP"},
+    {id : 'otus',              file:'unit_selectors/otus.html', name : "OTUs"},
+    {id : 'med_nodes',        file:'unit_selectors/med_nodes.html', name : "MED Nodes"}
                            ]}  
 
-module.exports = config;                                
+module.exports = config;  
--- routes/visuals/visualization.js
+++ routes/visuals/visualization.js
@@ -7,16 +7,16 @@ var app = express();
 
 //router.post('/unit_selection', isLoggedIn, function(req, res) {
 router.post('/unit_selection',  function(req, res) {
-	console.log("Got response: " + res.statusCode);
-	console.log(req.body);
-	console.log(JSON.stringify(req.C));
-	// dataset selection +/- is checked in routes/visualization.js: check_for_no_datasets()
-	res.render('visuals/unit_selection', { 	title: 'Unit Selection', 
-									body: JSON.stringify(req.body),
-									constants: JSON.stringify(req.C),
-			                        "user": req.user
-								})
-	
+  console.log("Got response: " + res.statusCode);
+  console.log(req.body);
+  console.log(JSON.stringify(req.C));
+  // dataset selection +/- is checked in routes/visualization.js: check_for_no_datasets()
+  res.render('visuals/unit_selection', {   title: 'Unit Selection',  
+                  body: JSON.stringify(req.body),
+                  constants: JSON.stringify(req.C),
+                              "user": req.user
+                })
+  
 });
 /* GET visualization page. */
 router.get('/',  function(req, res) {
@@ -30,67 +30,67 @@ router.get('/',  function(req, res) {
 //         {"id":6,"dname":"1St_114_Hardinsburg"},
 //         {"id":19,"dname":"1St_127_Pendleton"}
 //     ]}
-// }    
+// }  
     var db = req.db;
     var qDatasets = "SELECT project, projects.id as pid, dataset, datasets.id as did"
     qDatasets    += " FROM datasets"
     qDatasets    += " JOIN projects ON (projects.id=project_id)"
     var collection = db.query(qDatasets, function (err, rows, fields){
-    	if(err)	{
-			throw err;
-		}else{
-			var datasetsByProjectAll = {};
-			var projects = []
-			var datasets_list = []
-			var already_have_project
-			datasetsByProjectAll.projects = projects
-			for(n=0;n<rows.length;n++){
-				//console.log(rows[n].dataset)
-				//console.log(rows[n].project)
-				pname   = rows[n].project
-				pid     = rows[n].pid
-				did     = rows[n].did
-				dname   = rows[n].dataset
-				dataset = {
-				    "did"   : did,
-				    "dname" : dname
-				    }
-				project = {
-				    "pid"   : pid,
-				    "pname" : pname
-				    }
-				 
-				already_have_project = false
-				for (var i=0; i<datasetsByProjectAll.projects.length; i++) {
-				    if (datasetsByProjectAll.projects[i].pid == pid) {
-				        // here we add our dataset to datasetsByProjectAll.projects[i].datasets.push(dataset)
-				        datasetsByProjectAll.projects[i].datasets.push(dataset)
-				        already_have_project = true
-				    }
-				}
-				if(!already_have_project){
-				    // add this dataset to it -- first one	
-				    project.datasets = [dataset]
-				    datasetsByProjectAll.projects.push(project)
-				}	
-			}
-			
-			
-			
-			                                
-
-			//console.log(JSON.stringify(datasetsByProjectAll));                                                             
-			res.render('visuals/index',{ title   : 'Show Datasets!', 
-			                             rows    : JSON.stringify(datasetsByProjectAll)	,
-			                             "user": req.user		                                
-			                            	})
-		}
-		
-    });  
-    
+      if(err)  {
+      throw err;
+    }else{
+      var datasetsByProjectAll = {};
+      var projects = []
+      var datasets_list = []
+      var already_have_project
+      datasetsByProjectAll.projects = projects
+      for(n=0;n<rows.length;n++){
+        //console.log(rows[n].dataset)
+        //console.log(rows[n].project)
+        pname   = rows[n].project
+        pid     = rows[n].pid
+        did     = rows[n].did
+        dname   = rows[n].dataset
+        dataset = {
+            "did"   : did,
+            "dname" : dname
+            }
+        project = {
+            "pid"   : pid,
+            "pname" : pname
+            }
   
-   
-    
+        already_have_project = false
+        for (var i=0; i<datasetsByProjectAll.projects.length; i++) {
+            if (datasetsByProjectAll.projects[i].pid == pid) {
+                // here we add our dataset to datasetsByProjectAll.projects[i].datasets.push(dataset)
+                datasetsByProjectAll.projects[i].datasets.push(dataset)
+                already_have_project = true
+            }
+        }
+        if(!already_have_project){
+            // add this dataset to it -- first one  
+            project.datasets = [dataset]
+            datasetsByProjectAll.projects.push(project)
+        }  
+      }
+  
+  
+  
+  
+
+      //console.log(JSON.stringify(datasetsByProjectAll));  
+      res.render('visuals/index',{ title   : 'Show Datasets!',  
+                                   rows    : JSON.stringify(datasetsByProjectAll)  ,
+                                   "user": req.user  
+                                    })
+    }
+  
+    });  
+  
+  
+  
+  
 });
 
 
@@ -98,7 +98,7 @@ module.exports = router;
 
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on 
+    // if user is authenticated in the session, carry on  
     if (req.isAuthenticated())
         return next();
 
