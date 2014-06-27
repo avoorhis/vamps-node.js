06f48c4e5d29d09df913efd842fa85fc0ab19e05
Andrew Voorhis <avoorhis@mbl.edu>
2014-06-27 13:17:28 -0400

    dataset_accumulator in public/classes
    Former-commit-id: a83a83f811b340b4284c78001fad38e13fc8a17e

---
+++ public/classes/project_dataset.js
@@ -0,0 +1,19 @@
+// project_dataset.js 
+
+var express = require('express');
+var router = express.Router();
+
+// don't think this will work as you are unable to
+// do multiple instantiations.
+var DATASET = {
+	var id,
+	var full_name,
+	var pname,
+	var dname,
+	var seq_count,
+	var env_sample_source_id,
+	var seq_ids: [],
+
+
+}
+module.exports = DATASET;
