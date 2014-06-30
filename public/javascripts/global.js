e79844481f51e544e610b4006bbe66e72440e04f
annaship <tetya-loshad@yandex.ru>
2014-06-30 14:03:16 -0400

    include menu into home page

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -2,24 +2,24 @@
 
 
 function initialize(user){
-  var html = '';
-  html += "<div class='main_menu'>";
-  html += "  <ul>";
-  html += "    <li><a href='/'>VAMPS Home</a></li>";
-  html += "    <li><a href='/helloworld'>Hello! Page</a></li>";
-  html += "    <li><a href='/visuals'>Community Visualization</a></li>";
-  html += "    <li><a href='/projects'>Project List</a></li>";
-  html += "    <li><a href='/users'>User List</a></li>";
-  html += "    <li><a href='/users/profile'>User Account</a></li>";
-  html += "    <li><a href='#'>Upload Data</a></li>";
-  html += "    <li><a href='#'>Export Data</a></li>";
-  html += "    <li><a href='#'>Search</a></li>";
-  html += "    <li><a href='#'>Distribution</a></li>";
-  html += "   <li><a href='#'>Metadata</a></li>";
-  html += "    <li><a href='#'>Portals</a></li>";
-  html += "  </ul>";
-  html += "</div>";
-  document.getElementById('main_menu').innerHTML = html;
+  // var html = '';
+  // html += "<div class='main_menu'>";
+  // html += "  <ul>";
+  // html += "    <li><a href='/'>VAMPS Home</a></li>";
+  // html += "    <li><a href='/helloworld'>Hello! Page</a></li>";
+  // html += "    <li><a href='/visuals'>Community Visualization</a></li>";
+  // html += "    <li><a href='/projects'>Project List</a></li>";
+  // html += "    <li><a href='/users'>User List</a></li>";
+  // html += "    <li><a href='/users/profile'>User Account</a></li>";
+  // html += "    <li><a href='#'>Upload Data</a></li>";
+  // html += "    <li><a href='#'>Export Data</a></li>";
+  // html += "    <li><a href='#'>Search</a></li>";
+  // html += "    <li><a href='#'>Distribution</a></li>";
+  // html += "   <li><a href='#'>Metadata</a></li>";
+  // html += "    <li><a href='#'>Portals</a></li>";
+  // html += "  </ul>";
+  // html += "</div>";
+  // document.getElementById('main_menu').innerHTML = html;
 
   // login report
   html = '';
