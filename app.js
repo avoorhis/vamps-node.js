a3b6cce4238331a268d17060458621299c06d36e
annaship <tetya-loshad@yandex.ru>
2014-06-30 13:42:26 -0400

    spaces
    Former-commit-id: fa172f7bde75f5726ce5d52aee055e88334386f9

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var session      = require('express-session');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 var db = require('mysql');
 var connection = require('./config/database-dev');
 connection.connect();
@@ -17,11 +17,12 @@ var datasets = require('./routes/datasets');
 var visuals = require('./routes/visuals/visualization');
 var C = require('./public/constants');
 var app = express();
-require('./config/passport')(passport,connection); // pass passport for configuration
+require('./config/passport')(passport, connection); // pass passport for configuration
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
+
 app.set('view engine', 'html');
 //app.set('view engine', 'jade');
 
@@ -39,7 +40,7 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
+app.use(function(req, res, next){
     req.db = connection;
     req.C = C;
     next();
@@ -50,9 +51,6 @@ app.use('/users', users);
 app.use('/projects', projects);
 app.use('/datasets', datasets);
 app.use('/visuals', visuals);
-
-
-
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -70,7 +68,7 @@ if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error.ejs', {
-            message: err.message,
+            message: err.message, 
             error: err
         });
     });
@@ -83,7 +81,7 @@ if (app.get('env') === 'production') {
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error.ejs', {
-        message: err.message,
+        message: err.message, 
         error: {}
     });
 });
