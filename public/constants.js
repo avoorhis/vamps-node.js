57b62968fae86fe457106a8eadaf3874b461c599
Andrew Voorhis <avoorhis@mbl.edu>
2014-07-02 12:18:11 -0400

    added visuals/partials for unit selection
    Former-commit-id: f3457ae0370fb6226df013fa60ba9841699e890a [formerly 153d0ccc8904cb2548dd7f7855a61614c8e16865]
    
    Former-commit-id: 2c1fbed5f202f7e78d0b8d0b1ab088cb84a43d03

--- public/constants.js
+++ public/constants.js
@@ -9,8 +9,8 @@ config.DOMAINS = {"domains":[ {id:1,name:"Archaea"},
                                 ]}
 
 config.UNITSELECT = {"units":[ 
-		{id : 'tax_silva108_simple',file:'unit_selectors/taxa_sil108_simple.html', name : "Taxonomy Silva108 (Simple)"},
-    {id : 'tax_silva108_custom',file:'unit_selectors/taxa_sil108_cust.html', name : "Taxonomy Silva108 (Custom)"},
+		{id : 'tax_silva108_simple',file:'unit_selectors/taxa_silva108_simple.html', name : "Taxonomy Silva108 (Simple)"},
+    {id : 'tax_silva108_custom',file:'unit_selectors/taxa_silva108_cust.html', name : "Taxonomy Silva108 (Custom)"},
     {id : 'tax_gg_simple',		  file:'unit_selectors/taxa_gg_simple.html', name : "Taxonomy Greengenes (Simple)"},
     {id : 'tax_gg_custom',		  file:'unit_selectors/taxa_gg_cust.html', name : "Taxonomy Greengenes (Custom)"},
     {id : 'tax_rdp',				    file:'unit_selectors/taxa_rdp.html', name : "Taxonomy RDP"},
