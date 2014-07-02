ad1cdb23f7d1ea977c160d30d3d94b9d5e3599d5
annaship <tetya-loshad@yandex.ru>
2014-07-02 16:52:13 -0400

    root suite, intermediate
    Former-commit-id: a7440fb8da8af3c59d5e23e16c0d6fcc91f0c640

--- test/insert_user_into_db.js
+++ test/insert_user_into_db.js
@@ -1,72 +1,68 @@
-var app = require('../app'),
-    connection = require('../config/database-test');
+before(function() {
+	var async = require('async'),
+	    request = require('supertest'),
+	    should = require('should'),
+	    app = require('../app'),
+	    connection = require('../config/database-test');
 
-		// describe('Init', function() {
-		//   		before(function (done) {
+	connection.query("CREATE TABLE IF NOT EXISTS users (\
+	  id int(11) NOT NULL AUTO_INCREMENT,\
+	  username varchar(20) DEFAULT NULL,\
+	  email varchar(64) NOT NULL DEFAULT '',\
+	  institution varchar(128) DEFAULT NULL,\
+	  first_name varchar(20) DEFAULT NULL,\
+	  last_name varchar(20) DEFAULT NULL,\
+	  active tinyint(3) unsigned NOT NULL DEFAULT '0',\
+	  security_level tinyint(3) unsigned NOT NULL DEFAULT '50',\
+	  encrypted_password varchar(255) NOT NULL DEFAULT '',\
+	  reset_password_token varchar(255) DEFAULT NULL,\
+	  reset_password_sent_at datetime DEFAULT NULL,\
+	  remember_created_at datetime DEFAULT NULL,\
+	  sign_in_count int(11) DEFAULT '0',\
+	  current_sign_in_at datetime DEFAULT NULL,\
+	  last_sign_in_at datetime DEFAULT NULL,\
+	  current_sign_in_ip varchar(255) DEFAULT NULL,\
+	  last_sign_in_ip varchar(255) DEFAULT NULL,\
+	  confirmation_token varchar(255) DEFAULT NULL,\
+	  confirmed_at datetime DEFAULT NULL,\
+	  confirmation_sent_at datetime DEFAULT NULL,\
+	  unconfirmed_email varchar(255) DEFAULT NULL,\
+	  PRIMARY KEY (id),\
+	  UNIQUE KEY contact_email_inst (first_name,last_name,email,institution),\
+	  UNIQUE KEY username (username),\
+	  UNIQUE KEY index_users_on_reset_password_token (reset_password_token),\
+	  UNIQUE KEY index_users_on_confirmation_token (confirmation_token),\
+	  KEY institution (institution(15))\
+	) ENGINE=InnoDB;", function(err, rows, fields) {
+	  if (err) throw err;
+	});
 
-	    // this.timeout(5000);
+	var query = connection.query('INSERT IGNORE INTO users (username, encrypted_password, first_name, last_name, email, institution) VALUES ("TEST","7kT94LYj7y5RnJVb34jrJw==","TEST","TEST","TEST","TEST")', function(err, result) {
+	  if (err) throw err;
 
-			connection.query("CREATE TABLE IF NOT EXISTS users (\
-			  id int(11) NOT NULL AUTO_INCREMENT,\
-			  username varchar(20) DEFAULT NULL,\
-			  email varchar(64) NOT NULL DEFAULT '',\
-			  institution varchar(128) DEFAULT NULL,\
-			  first_name varchar(20) DEFAULT NULL,\
-			  last_name varchar(20) DEFAULT NULL,\
-			  active tinyint(3) unsigned NOT NULL DEFAULT '0',\
-			  security_level tinyint(3) unsigned NOT NULL DEFAULT '50',\
-			  encrypted_password varchar(255) NOT NULL DEFAULT '',\
-			  reset_password_token varchar(255) DEFAULT NULL,\
-			  reset_password_sent_at datetime DEFAULT NULL,\
-			  remember_created_at datetime DEFAULT NULL,\
-			  sign_in_count int(11) DEFAULT '0',\
-			  current_sign_in_at datetime DEFAULT NULL,\
-			  last_sign_in_at datetime DEFAULT NULL,\
-			  current_sign_in_ip varchar(255) DEFAULT NULL,\
-			  last_sign_in_ip varchar(255) DEFAULT NULL,\
-			  confirmation_token varchar(255) DEFAULT NULL,\
-			  confirmed_at datetime DEFAULT NULL,\
-			  confirmation_sent_at datetime DEFAULT NULL,\
-			  unconfirmed_email varchar(255) DEFAULT NULL,\
-			  PRIMARY KEY (id),\
-			  UNIQUE KEY contact_email_inst (first_name,last_name,email,institution),\
-			  UNIQUE KEY username (username),\
-			  UNIQUE KEY index_users_on_reset_password_token (reset_password_token),\
-			  UNIQUE KEY index_users_on_confirmation_token (confirmation_token),\
-			  KEY institution (institution(15))\
-			) ENGINE=InnoDB;", function(err, rows, fields) {
-			  if (err) throw err;
-			});
+	  // console.log(query.sql); 
 
-			var query = connection.query('INSERT IGNORE INTO users (username, encrypted_password, first_name, last_name, email, institution) VALUES ("TEST","7kT94LYj7y5RnJVb34jrJw==","TEST","TEST","TEST","TEST")', function(err, result) {
-			  if (err) throw err;
+		console.log("result.insertId: " + result.insertId);
+	  console.log("=========");
+	});
 
-			  // console.log(query.sql); 
+	connection.query('SELECT * FROM users WHERE username="TEST"'+
+	      ' AND email="TEST"', function(err, rows, fields) {
+	  if (err) throw err;
+		// console.log("fields");
+		// console.log(fields[0]);
 
-				console.log("result.insertId: " + result.insertId);
-			  console.log("=========");
-			});
+	  rows[0].username.should.equal('TEST');
+		rows[0].email.should.equal('TEST');
+		rows[0].institution.should.equal('TEST');
+		rows[0].first_name.should.equal('TEST');
+		rows[0].last_name.should.equal('TEST');
+		rows[0].active.should.equal(0);
+		rows[0].security_level.should.equal(50);
+		rows[0].encrypted_password.should.equal('7kT94LYj7y5RnJVb34jrJw==');
 
-			connection.query('SELECT * FROM users WHERE username="TEST"'+
-	        ' AND email="TEST"', function(err, rows, fields) {
-			  if (err) throw err;
-				// console.log("fields");
-				// console.log(fields[0]);
+	  fields[0].table.should.equal('users');
 
-			  rows[0].username.should.equal('TEST');
-				rows[0].email.should.equal('TEST');
-				rows[0].institution.should.equal('TEST');
-				rows[0].first_name.should.equal('TEST');
-				rows[0].last_name.should.equal('TEST');
-				rows[0].active.should.equal(0);
-				rows[0].security_level.should.equal(50);
-				rows[0].encrypted_password.should.equal('7kT94LYj7y5RnJVb34jrJw==');
-
-			  fields[0].table.should.equal('users');
-
-				done();
-			});
-		// });
-	// });		
-module.export	= insert_user_into_db
-	
+	});
+  console.log('before every test')
+});
