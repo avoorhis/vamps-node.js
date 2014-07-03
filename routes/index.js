585377ba3e61acea6cb6fb7234035f4478ea706a
Andrew Voorhis <avoorhis@mbl.edu>
2014-07-03 16:14:00 -0400

    working to get datasets global
    Former-commit-id: 67c7f2fdd994aa711565030421fd31e5100c9000

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'VAMPS-Node.js', user: req.user });
-});
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' });
+// GLOBAL VARIABLES HERE
+//if(DATASETS==undefined){
+// if DATASETS exists don't go here again!  
+DATASETS={}
+ds = require('./load_all_datasets')
+rs = ds.get_datasets(function(DATASETS){
+	
+	/* GET home page. */
+	router.get('/', function(req, res) {
+	  res.render('index', { title: 'VAMPS-Node.js', user: req.user });
+	});
+
+	/* GET Hello World page. */
+	router.get('/helloworld', function(req, res) {
+	    res.render('helloworld', { title: 'Hello, World!' });
+	});
 });
 
 module.exports = router;
