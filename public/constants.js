92c526bc4749503e4fdecf4bffa0fb6f6871ce9b
Andrew Voorhis <avoorhis@mbl.edu>
2014-07-09 11:16:53 -0400

    documenting visualization.js
    Former-commit-id: a25472c4d0504c7c03a80cb9dd740200441f0a4d

--- public/constants.js
+++ public/constants.js
@@ -11,26 +11,26 @@ config.DOMAINS = { domains: [
 
 config.UNITSELECT = { units: [ 
     		{id : 'tax_silva108_simple',file: 'unit_selectors/taxa_silva108_simple.html', name : "Taxonomy Silva108 (Simple)"},
-        {id : 'tax_silva108_custom',file: 'unit_selectors/taxa_silva108_cust.html', name : "Taxonomy Silva108 (Custom)"},
-        {id : 'tax_gg_simple',		  file: 'unit_selectors/taxa_gg_simple.html', name : "Taxonomy Greengenes (Simple)"},
-        {id : 'tax_gg_custom',		  file: 'unit_selectors/taxa_gg_cust.html', name : "Taxonomy Greengenes (Custom)"},
-        {id : 'tax_rdp',				    file: 'unit_selectors/taxa_rdp.html', name : "Taxonomy RDP"},
-        {id : 'otus',					      file: 'unit_selectors/otus.html', name : "OTUs"},
-        {id : 'med_nodes',			    file: 'unit_selectors/med_nodes.html', name : "MED Nodes"}
+        {id : 'tax_silva108_custom',file: 'unit_selectors/taxa_silva108_cust.html',   name : "Taxonomy Silva108 (Custom)"},
+        {id : 'tax_gg_simple',		  file: 'unit_selectors/taxa_gg_simple.html',       name : "Taxonomy Greengenes (Simple)"},
+        {id : 'tax_gg_custom',		  file: 'unit_selectors/taxa_gg_cust.html',         name : "Taxonomy Greengenes (Custom)"},
+        {id : 'tax_rdp',				    file: 'unit_selectors/taxa_rdp.html',             name : "Taxonomy RDP"},
+        {id : 'otus',					      file: 'unit_selectors/otus.html',                 name : "OTUs"},
+        {id : 'med_nodes',			    file: 'unit_selectors/med_nodes.html',             name : "MED Nodes"}
     ]};
 
 config.VISUALOUTPUTCHOICES = { visulachoices: [
         {id : 'heatmap',        show: 'Heatmap'},
         {id : 'counts_table',   show: 'Counts Table'},
-        {id : 'barcharts',      show: 'Bar_Charts'},
+        {id : 'barcharts',      show: 'Bar Charts'},
         {id : 'dendrogram',     show: 'Dendrogram'},
-        {id : 'alphadiversity', show: 'Alpha_Diversity'}
+        {id : 'alphadiversity', show: 'Alpha Diversity'}
     ]};;
 
 config.NORMALIZATIONCHOICES = { normchoices: [
-        {id: 'none',  show: 'Not Normalized (default)'},
-        {id: 'max',   show: 'Normalized to the Maximum Sample'},
-        {id: 'freq',  show: 'Normalized to Frequency'}
+        {id: 'none',            show: 'Not Normalized (default)'},
+        {id: 'max',             show: 'Normalized to the Maximum Sample'},
+        {id: 'freq',            show: 'Normalized to Frequency'}
     ]};
 
 
