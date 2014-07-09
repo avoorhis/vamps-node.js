849de9f66c827ff5ca4b670a4a24c40c5dbda525
annaship <tetya-loshad@yandex.ru>
2014-07-09 12:33:26 -0400

    make static project list an html, not ajax

--- public/javascripts/projects.js
+++ public/javascripts/projects.js
@@ -1,7 +1,21 @@
 // projects.js
+// onload="load_projectslist('<%= JSON.stringify(rows) %>"
+// document.addEventListener( "DOMContentLoaded", load_projectslist(JSON.stringify(all_projects)), false );
+document.addEventListener( "DOMContentLoaded", call_me(all_projects), false );
+
+function call_me()
+{
+	var fooJSON = JSON.stringify(all_projects);
+	
+	alert("URA");
+	alert(fooJSON);
+	// get it as json from an ajax-call??? http://stackoverflow.com/questions/23031161/get-variable-on-client-side-from-the-server-side-express-js-node-js
+}
 
 function load_projectslist(rows)
 {
+	alert("URA");
+	alert(all_projects);
   rows = JSON.parse(rows);
   var html = '<ul>';
   var i;
@@ -25,4 +39,9 @@ function load_project(p){
   html += '<li>'+project[0].project_description+'</li>';
   html += '</ul>';
   var div = document.getElementById('project_info_div').innerHTML = html;
+}
+
+if (typeof exports !== 'undefined') {
+   exports.load_projectslist = load_projectslist
+   exports.load_project = load_project
 }
