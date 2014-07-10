895fa53796e5bf6a515157a9fb861d479429f694
Andrew Voorhis <avoorhis@mbl.edu>
2014-07-10 15:59:23 -0400

    working on custom taxonomy
    Former-commit-id: fdc89d709fc243c06b0bde9590e4ee7579d8c152 [formerly ecda228218a9afdfbc79e28c683dbbd1ee1ff779]
    
    Former-commit-id: d17826d593f26acc9ba21c990285fc437f69e397

---
+++ public/javascripts/taxonomy_tree.js
@@ -0,0 +1,88 @@
+// taxonomy_tree.js
+
+function toggle_selected_taxa(level,name) {
+  
+  var tax_div = document.getElementById(name+'_div');
+  var cbs = tax_div.getElementsByTagName('input');
+  var toggle = document.getElementById(name+'_toggle');
+  var i;
+  
+  if (tax_div.style.display === 'inline'){
+    tax_div.style.display = 'none';
+    document.getElementById(name+'--cbid').checked = false;
+    // change image to plus.png
+    toggle.innerHTML = "<img alt='plus' src='/images/tree_plus.gif' height='9' />";
+    for (i=0; i < cbs.length; i++) {
+      if (cbs[i].type === 'checkbox') {
+        cbs[i].checked = false;
+      }
+    }
+  } else {
+    tax_div.style.display = 'inline';
+    document.getElementById(name+'--cbid').checked = true;
+    toggle.innerHTML = "<img alt='minus' src='/images/tree_minus.gif' height='9' />";
+    for (i=0; i < cbs.length; i++) {
+      if (cbs[i].type === 'checkbox') {
+        cbs[i].checked = true  ;
+      }
+    }
+  }
+}
+
+function open_taxa(name) {
+
+  // Called from index_visuals: project--dataset tree
+
+  tax_div = document.getElementById(name+'_div');
+  cbs = tax_div.getElementsByTagName('input');
+  toggle = document.getElementById(name+'_toggle');
+  if(tax_div.style.display == 'inline'){
+
+
+    // uncheck project
+    if (cbs[0].checked) {
+      document.getElementById(name+'--cbid').checked = false;
+      for (i=0; i < cbs.length; i++) {
+        cbs[i].checked = false;
+      }
+    } else {
+      document.getElementById(name+'--cbid').checked = true;
+      for (i=0; i < cbs.length; i++) {
+        cbs[i].checked = true;
+      }
+    }
+
+  } else {
+
+    // check project
+    tax_div.style.display = 'inline';
+    document.getElementById(name+'--cbid').checked = true;
+    toggle.innerHTML = "<img alt='minus' src='/images/tree_minus.gif' height='9' />";
+    // now set all the ds checkboxes to 'checked'
+    for (i=0; i < cbs.length; i++) {
+      if (cbs[i].type === 'checkbox') {
+        cbs[i].checked = true;
+      }
+    }
+  }
+}
+
+function open_level(id, names,level) {
+	var x = JSON.parse(names);
+	alert(id)
+	for(i in x){
+		//alert(i)
+		var new_id = id+';'+i;
+		open_taxa(new_id);
+	}
+	
+	
+
+}
+
+
+
+
+
+
+
