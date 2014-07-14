04b8c015fe1d647357355a9d6fb68de9cd832cfb
annaship <tetya-loshad@yandex.ru>
2014-07-14 14:40:43 -0400

    js style issues, test green
    Former-commit-id: f54a24f658eca60e4c6eb1245c6b836798d1981a [formerly 415e4eb13f112eef3a5238cf07f40021401e5839]
    Former-commit-id: 8c365cd89a4833ddbabb19c81d13636359a7a602

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 
-ds = require('./load_all_datasets')
-rs = ds.get_datasets(function(ALL_DATASETS){
-	GLOBAL.ALL_DATASETS = ALL_DATASETS
-	
-	/* GET home page. */
-	router.get('/', function(req, res) {
-	  res.render('index', { title: 'VAMPS-Node.js', user: req.user });
-	});
+var ds = require('./load_all_datasets');
+var rs = ds.get_datasets(function(ALL_DATASETS){
+  GLOBAL.ALL_DATASETS = ALL_DATASETS;
 
-	/* GET Hello World page. */
-	router.get('/helloworld', function(req, res) {
-	    res.render('helloworld', { title: 'Hello, World!' });
-	});
+  /* GET home page. */
+  router.get('/', function(req, res) {
+    res.render('index', { title: 'VAMPS-Node.js', user: req.user });
+  });
+
+  /* GET Hello World page. */
+  router.get('/helloworld', function(req, res) {
+      res.render('helloworld', { title: 'Hello, World!' });
+  });
 });
 
 module.exports = router;
