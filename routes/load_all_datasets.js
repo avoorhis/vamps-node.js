26d4242b0dbc2acc71c83938e8cf7350015e5661
annaship <tetya-loshad@yandex.ru>
2014-07-14 14:40:43 -0400

    js style issues, test green
    Former-commit-id: f54a24f658eca60e4c6eb1245c6b836798d1981a [formerly 415e4eb13f112eef3a5238cf07f40021401e5839]
    
    Former-commit-id: 7dff58c7edd01bb229082cb559604e5377b5f2fb

--- routes/load_all_datasets.js
+++ routes/load_all_datasets.js
@@ -4,52 +4,55 @@ var router = express.Router();
 //
 //
 //
-var qSelectDatasets = "SELECT project, datasets.id as did, dataset"
-qSelectDatasets += " FROM datasets"
-qSelectDatasets += " JOIN projects ON (projects.id=project_id)"
-qSelectDatasets += " ORDER BY project,dataset"
+var qSelectDatasets = "SELECT project, datasets.id as did, dataset";
+qSelectDatasets += " FROM datasets";
+qSelectDatasets += " JOIN projects ON (projects.id=project_id)";
+qSelectDatasets += " ORDER BY project, dataset";
 
 // This connection object is made global in app.js
 module.exports.get_datasets = function(callback){
-	connection.query(qSelectDatasets, function(err, rows, fields){
-	  	if(err)	{
-	  		throw err;
-		  }else{
-				var datasetsByProject = {}	
-				var ALL_DATASETS = {}
-	  		ALL_DATASETS.projects = []
-				//datasetsByProject.projects = []
-				console.log('GETTING ALL DATASETS FROM DB-3')
-	  		for(i in rows){
-	  			project = rows[i].project
-	  			did = rows[i].did
-	  			dataset = rows[i].dataset
-	  			if(project===undefined){ continue }
-	        if(project in datasetsByProject){
-	            datasetsByProject[project].push({ did:did,dname:dataset })
-	        }else{
-	            datasetsByProject[project] = [{ did:did, dname:dataset }]
-	        }
-	  		}
-	  		
-	  		for(p in datasetsByProject){
-	  			tmp = {}
-	  			tmp.name = p
-	  			tmp.datasets = []
-	  			for(d in datasetsByProject[p]){
-	  				ds = datasetsByProject[p][d].dname
-	  				did = datasetsByProject[p][d].did
-	  				tmp.datasets.push({ did:did, dname:ds })
-	  			}
-	  			ALL_DATASETS.projects.push(tmp)
-	  		}
-	  		
-	  	}
-	  	callback(ALL_DATASETS);
-	});
+  connection.query(qSelectDatasets, function(err, rows, fields){
+    var ALL_DATASETS = {};
+
+      if (err)  {
+        throw err;
+      } else {
+        var datasetsByProject = {};
+        ALL_DATASETS.projects = [];
+        //datasetsByProject.projects = []
+        console.log('GETTING ALL DATASETS FROM DB-3');
+
+        for (var i=0; i < rows.length; i++) {
+          var project = rows[i].project;
+          var did = rows[i].did;
+          var dataset = rows[i].dataset;
+          if (project === undefined){ continue; }
+          if (project in datasetsByProject){
+              datasetsByProject[project].push({ did:did, dname:dataset });
+          } else {
+              datasetsByProject[project] = [{ did:did, dname:dataset }];
+          }
+        }
+
+				// todo: console.log(datasetsByProject.length); datasetsByProject - not an array
+        for (var p in datasetsByProject){
+          var tmp = {};
+          tmp.name = p;
+          tmp.datasets = [];
+          for (var d in datasetsByProject[p]){
+            var ds = datasetsByProject[p][d].dname;
+            var dp_did = datasetsByProject[p][d].did;
+            tmp.datasets.push({ did: dp_did, dname:ds });
+          }
+          ALL_DATASETS.projects.push(tmp);
+        }
+
+      }
+      callback(ALL_DATASETS);
+  });
 };
 
-// { projects: 
+// { projects:
 //    [ { name: 'SLM_NIH_Bv6', datasets: [Object] },
 //      { name: 'SLM_NIH_v1', datasets: [Object] },
 //      { name: 'SLM_NIH_v2', datasets: [Object] },
@@ -57,25 +60,25 @@ module.exports.get_datasets = function(callback){
 //      { name: 'SLM_NIH_Bv4v5', datasets: [Object] } ] }
 
 // connection.query(qSelectDatasets, function(err, rows, fields){
-//   	if(err)	{
-//   		throw err;
-//   	}else{
-  		
-//   		for(i in rows){
-//   			project = rows[i].project
-//   			did = rows[i].did
-//   			dataset = rows[i].dataset
-//   			if(project===undefined){ continue }
-//         if(project in DATASETS){
+//     if (err)  {
+//       throw err;
+//     } else {
+
+//       for (i in rows){
+//         project = rows[i].project
+//         did = rows[i].did
+//         dataset = rows[i].dataset
+//         if (project===undefined){ continue }
+//         if (project in DATASETS){
 //             global.DATASETS[project].push({ 'did':did,'dname':dataset })
-//         }else{
+//         } else {
 //             global.DATASETS[project] = [{ 'did':did,'dname':dataset }]
 //         }
-//   		}
-  		
-  		
-//   	}
-//   	module.exports = global.DATASETS;
+//       }
+
+
+//     }
+//     module.exports = global.DATASETS;
 // });
 
 
