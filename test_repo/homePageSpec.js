42c0f35d2f98993605e45f3ed5d6267ee1704ca1
annaship <tetya-loshad@yandex.ru>
2014-07-14 14:22:37 -0400

    trailing space, space vs. \t, space around else

--- test_repo/homePageSpec.js
+++ test_repo/homePageSpec.js
@@ -5,14 +5,14 @@ var async = require('async'),
   request = require('supertest'),
   should  = require('should'),
   app     = require('../app');
-  
+
 
 // 1.1 Home Page Requirements:
 // 1.2 Should contain certain text:  'Visualization'
 // 1.3 Should have links: login,Community Visualization,....
 
 describe('HomePage', function(){
-  
+
   it('1.1 Text of home page', function(done){
   request(app)
   .get('/')
