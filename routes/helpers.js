7406cb0fd4d5216db26be3243a038fc9dab92f59
annaship <tetya-loshad@yandex.ru>
2014-07-16 17:59:06 -0400

    tabs vs. spaces, semicolons etc
    Former-commit-id: 2e7080c257ea0aa8376e90a6b7dc34d66048c123

--- routes/helpers.js
+++ routes/helpers.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 module.exports = {
 
-	// route middleware to make sure a user is logged in
-	  isLoggedIn: function (req, res, next) {
+  // route middleware to make sure a user is logged in
+    isLoggedIn: function (req, res, next) {
 
-	    // if user is authenticated in the session, carry on
-	    if (req.isAuthenticated()) {
-	      return next();
-	    }
-	    // if they aren't redirect them to the home page
-	    res.redirect('/');
-	}
-}
+      // if user is authenticated in the session, carry on
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      // if they aren't redirect them to the home page
+      res.redirect('/');
+  }
+};
