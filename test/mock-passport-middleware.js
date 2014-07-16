4285e523bf57c164c515dbff0e79611fef61015c
annaship <tetya-loshad@yandex.ru>
2014-07-16 18:33:34 -0400

    test w fake auth, intermediate
    Former-commit-id: 4801576027c9ad71a8cf2da1354f95d023de6789 [formerly 248689a014e245c98fa36d1b9d5c0a555629f221]
    
    Former-commit-id: 1c3b9121b99691e35c3a9fcf3e96a48d5ee90863

---
+++ test/mock-passport-middleware.js
@@ -0,0 +1,25 @@
+// Fake user login with passport.
+app.mockPassportInitialize = function () {
+    var passport = require('passport');
+    passport.initialize = function () {
+        return function (req, res, next) {
+            passport = this;
+            passport._key = 'passport';
+            passport._userProperty = 'user';
+            passport.serializeUser = function(user, done) {
+                return done(null, user.id);
+            };
+            passport.deserializeUser = function(user, done) {
+                return done(null, user);
+            };
+            req._passport = {
+                instance: passport
+            };
+            req._passport.session = {
+                user: new app.models.User({ id: 100, name: 'TEST' })
+            };
+
+            return next();
+        };
+    };
+};
