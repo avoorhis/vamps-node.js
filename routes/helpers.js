1bf716052d771126b16c1af05cd9b9740f41c873
annaship <tetya-loshad@yandex.ru>
2014-07-16 12:02:44 -0400

    move isLoggedIn - to helpers, use in users, userSpec

--- routes/helpers.js
+++ routes/helpers.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-// route middleware to make sure a user is logged in
+module.exports = {
 
+	// route middleware to make sure a user is logged in
+	  isLoggedIn: function (req, res, next) {
 
-module.exports = isLoggedIn;
+	    // if user is authenticated in the session, carry on
+	    if (req.isAuthenticated()) {
+	      return next();
+	    }
+	    // if they aren't redirect them to the home page
+	    res.redirect('/');
+	}
+}
