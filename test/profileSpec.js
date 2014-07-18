bd532586fe0a552ca33b9523d3f4fb39f1421517
annaship <tetya-loshad@yandex.ru>
2014-07-18 18:01:52 -0400

    test user

--- test/profileSpec.js
+++ test/profileSpec.js
@@ -11,27 +11,11 @@ req = request(app);
 describe('Profile page functionality', function(){
   
   it('redirect to / if not logged in', function(done){
-    request(app)
+
+    req
       .get('/users/profile')
       .expect(302)
       .end(function (err, res) {
-        res.header.location.should.include('/');
-        //   
-        // res.text.should.include('VAMPS Login');
-        // res.text.should.include('Username');
-        // res.text.should.include('Password');
-        done();
-      });
-  });
-
-//   it('Text on profile page if logged in');
-//     
-// });
-// 
-// describe('GET /admin', function() {
-  it('responds with 302 if not logged in', function(done) {
-    req.get('/users/profile').expect(302)
-    .end(function (err, res) {
         res.header.location.should.include('/');
         done();
       });
@@ -42,7 +26,8 @@ describe('Profile page functionality', function(){
       username: 'TEST', password: 'TEST'    
     });
 
-    req.get('/users/profile')
+    req
+      .get('/users/profile')
       .expect(200)
       .end(function (err, res) {
         // console.log("===2===");
@@ -55,4 +40,6 @@ describe('Profile page functionality', function(){
     
     // passportStub.logout();
   });
+  
+  
 });
