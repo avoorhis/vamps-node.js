b7429b06c4dae0db36f404a2ed390950a3084499
Andrew Voorhis <avoorhis@mbl.edu>
2014-07-29 16:17:03 -0400

    modularize vaisualization.js
    Former-commit-id: 801b9638a8779ecf6a43cae12ac5d5b664759298 [formerly 985d584272703981419c0562be6235309cd9c77a]
    
    Former-commit-id: f7f86bdd828f7938a3bc31a0f20939c5c3c6ff2f

---
+++ routes/visuals/common.js
@@ -0,0 +1,171 @@
+// common.js
+
+
+
+module.exports = {
+
+	get_selection_markup: function( visual, body ) {
+	  var html = "<div id='' class='selection_info'>";
+	  if(visual === 'heatmap' || visual === 'dendrogram') {
+	    html += '<li>Selected Distance Metric: ' + body.selected_distance + '</li>';
+	  }
+	  html += '<li>Maximum Dataset Count: ' + body.max_ds_count.toString() + '</li>';
+	  if(body.unit_choice.indexOf('tax') === 0 ) {
+	    html += '<li>Included Domains: ' + body.domains     + '</li>';
+	    html += '<li>Include NAs?: '     + body.include_nas + '</li>';
+	    html += '<li>Taxonomic Depth: '  + body.tax_depth   + '</li>';
+	  }
+	  html += '<li>Normalization: ' + body.normalization + '</li>';
+	  html += '</div>';
+	  return html;
+	},
+
+
+	//
+	//
+	//
+	get_taxonomy_query: function( db, uitems, body ) {
+	  console.log(body);
+	  selection_obj = body.selection_obj;
+	  //selection_obj = body.selection_obj;
+	  
+	  if (uitems[0] === 'tax' && uitems[1] === 'silva108'){  //covers both simple and custom taxonomy
+	    uassoc = 'tax_silva108_id';
+	  }
+	  var domains = body.domains || ['Archaea', 'Bacteria', 'Eukarya', 'Organelle', 'Unknown'];
+	  var tax_depth = body.tax_depth || 'phylum';
+	  var fields = [];
+	  var joins = '';
+	  var and_domain_in = '';
+	  if (tax_depth === 'domain') {
+	    fields = ['domain'];
+	    joins = " JOIN domains  as dom on (t.domain_id=dom.id)";
+	  } else if (tax_depth === 'phylum') {
+	    fields = ['domain','phylum'];
+	    joins =  " JOIN domains  as dom on ( t.domain_id = dom.id )\n";
+	    joins += " JOIN phylums  as phy on ( t.phylum_id = phy.id )\n";
+	  } else if (tax_depth === 'class')  {
+	    fields = ['domain','phylum','klass'];
+	    joins =  " JOIN domains  as dom on ( t.domain_id = dom.id )\n";
+	    joins += " JOIN phylums  as phy on ( t.phylum_id = phy.id )\n";
+	    joins += " JOIN klasses  as kla on ( t.klass_id  = kla.id )\n";
+	  } else if (tax_depth === 'order')  {
+	    fields = ['domain','phylum','klass','orderx'];
+	    joins =  " JOIN domains  as dom on ( t.domain_id = dom.id )\n";
+	    joins += " JOIN phylums  as phy on ( t.phylum_id = phy.id )\n";
+	    joins += " JOIN klasses  as kla on ( t.klass_id  = kla.id )\n";
+	    joins += " JOIN orders   as ord on ( t.order_id  = ord.id )\n";
+	  } else if (tax_depth === 'family') {
+	    fields = ['domain','phylum','klass','orderx','family'];
+	    joins =  " JOIN domains  as dom on ( t.domain_id = dom.id )\n";
+	    joins += " JOIN phylums  as phy on ( t.phylum_id = phy.id )\n";
+	    joins += " JOIN klasses  as kla on ( t.klass_id  = kla.id )\n";
+	    joins += " JOIN orders   as ord on ( t.order_id  = ord.id )\n";
+	    joins += " JOIN families as fam on ( t.family_id = fam.id )\n";
+	  } else if (tax_depth === 'genus') {
+	    fields = ['domain','phylum','klass','orderx','family','genus'];
+	    joins =  " JOIN domains  as dom on ( t.domain_id = dom.id )\n";
+	    joins += " JOIN phylums  as phy on ( t.phylum_id = phy.id )\n";
+	    joins += " JOIN klasses  as kla on ( t.klass_id  = kla.id )\n";
+	    joins += " JOIN orders   as ord on ( t.order_id  = ord.id )\n";
+	    joins += " JOIN families as fam on ( t.family_id = fam.id )\n";
+	    joins += " JOIN genera   as gen on ( t.genus_id  = gen.id )\n";
+	  } else if (tax_depth === 'species') {
+	    fields = ['domain','phylum','klass','orderx','family','genus','species'];
+	    joins =  " JOIN domains  as dom on ( t.domain_id = dom.id )\n";
+	    joins += " JOIN phylums  as phy on ( t.phylum_id = phy.id )\n";
+	    joins += " JOIN klasses  as kla on ( t.klass_id  = kla.id )\n";
+	    joins += " JOIN orders   as ord on ( t.order_id  = ord.id )\n";
+	    joins += " JOIN families as fam on ( t.family_id = fam.id )\n";
+	    joins += " JOIN genera   as gen on ( t.genus_id  = gen.id )\n";
+	    joins += " JOIN species  as spe on ( t.species_id= spe.id )\n";
+	  }
+	    
+	  if (domains.length < 5){
+	    domains = domains.join("','");
+	    and_domain_in = " AND domain in ('"+domains+"')";
+	  }
+	  
+	  var tax_query = "SELECT distinct t.id, concat_ws(';',"+fields+") as tax FROM taxonomies as t\n";
+	  tax_query     += joins;
+	  
+	  unit_id_array = []
+	  for(var n in selection_obj.unit_assoc[uassoc]){
+	    unit_id_array = unit_id_array.concat(selection_obj.unit_assoc[uassoc][n])
+
+	  }
+	  tax_query     += " WHERE t.id in (" + unit_id_array + ")\n";
+	  tax_query     += and_domain_in;
+	  return tax_query;
+
+	},
+
+	//
+	//
+	//
+	get_otus_query: function() {
+
+	},
+
+	//
+	//
+	//
+	get_med_query: function() {
+
+	},
+
+
+	//
+	// NORMALIZATION
+	//
+	normalize_counts: function(norm_type, body) {
+	  
+	  // get max dataset count
+	  //var selection_obj = JSON.parse(obj);
+	  var max_count = body.max_ds_count;
+	  //selection_obj.max_ds_count = max_count;
+	  console.log('in normalization: '+norm_type+' max: '+max_count.toString());
+
+	  var new_counts_obj = [];
+	  for (var n=0; n < body.selection_obj.seq_freqs.length; n++) {
+	    var sum=0;
+	    for (var i = 0; i < body.selection_obj.seq_freqs[n].length; i++ ) {
+	      sum += body.selection_obj.seq_freqs[n][i];
+	    }
+	    var temp = [];
+	    for (i=0; i < body.selection_obj.seq_freqs[n].length; i++) {
+
+	      if (norm_type === 'max') {
+	        temp.push( parseInt( ( body.selection_obj.seq_freqs[n][i] * max_count ) / sum, 10) );
+	      } else {  // freq
+	        temp.push( parseFloat( ( body.selection_obj.seq_freqs[n][i]  / sum ).toFixed(8) ) );
+	      }
+
+	    }
+	    new_counts_obj.push(temp);
+	  }
+	  body.selection_obj.seq_freqs = new_counts_obj;
+	  body.selection_obj.max_ds_count = max_count;
+	  return body.selection_obj;
+	},
+
+	//
+	//  MAX DS COUNT
+	//
+	get_max_dataset_count: function(obj) {
+	  // Gets the maximum dataset count from the 'seq_freqs' in selection_obj
+	  var max_count = 0;
+	  for (var n=0; n < obj.seq_freqs.length; n++) {
+	    var sum=0;
+	    for (var i = 0; i < obj.seq_freqs[n].length; i++) {  
+	      sum += parseInt(obj.seq_freqs[n][i]);
+	      //console.log(obj.seq_freqs[n][i]);
+	    }
+	    
+	    if (sum > max_count) {
+	      max_count = sum;
+	    }
+	  }
+	  return max_count;
+	}
+}
