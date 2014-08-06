3f8ff91d5ba69561bfb9d382cc15596fc7af37d2
annaship <tetya-loshad@yandex.ru>
2014-08-06 13:37:08 -0400

    change queries to the new schema
    Former-commit-id: b8b9e9ee2908678841a7f973a5228390b80b0cf0 [formerly 03dd4d6ac62063e2f5ccfcf06eb26aa389f088ba]
    Former-commit-id: 17ab090144d042bdd4519fe533d67de18393a5ab

--- routes/projects.js
+++ routes/projects.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET New User page. */
 router.get('/index_projects', function(req, res) {
     var db = req.db;
-    var qSelect = "SELECT id, project, title, project_description from projects";
+    var qSelect = "SELECT project_id, project, title, project_description from project";
     console.log(qSelect);
     var collection = db.query(qSelect, function (err, rows, fields){
       if (err) {
@@ -19,7 +19,7 @@ router.get('/index_projects', function(req, res) {
 
 router.get('/:id', function(req, res) {
     var db = req.db;
-    var qSelect = "SELECT id, project, title, project_description from projects where id = '" + req.params.id +"'";
+    var qSelect = "SELECT project_id, project, title, project_description from project where id = '" + req.params.id +"'";
     console.log(qSelect);
     var collection = db.query(qSelect, function (err, row, fields){
       if (err)  {
