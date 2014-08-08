03dd0386230088cd3b0f73e3f599d2bd24f0f164
Andrew Voorhis <avoorhis@mbl.edu>
2014-08-08 12:04:17 -0400

    changed file names in routes to avoid confusion

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var users = require('./routes/users');
 var projects = require('./routes/projects');
 var datasets = require('./routes/datasets');
 //var ALL_DATASETS = require('./routes/load_all_datasets2')(connection);
-var visuals = require('./routes/visuals/visualization');
+var visuals = require('./routes/visuals/routes_visualization');
 var C = require('./public/constants');
 
 var app = express();
