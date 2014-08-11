99b8b06669707383f4680ac90890514b66138e26
annaship <tetya-loshad@yandex.ru>
2014-08-11 17:02:32 -0400

    DRY check_form_pg2.js, intermediate
    Former-commit-id: 9b04b9b825e216ac5ba438ac98ee338ed0788d6e

--- public/javascripts/check_form_pg2.js
+++ public/javascripts/check_form_pg2.js
@@ -1,7 +1,7 @@
 // visualization: check_form_pg2.js
 
 var get_graphics_form = document.getElementById('get_graphics_form');
-get_graphics = document.getElementById('get_graphics');
+var get_graphics = document.getElementById('get_graphics');
 if (typeof get_graphics !=="undefined") 
 {
   get_graphics.addEventListener('click', function () {
@@ -12,44 +12,38 @@ if (typeof get_graphics !=="undefined")
 //
 // CHECK_VIZ Page 2
 //
+
+function is_checked()
+{
+  return $('input[name="domains[]"]:checked').length > 0; 
+}
+
 function check_viz_selection_pg2(thisform) {
   // simple function to check if user has not selected any visuals.
   // alerts and returns if not.
   var visuals = thisform["visuals[]"];
   var unit_selection = thisform["unit_selection"].value;
+  // var domain_names = thisform["domains[]"];
+  
   var valid  = false;
   var msg = '';
-  var domain_names;
   var i;
   
   // test for selected units: taxonomy, med_nodes, otus
   if (unit_selection === 'tax_silva108_simple') {
     // check for domains[].length must be > 0
-    domain_names = thisform["domains[]"];
     msg = '1) You must select some taxa';
-    for (i=0; i<domain_names.length; i++)
-    {
-      if (domain_names[i].checked) {
-        valid = true;
-      }
-    }
+    valid = is_checked();
   } else if (unit_selection === 'tax_silva108_custom'){
     // check for domain_names <-- atleast one selected
-    domain_names = thisform["domain_names[]"];
     msg = '2) You must select some taxa';
-
-    for (i=0; i<domain_names.length; i++)
-    {
-      if (domain_names[i].checked) {
-        valid = true;
-      }
-    }
-
-  }else{
-
+    valid = is_checked();
   }
+  // else{
+  // 
+  // }
   
-  if(!valid){alert(msg);return;}
+  if(!valid) {alert(msg);return;}
 
   //
   // test for selected visuals: counts_table, heatmap ...
