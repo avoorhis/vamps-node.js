be935cd344e07a04b39b0c6e55e064f071968b98
annaship <tetya-loshad@yandex.ru>
2014-08-12 12:19:59 -0400

    refactoring form checking
    Former-commit-id: d67a1392d09a6b920b5b58e15ce5c4cb1325ef02

--- public/javascripts/check_form_pg1.js
+++ public/javascripts/check_form_pg1.js
@@ -1,4 +1,8 @@
-// visualization: check_form_pg1.js
+/* visualization: check_form_pg1.js
+* CHECK_VIZ Page 1
+* check if user has not selected any datasets.
+* alerts and returns if not.
+*/
 
 var dataset_select_form = document.getElementById('dataset_select_form');
 var unit_selection = document.getElementById('unit_selection');
@@ -6,29 +10,45 @@ if (typeof unit_selection !=="undefined")
 {
   unit_selection.addEventListener('click', function () {
     // alert('Hello world');
-    check_viz_selection_pg1(dataset_select_form);
+    msg = 'You must select some datasets';
+    checkbox_name = "dataset_ids[]";
+    check_form(dataset_select_form, msg, checkbox_name);
+    // check_viz_selection_pg1(dataset_select_form);
   });
 }
 
-
-//
-// CHECK_VIZ Page 1
-//
-function check_viz_selection_pg1(thisform) {
-  // simple function to check if user has not selected any datasets.
-  // alerts and returns if not.
-  var x = thisform["dataset_ids[]"];
-  var gotone = false;
-  for (var i=0; i<x.length; i++)
-  {
-      if (x[i].checked) {
-        gotone = true;
-      }
-  }
-  if (gotone){
-    thisform.submit();
-  } else {
-    alert('You must select some datasets');
-    return;
-  }
+function is_checked(id_names)
+{
+  return $('input[name="'+id_names+'"]:checked').length > 0; 
 }
+
+function check_form(select_form, msg, checkbox_name)
+{
+  var my_check = false;
+  my_check = is_checked(checkbox_name);
+  my_check ? select_form.submit() : alert(msg);
+  return;
+}
+
+// 
+// //
+// // CHECK_VIZ Page 1
+// //
+// function check_viz_selection_pg1(thisform) {
+//   // simple function to check if user has not selected any datasets.
+//   // alerts and returns if not.
+//   var x = thisform["dataset_ids[]"];
+//   var gotone = false;
+//   for (var i=0; i<x.length; i++)
+//   {
+//       if (x[i].checked) {
+//         gotone = true;
+//       }
+//   }
+//   if (gotone){
+//     thisform.submit();
+//   } else {
+//     alert('You must select some datasets');
+//     return;
+//   }
+// }
