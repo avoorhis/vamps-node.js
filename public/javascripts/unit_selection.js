aeb4116f74a357cb81695255a84e5a0700f7b8b0
annaship <tetya-loshad@yandex.ru>
2014-08-12 13:00:56 -0400

    refactor form checking
    get graphs gives errorr if choose only Archaea

--- public/javascripts/unit_selection.js
+++ public/javascripts/unit_selection.js
@@ -66,67 +66,21 @@ var get_graphics = document.getElementById('get_graphics');
 if (typeof get_graphics !=="undefined") 
 {
   get_graphics.addEventListener('click', function () {
-    // msg = 'You must select some datasets';
-    // checkbox_name = "dataset_ids[]";
-    // check_form(dataset_select_form, msg, checkbox_name);
-    
-      check_viz_selection_pg2(get_graphics_form);
+    var unit_selection = get_graphics_form["unit_selection"].value;
+    if (unit_selection === 'tax_silva108_simple' || unit_selection === 'tax_silva108_custom') 
+    {
+      msg = 'You must select some taxa';
+      checkbox_name = "domains[]";      
+      var taxa_checked = check_form(get_graphics_form, msg, checkbox_name);
+    }
+    /*
+    * uncomment if we want just one message even if both are unchecked. Untested!
+    * if (taxa_checked) 
+    {    
+    */
+      msg = 'You must select one or more display output choices';
+      checkbox_name = "visuals[]";      
+      check_form(get_graphics_form, msg, checkbox_name);
+    // }    
   });
 }
-
-//
-// CHECK_VIZ Page 2 (visuals/unit_selection)
-//
-
-// function is_checked(id_names)
-// {
-//   return $('input[name="'+id_names+'"]:checked').length > 0; 
-// }
-
-// todo: DRY valid = false; check; if(!valid) {alert(msg);return;}
-// function check_for_taxa()
-// {
-//   
-// }
-function check_viz_selection_pg2(thisform) {
-  // simple function to check if user has not selected any visuals.
-  // alerts and returns if not.
-  var unit_selection = thisform["unit_selection"].value;
-  var valid = false;
-  var msg = '';
-  
-  // test for selected units: taxonomy, med_nodes, otus
-  if (unit_selection === 'tax_silva108_simple' || unit_selection === 'tax_silva108_custom') {
-  
-  // if (unit_selection === 'tax_silva108_simple') {
-  //   // check for domains[].length must be > 0
-  //   msg = '1) You must select some taxa';
-  //   valid = is_checked(domains_id_names);
-  // } else if (unit_selection === 'tax_silva108_custom'){
-  //   // check for domain_names <-- atleast one selected
-    // msg = '2) You must select some taxa';
-    msg = 'You must select some taxa';
-    valid = is_checked("domains[]");
-  }
-  // else{
-  // 
-  // }
-  
-  if(!valid) {alert(msg);return;}
-
-  //
-  // test for selected visuals: counts_table, heatmap ...
-  //
-  valid = false;
-  msg = 'You must select one or more display output choices';
-  valid = is_checked("visuals[]");
-  
-  if(!valid){alert(msg); return;}
-  
-  thisform.submit();
-  
-  // what else do we check here?
-  // check for NO unit_associations: unit_assoc":{"tax_silva108_id":[],"tax_gg_id":[],"med_node_id":[],"otu_id":[]}}
-
-}
-
