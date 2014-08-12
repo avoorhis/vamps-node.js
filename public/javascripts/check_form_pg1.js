051f8c3d31cb18e2d23c644af24a804de1ccdfd9
annaship <tetya-loshad@yandex.ru>
2014-08-12 12:25:26 -0400

    refactoring form checking
    Former-commit-id: c72a1d7aa417b8b1f34b0a7b28236754574fd1fd [formerly c83bee62ebf26002a1d2a28c47281ef0a5b099cc]
    
    Former-commit-id: 041dfe07f8200bc9a095daa427d1eae6e002a3ee

--- public/javascripts/check_form_pg1.js
+++ public/javascripts/check_form_pg1.js
@@ -9,46 +9,8 @@ var unit_selection = document.getElementById('unit_selection');
 if (typeof unit_selection !=="undefined") 
 {
   unit_selection.addEventListener('click', function () {
-    // alert('Hello world');
     msg = 'You must select some datasets';
     checkbox_name = "dataset_ids[]";
     check_form(dataset_select_form, msg, checkbox_name);
-    // check_viz_selection_pg1(dataset_select_form);
   });
 }
-
-function is_checked(id_names)
-{
-  return $('input[name="'+id_names+'"]:checked').length > 0; 
-}
-
-function check_form(select_form, msg, checkbox_name)
-{
-  var my_check = false;
-  my_check = is_checked(checkbox_name);
-  my_check ? select_form.submit() : alert(msg);
-  return;
-}
-
-// 
-// //
-// // CHECK_VIZ Page 1
-// //
-// function check_viz_selection_pg1(thisform) {
-//   // simple function to check if user has not selected any datasets.
-//   // alerts and returns if not.
-//   var x = thisform["dataset_ids[]"];
-//   var gotone = false;
-//   for (var i=0; i<x.length; i++)
-//   {
-//       if (x[i].checked) {
-//         gotone = true;
-//       }
-//   }
-//   if (gotone){
-//     thisform.submit();
-//   } else {
-//     alert('You must select some datasets');
-//     return;
-//   }
-// }
