d5eee43159ba21e1a076329d243b676f03259997
annaship <tetya-loshad@yandex.ru>
2014-08-21 18:09:32 -0400

    refactoring taxa class; add make dict function - interim
    Former-commit-id: 9662ef5bc241d6abd2572fefc4eb46e64450d7f5 [formerly 75c736e89ad208de0e605959a47b17c3264c0746]
    
    Former-commit-id: 41bc89e02f731949e259215fac58c4fcda5863f6

--- routes/visuals/custom_taxa_class.js
+++ routes/visuals/custom_taxa_class.js
@@ -21,15 +21,51 @@ function make_dictMap_by_rank(tags) {
   return dictMap_by_rank
 }
 
-function get_by_key(dictMap, dictMap_key) {
-    return dictMap[dictMap_key];
+function add_to_dict_by_key(dictMap, dictMap_key, value) 
+{
+  dictMap[dictMap_key] = value;
+  return dictMap
 };
+
+function get_by_key(dictMap, dictMap_key) 
+{
+  return dictMap[dictMap_key];
+};
+
+function make_current_dict(taxa_name, taxa_rank, i_am_a_parent, taxon_name_id)
+{
+  var current_dict =
+  {
+    parent_id: "",
+    children_ids : [],
+    taxon: "",
+    rank: "",
+    node_id: 1
+  };
+  
+  current_dict.taxon = taxa_name;
+  current_dict.rank = taxa_rank;
+  current_dict.parent_id = i_am_a_parent;
+  current_dict.node_id = taxon_name_id;
+  return current_dict;
+}
+
+function add_children_to_parent(dictMap_by_id, current_dict)
+{
+  add_to_dict_by_key(dictMap_by_id, current_dict.node_id, current_dict);
+  
+  parent_node = dictMap_by_id[current_dict.parent_id];
+  if (parent_node)
+  {
+    parent_node.children_ids.push(current_dict.node_id)
+  }
+  return parent_node;
+}
 
 function make_taxa_tree_dict(taxonomy_obj)
 {
   var taxa_tree_dict = [];
-  var dictMap_by_name_n_rank = {};
-  var dictMap_by_id = {};
+  var dictMap_by_name_n_rank = dictMap_by_id = {};
   for (var i=0; i < taxonomy_obj.length; i++)
   {
     in_obj = taxonomy_obj[i];
@@ -38,45 +74,28 @@ function make_taxa_tree_dict(taxonomy_obj)
 
     for (var taxa_rank in in_obj)
     {
-      var parent_node = current_dict =
-      {
-        parent_id: "",
-        children_ids : [],
-        taxon: "",
-        rank: "",
-        node_id: 1
-      };
-      // var parent_node = current_dict;
+      var parent_node = current_dict = {};
 
       if (in_obj.hasOwnProperty(taxa_rank))
       {
         var taxa_name = in_obj[taxa_rank];
         if (taxa_name)
         {
-          current_dict.taxon = taxa_name;
-          current_dict.rank = taxa_rank;
-
-          current_dict.parent_id = i_am_a_parent;
           node = get_by_key(dictMap_by_name_n_rank, taxa_name + taxa_rank);
 
           if (!node)
           {
-            current_dict.node_id = taxon_name_id;
-            taxa_tree_dict.push(current_dict);
+            current_dict = make_current_dict(taxa_name, taxa_rank, i_am_a_parent, taxon_name_id)
 
-            dictMap_by_name_n_rank[current_dict.taxon + current_dict.rank] = current_dict;
-            dictMap_by_id[current_dict.node_id] = current_dict;
+            taxa_tree_dict.push(current_dict);
+            
+            add_to_dict_by_key(dictMap_by_name_n_rank, current_dict.taxon + current_dict.rank, current_dict);
 
             i_am_a_parent = current_dict.node_id;
 
             taxon_name_id += 1;
-
-            parent_node = dictMap_by_id[current_dict.parent_id];
-            if (parent_node)
-            {
-              parent_node.children_ids.push(current_dict.node_id)
-            }
-
+            
+            add_children_to_parent(dictMap_by_id, current_dict);
           }
           else
           {
@@ -89,9 +108,8 @@ function make_taxa_tree_dict(taxonomy_obj)
   return taxa_tree_dict;
 }
 
-
 // Public
-self = module.exports = TaxonomyTree;
+module.exports = TaxonomyTree;
 
 function TaxonomyTree(rows) {
   this.taxa_tree_dict = this.taxa_tree_dict_map_by_rank = [];
@@ -101,15 +119,28 @@ function TaxonomyTree(rows) {
   this.taxa_tree_dict_map_by_rank = make_dictMap_by_rank(this.taxa_tree_dict);
 }
 
-TaxonomyTree.prototype.togglePaid = function() {
-  this._paid = !this._paid;
-};
+TaxonomyTree.prototype.make_dict = function(tree_obj, key_name) 
+{
+  var i = null;
+  new_dict = {};
+  console.log("tree_obj NNN1 " + JSON.stringify(tree_obj));
+  console.log("key_name NNN2 " + JSON.stringify(key_name));
+  for (i = 0; tree_obj.length > i; i += 1) {
+    new_dict[tree_obj[i][key_name]] = tree_obj[i];
+  }
+  console.log("new_dict NNN " + JSON.stringify(new_dict));
+}
 
-TaxonomyTree.prototype.userType = function() {
-  if(this._paid) return 'Paid TaxonomyTree';
-  else           return 'Free TaxonomyTree';
-};
-
-TaxonomyTree.prototype.addBalance = function(amount) {
-  this.balance += depositeMinusFee(amount);
-};
+// 
+// TaxonomyTree.prototype.togglePaid = function() {
+//   this._paid = !this._paid;
+// };
+// 
+// TaxonomyTree.prototype.userType = function() {
+//   if(this._paid) return 'Paid TaxonomyTree';
+//   else           return 'Free TaxonomyTree';
+// };
+// 
+// TaxonomyTree.prototype.addBalance = function(amount) {
+//   this.balance += depositeMinusFee(amount);
+// };
