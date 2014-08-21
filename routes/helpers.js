16184d095cb94e735118ff4f181e34673759b51e
annaship <tetya-loshad@yandex.ru>
2014-08-21 16:55:11 -0400

    refactoring taxa node class
    Former-commit-id: 54d0744bb872080c96450e71e978f476df27367a [formerly 13fd63698eed2732bc82d8507de9d03caa0531b2]
    
    Former-commit-id: 95fa68d1e780462b06347e09c7611bb34d84483d

--- routes/helpers.js
+++ routes/helpers.js
@@ -21,15 +21,16 @@ module.exports = {
 
 /** Benchmarking
 * Usage: 
-* start = process.hrtime();
+    var helpers = require('../helpers');
+
+    helpers.start = process.hrtime();
     some code
-* lapsed_time("This is the running time for some code");
+    helpers.elapsed_time("This is the running time for some code");
 */
 
-var start = process.hrtime();
+module.exports.start = process.hrtime();
 module.exports.elapsed_time = function(note){
     var precision = 3; // 3 decimal places
-    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
-    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
-    start = process.hrtime(); // reset the timer
+    var elapsed = process.hrtime(module.exports.start)[1] / 1000000; // divide by a million to get nano to milli
+    console.log(process.hrtime(module.exports.start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
 };
