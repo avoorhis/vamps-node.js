67f2b59b9e8abc19ebc2e511e6c137213ce97ad5
annaship <tetya-loshad@yandex.ru>
2014-08-21 15:40:41 -0400

    move benchmarking function to helpers
    Former-commit-id: 37ff466228fff6fca4588abae619e620bf01445e

--- routes/helpers.js
+++ routes/helpers.js
@@ -4,7 +4,7 @@ var router = express.Router();
 module.exports = {
 
   // route middleware to make sure a user is logged in
-    isLoggedIn: function (req, res, next) {
+  isLoggedIn: function (req, res, next) {
 
       // if user is authenticated in the session, carry on
       if (req.isAuthenticated()) {
@@ -15,4 +15,21 @@ module.exports = {
       console.log("Oops! NOT isLoggedIn.req.isAuthenticated");
       res.redirect('/');
   }
+  
+  
+};
+
+/** Benchmarking
+* Usage: 
+* start = process.hrtime();
+    some code
+* lapsed_time("This is the running time for some code");
+*/
+
+var start = process.hrtime();
+module.exports.elapsed_time = function(note){
+    var precision = 3; // 3 decimal places
+    var elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get nano to milli
+    console.log(process.hrtime(start)[0] + " s, " + elapsed.toFixed(precision) + " ms - " + note); // print message + time
+    start = process.hrtime(); // reset the timer
 };
