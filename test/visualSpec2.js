2d5f56fe4df380dac5cfa5a6df7995f116444448
Andrew Voorhis <avoorhis@mbl.edu>
2014-08-29 16:03:57 -0400

    some testing with zombie

--- test/visualSpec2.js
+++ test/visualSpec2.js
@@ -15,7 +15,7 @@ describe('visualization functionality:', function(){
 	    // In our tests we use the test db
 	    //mongoose.connect(config.db.mongodb);              
 	    this.server = http.createServer(app).listen(3001);
-	    this.browser = new Browser({ site: 'http://localhost:3001' });
+	    this.browser = new Browser({ debug: true, site: 'http://localhost:3001' });
 	    
 	  });
 	  // load the contact page
@@ -26,36 +26,37 @@ describe('visualization functionality:', function(){
 	  //
 	  describe('index_visuals', function(){
 
-			it('should show project list', function() {
-		    assert.ok(this.browser.success);
-		    assert.equal(this.browser.text('label#SLM_NIH_Bv6'), 'SLM_NIH_Bv6');
-		    //assert.equal(this.browser.text('input#unit_selection'), 'Next: Unit Selection');
-	  	});
+			// it('should show project list', function() {
+		 //    assert.ok(this.browser.success);
+		 //    assert.equal(this.browser.text('label#SLM_NIH_Bv6'), 'SLM_NIH_Bv6');
+		 //    //assert.equal(this.browser.text('input#unit_selection'), 'Next: Unit Selection');
+	  // 	});
 
-	  	it('should refuse empty submissions', function(done) {
-	    	var browser = this.browser;
+	  // 	it('should refuse empty submissions', function(done) {
+		 //    this.browser.pressButton('#unit_selection').then(function() {
+		 //      // http://redotheweb.com/2013/01/15/functional-testing-for-nodejs-using-mocha-and-zombie-js.html
+		 //      // http://zombie.labnotes.org/
+		 //      assert.equal(window.location.pathname, "/visuals/index_visuals");
+		 //      //assert.equal(browser.text('div.alert'), 'Please choose some data');
+		 //    }).then(done,done);
+		 //  });
+	  	//
+	  	//
+	 		it('should send form when button is pressed', function(done) {
+		    var browser = this.browser;
+		    browser.check('#SLM_NIH_Bv6--pj-id');
+		    //console.log(this.browser.html());
 		    browser.pressButton('#unit_selection').then(function() {
-		      // http://redotheweb.com/2013/01/15/functional-testing-for-nodejs-using-mocha-and-zombie-js.html
-		      // http://zombie.labnotes.org/
-		      assert.equal(browser.location.pathname, "/visuals/index_visuals");
-		      //assert.equal(browser.text('div.alert'), 'Please choose some data');
-		    }).then(done,done);
-		  });
-	  	//
-	  	//
-	 		it('should work', function() {
-	    	var browser = this.browser;
-		    browser
-		    	.check('#SLM_NIH_Bv6--pj-id');
-
-		    browser.pressButton('#unit_selection', function(done) {
-		      // http://redotheweb.com/2013/01/15/functional-testing-for-nodejs-using-mocha-and-zombie-js.html
-		      // http://zombie.labnotes.org/
-		      //assert.ok(browser.success);
-		      //assert.equal(browser.text('div.alert'), 'Please choose some data');
-		      assert.equal(browser.location.pathname, "/visuals/unit_nselection");
-		      done();
-		    });
+			      //if (error) return done(error);
+			      // http://redotheweb.com/2013/01/15/functional-testing-for-nodejs-using-mocha-and-zombie-js.html
+			      // http://zombie.labnotes.org/
+			      assert.fail();
+			      //assert.equal(browser.text('div.alert'), 'Please choose some data');
+			      console.log('see me1');
+			      assert.equal('xx', "yy");
+			      console.log('see me2');
+			      //done();
+		    	}).then(done, done);
 		  });
 
 	 	});  // END describe index_visuals
