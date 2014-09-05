03b4d7d8002b161d1d160b1842d016212879ad2b
Andrew Voorhis <avoorhis@mbl.edu>
2014-09-05 13:19:06 -0400

    cleanup unit counts table
    Former-commit-id: b7077f8099f32b00435a6f4c108e889b18acd0bd

--- routes/visuals/routes_counts_table.js
+++ routes/visuals/routes_counts_table.js
@@ -9,7 +9,7 @@ module.exports = {
 	//
 	// CREATE COUNTS TABLE HTML
 	//
-	create_counts_table_html: function( ts, count_matrix, obj ) {
+	create_counts_table_html: function( ts, matrix, obj ) {
 	  // Intend to create (write) counts_table page here.
 	  // The page should have a timestamp and/or username appeneded to the file name
 	  // so that it is unique to the user.
@@ -20,7 +20,7 @@ module.exports = {
 	  //var choices_html
 	  var out_file = '../../tmp/'+ts+'_counts_table.html';
 	  
-		var html = create_ct_html(count_matrix)
+		var html = create_ct_html(matrix)
 	  
 	  //console.log(column_totals)
 	  COMMON.write_file(out_file,html);
@@ -32,38 +32,44 @@ module.exports = {
 //
 //  CREATE HTML
 //
-function create_ct_html(count_matrix) {
+function create_ct_html(matrix) {
 
 	var html = '';
 	html += "<div class='' id='counts_table_div' >";
 	  var column_totals = {};
 	  html += "<table border='1' class='counts_table' >";
 	  html += "<tr><td></td>";
-	  for(n in count_matrix.dataset_names) {
-	    html += "<td>"+count_matrix.dataset_names[n]+"</td>";
+	  for (n in matrix.dataset_names) {
+	    html += "<td>"+matrix.dataset_names[n]+"</td>";
 	  }
 	  html += "</tr>";  
-	  var row_count = 0;
-	  for(name in count_matrix.unit_names) {
-	    
+	  
+	  // get column totals
+	  for (name in matrix.unit_names) {
+	  	for(c in matrix.unit_names[name]) {
+	      if(column_totals[c] === undefined) {
+	        column_totals[c] =  matrix.unit_names[name][c];
+	      }else{
+	        column_totals[c] +=  matrix.unit_names[name][c];
+	      }
+	    }
+	  }
+
+	  for (name in matrix.unit_names) {
 	    html += "<tr>";
 	    html += "<td>"+name+"</td>";
-	    var col_count = 0;
-	    for(c in count_matrix.unit_names[name]) {
-	      if(column_totals[col_count] === undefined){
-	        column_totals[col_count] =  count_matrix.unit_names[name][c];
-	      }else{
-	        column_totals[col_count] +=  count_matrix.unit_names[name][c];
-	      }
-	      
-	      html += "<td>"+count_matrix.unit_names[name][c]+"</td>";
-	      col_count += 1;
+	    for(c in matrix.unit_names[name]) {
+	    	var cnt = matrix.unit_names[name][c];
+	    	var pct =  (cnt * 100 / column_totals[c]).toFixed(2)
+	      var id  = matrix.dataset_names[c]+'-|-'+cnt.toString()+'-|-'+pct.toString();
+	      html += "<td id='"+id+"' class='tooltip'>"+matrix.unit_names[name][c]+"</td>";
+	      //col_count += 1;
 	    }
 	    html += "</tr>";
-	    row_count += 1;
+	    
 	  }
 	  html += "<tr><td>TOTAL COUNTS:</td>";
-	  for(n in column_totals) {
+	  for (n in column_totals) {
 	    html += "<td>"+column_totals[n]+"</td>";
 	  }
 	  html += "</tr>";
