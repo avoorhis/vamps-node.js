61dbf4c39d49301533a68aa26be39dec0c6a6f56
Andrew Voorhis <avoorhis@mbl.edu>
2014-09-05 13:19:06 -0400

    cleanup unit counts table
    Former-commit-id: 200f6892fc8308b323372cbdcdf9b5c28cb3df30 [formerly eb2978275de8afef681e2d540eac1f1c964ecd33]
    Former-commit-id: 7b6c9815d898926b4cb5f8aa647dab9b86e7b8b5

--- routes/visuals/routes_common.js
+++ routes/visuals/routes_common.js
@@ -1,13 +1,14 @@
 // common.js
 var path = require('path');
 var fs = require('fs');
+var extend = require('util')._extend;
 var C = require('../../public/constants');
 
 module.exports = {
 
 	get_selection_markup: function( visual, obj ) {
 	  
-		console.log(obj)
+		// obj is visual_post_items
 	  var html = "<div id='' class='selection_info'>";
 	  if(visual === 'heatmap') {
 	    html += '<li>Selected Distance Metric: ' + obj.selected_heatmap_distance + '</li>';
@@ -42,7 +43,7 @@ module.exports = {
 	    html += '<li>Change Distance Metric: ';
 	    html += "<select name='selected_distance' class='small_font'>";
       for(d in distrows ) {
-      	if(obj[viz_page]===distrows[d].id) {
+      	if(obj[viz_page] === distrows[d].id) {
       		html += "<option value='"+distrows[d].id+"' selected='selected' >"+distrows[d].show+"</option>";
       	}else{
       		html += "<option value='"+distrows[d].id+"'>"+distrows[d].show+"</option>";
@@ -62,11 +63,11 @@ module.exports = {
   // domains: [ 'Archaea', 'Bacteria', 'Eukarya', 'Organelle', 'Unknown' ],
   // include_nas: 'yes' }
 	  html += '<li>Normalization: ';
-	  if(obj.normalization ==='freq') {
+	  if(obj.normalization === 'freq') {
 		  html += '<input type="radio" name="norm" value="none" >None &nbsp;&nbsp;&nbsp;';
 		  html += '<input type="radio" name="norm" value="max" >To Maximum Count &nbsp;&nbsp;&nbsp;';
 		  html += '<input type="radio" name="norm" value="freq" checked="checked">To Frequency';
-		} else if (obj.normalization ==='max') {
+		} else if (obj.normalization === 'max') {
 			html += '<input type="radio" name="norm" value="none" >None &nbsp;&nbsp;&nbsp;';
 		  html += '<input type="radio" name="norm" value="max" checked="checked">To Maximum Count &nbsp;&nbsp;&nbsp;';
 		  html += '<input type="radio" name="norm" value="freq" >To Frequency';
@@ -81,7 +82,7 @@ module.exports = {
 	 	var range = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100];
 	 	html += "MIN <select name='min_range' class='small_font'>";
 	 	for( var n=0;n < range.length-1;n++ ) {
-	 		if(obj.min_range.toString()===range[n].toString()) {
+	 		if(obj.min_range.toString() === range[n].toString()) {
 	 			html += "<option value='"+range[n]+"' selected='selected'>"+range[n]+" %</option>";
 	 		}else{
 	 			html += "<option value='"+range[n]+"'>"+range[n]+" %</option>";
@@ -91,7 +92,7 @@ module.exports = {
 	  html += "&nbsp;&nbsp;&nbsp; MAX <select name='max_range' class='small_font'>";
 	 	for( var n=1;n < range.length;n++ ) {
 	 		
-	 		if(obj.max_range.toString()===range[n].toString()) {
+	 		if(obj.max_range.toString() === range[n].toString()) {
 	 			html += "<option value='"+range[n]+"' selected='selected'>"+range[n]+" %</option>";
 	 		}else{
 	 			html += "<option value='"+range[n]+"'>"+range[n]+" %</option>";
@@ -265,7 +266,7 @@ module.exports = {
 	},
 
 	//
-	//
+	// STRING to COLOR CODE
 	//
 	string_to_color_code: function(str) {
 		// str to hash
@@ -277,7 +278,104 @@ module.exports = {
     return colour;
 
 	},
+	//
+	// GET CUSTOM COUNT MATRIX
+	//
+	get_custom_count_matrix: function(visual_post_items, old_vals, count_matrix) {
+		var custom_count_matrix = extend({},count_matrix);  // this clones count_matrix which keeps original intact.
+		console.log(visual_post_items);
+		// { dataset_names: 
+	  //  [ 'SLM_NIH_Bv4v5--01_Boonville',
+	  //    'SLM_NIH_Bv4v5--02_Spencer',
+	  //    'SLM_NIH_Bv4v5--03_Junction_City_East' ],
+	  // unit_names: 
+	  //  { 'Bacteria;Proteobacteria;Gammaproteobacteria;Enterobacteriales;Enterobacteriaceae;genus_NA': [ 2, 2, 4 ],
+	  //    'Bacteria;Proteobacteria;Gammaproteobacteria;Pseudomonadales;Pseudomonadaceae;Pseudomonas': [ 1, 0, 0 ],
+	  //    'Bacteria;Proteobacteria;Alphaproteobacteria;order_NA;family_NA;genus_NA': [ 1, 0, 0 ],
+	  //    'Bacteria;Bacteroidetes;Bacteroidia;Bacteroidales;Bacteroidaceae;Bacteroides': [ 430, 401, 272 ] } }
+		var max_cnt = visual_post_items.max_ds_count;
+		var min = visual_post_items.min_range;
+		var max = visual_post_items.max_range;
+		var norm = visual_post_items.normalization;
 
+		// normalize and filter pct here
+		var dsnames_sums={};
+		var dsnames_pcts={};
+		for(var n in custom_count_matrix.dataset_names) {
+			dsnames_sums[n] =0;
+			dsnames_pcts[n] =0;
+		}
+		var new_unames1 = {}
+		var new_unames2 = {}
+		for(var uname in custom_count_matrix.unit_names) {
+			var counts = custom_count_matrix.unit_names[uname];
+			new_unames1[uname] = [];
+			new_unames2[uname] = [];
+			console.log(uname)
+			for(k in counts) {
+				dsnames_sums[k] += counts[k];
+			}
+		}
+		
+
+		// DEF: filter pct by removing taxa where none of the values are in the range min-max% of max for that ds
+		//  		Should we normalize first? or filter first?
+		// 			the filtering needs to be reversable: count_matrix is kept intact
+		
+		//if(old_vals.min != min || old_vals.max != max) {
+		  	
+		  	
+				for(var uname in custom_count_matrix.unit_names) {
+					var counts = custom_count_matrix.unit_names[uname];
+					var got_one = false;
+					for(k in counts) {
+						//dsnames_pcts[k].push(counts[k]/dsnames_sums[k]);  //list of freqs
+						console.log((counts[k]*100)/dsnames_sums[k])
+						var x = (counts[k]*100)/dsnames_sums[k];
+						if(x > min && x < max){
+							got_one = true;
+						}
+					}			
+
+					// 
+					if(got_one){
+						new_unames1[uname] = counts;
+					}else{
+						console.log('rejecting '+uname)
+						delete new_unames1[uname];
+						delete new_unames2[uname];
+					}
+				}
+				custom_count_matrix.unit_names = new_unames1;
+				
+		//}
+
+
+		// normalize:
+		//if(old_vals.norm != norm) {
+				
+				// name_sums: { '0': 434, '1': 403, '2': 276 }
+				
+				for(var uname in custom_count_matrix.unit_names) {
+					var counts = custom_count_matrix.unit_names[uname];
+					for(k in counts) {
+							if (norm === 'max') {
+								new_unames2[uname].push(parseInt( ( counts[k] * max_cnt ) / dsnames_sums[k], 10) )
+							}else if(norm === 'freq') {
+								new_unames2[uname].push(parseFloat( counts[k] / dsnames_sums[k].toFixed(8) ) );
+							}else{
+								new_unames2[uname].push( counts[k] );
+							}
+					}
+				}
+				//console.log(new_unames);
+				custom_count_matrix.unit_names = new_unames2;
+				
+		//}
+
+
+		return custom_count_matrix;
+	} 
 
 }   // end module.exports
 
