b183af427b6cb1249511cc48b0a68e6f4e0514c6
annaship <tetya-loshad@yandex.ru>
2014-09-18 11:24:13 -0400

    custom taxa - clean up, add dblclick on name

--- public/javascripts/unit_selection.js
+++ public/javascripts/unit_selection.js
@@ -100,7 +100,7 @@ function show_custom_taxa_tree()
   $('.tree li:has(ul)').addClass('parent_li');
   // .find(' > span').attr('title', 'Collapse this branch');
   
-  $('.tree li.parent_li > span').each(function(i,e){
+  $('.tree li.parent_li > span').filter('.sign').each(function(i,e){
       if ($(this).find('i').hasClass("icon-plus-sign")){
           $(this).parent('li.parent_li').find(' > ul > li').hide();
       }
@@ -111,12 +111,8 @@ function show_custom_taxa_tree()
       $(this).prop( "checked", true );
   });
   
-  $('.tree li.parent_li > span').click(toggle_children);
-  // $('.tree li.parent_li > input').click(toggle_children1);
-  // $('.tree li.parent_li').dblclick(toggle_children2);
-  // $('input.custom-taxa').click(toggle_children1);
-  
-  // -=-=-=-
+  $('.tree li.parent_li > span').filter('.sign').click(toggle_children);
+
   $('input.custom-taxa').click(function() {
     var children = $(this).parent('li.parent_li').find(' > ul > li');
     var checkbox = $(this);
@@ -128,86 +124,16 @@ function show_custom_taxa_tree()
       show_children(this);
     }
   });
-  // -=-==-=
-  
-  // $('input.custom-taxa').click(function() {
-  //   var checkbox = $(this),
-  //     this_to_check = $(this.parentNode.parentNode).find('.custom-taxa');
-  // 
-  //   // if (!is_visible(this_to_check)) {
-  //   if (this_to_check.not(":visible")) {
-  //     this_to_check.show();
-  //   }
-  //   toggle_checking_taxa(checkbox, this_to_check);
-  // });
+
+  $('.open-one-layer').dblclick(function() {
+    alert(this.className)
+  })
 
   var toggle_children2 = function()
   {
     alert($(this).class);
   }
   
-  // $('.tree li.parent_li > span').click(function(e){
-  //   toggle_children();
-  //   e.stopPropagation();
-  // });
-  
-  // $('.tree li.parent_li > span').on('click', function (e) {
-  //     var children = $(this).parent('li.parent_li').find(' > ul > li');
-  //     if (children.is(":visible")) {
-  //         children.hide('fast');
-  //         $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-  //         // $(this).attr('title', 'Expand this branch').find(' > img').attr("src").replace("plus", "minus");
-  //     } else {
-  //         children.show('fast');
-  //         $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-  //         // $(this).attr('title', 'Expand this branch').find(' > img').attr("src").replace("minus", "plus");
-  //         // $(this).find('img').attr("src").replace("minus", "plus");
-  //     }
-  //     e.stopPropagation();
-  // });
-
-
-
-// $( "input:checked" )
-// Check #x
-// $( "#x" ).prop( "checked", true );
-// Uncheck #x
-// $( "#x" ).prop( "checked", false );
-  // th.parent('li.parent_li').find('input').each(function(i){$(this).prop( "checked", false )})
-// .attr("src", toggle_switch.attr("src").replace("down", "up"));
-  
-  // $('li.expandable').click(function() {
-  //     $(this).children('ul').toggle();
-  //     return false;
-  // });
-  // 
-  // location.reload();
-  
-  // $('div#custom_taxa_tree > ul.domain').children().css( "border", "3px double red" );
-  // .addClass( "display_none" );
-
-
-  // works
-  // $( "div#my_custom_list ul" ).click(function(){
-  //   $(this).children().css( "border", "3px double red" );
-  //   // $(this).children().toggle();
-  //   
-  // });
-  
-  // works
-  // $( "div#my_custom_list ul > li" ).click(function(){
-  //   $(this).css( "border", "3px double red" );
-  // });
-
-  // var show_next_level = function() {
-  //   children.show('fast');
-  //   $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
-  // }
-
-  // var hide_next_level = function() {
-  //   children.hide('fast');
-  //   $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
-  // }
   
 }
 
@@ -226,60 +152,19 @@ var see_this = function(variable)
 var show_children = function(current)
 {
   $(current).parent('li.parent_li').find(' > ul > li').show('fast');
-  
-  // $(current).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');  
-  $(current).parent('li.parent_li').find(' > span > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');  
-  // $(current).parent('li.parent_li').find("span[title=Collapse this branch]").find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');  
-
-  // $(current).parent('li.parent_li').find("span[title=Collapse this branch]").find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');  
+  $(current).parent('li.parent_li').find(' > span.sign > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');  
 }
 
 var hide_children = function(current, children)
 {
   children.hide('fast');
-  $(current).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+  // $(current).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+  $(current).parent('li.parent_li').find(' > span.sign > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');  
 }
 
-// $(this).siblings().find('input').each(function(i){$(this).prop( "checked", false )});
-// if (this.type === "checkbox")
-// {
-//   $(this).prop( "checked", false );    
-// }
-
 var toggle_checking = function(cl_checkbox) {
-  // alert(cl_checkbox.parentElement.className)
   val = cl_checkbox.value;
-  // val = "Crenarchaeota";
   cl_checkbox = $('input[value='+val+']');
-  // $('#special').click(function () {
-  //     specialFunction();
-  //     return false;
-  // });
-
-  // cl_checkbox.parent().find('input[value="'+val+'"]').prop('checked',
-  //            function(idx, oldProp) {
-  //              return !oldProp;
-  //            });
-             
-  // val = cl_checkbox.value;
-  // alert(val);
-  // qq = $(cl_checkbox.parentNode).find('input[value="'+val+'"]')
-  // alert(qq);
-  // if ($(cl_checkbox).prop('checked')) {
-  //   // alert("cl_checkbox yes, parentElement:");
-  //   aa = $(cl_checkbox).prop('checked');
-  //   alert(aa);
-  //  // this_to_check.find('input').prop('checked', true);
-  //  $(cl_checkbox).prop('checked', aa);
-  // }
-  // else {
-  //   $(cl_checkbox).prop('checked', true);
-  //   
-  //   // alert("cl_checkbox NO, parentElement:");
-  //   // alert(parentElement);
-  //   
-  //  // this_to_check.find('input').prop('checked', false);
-  // }
 };
 
 var toggle_checking_taxa = function(pr_checkbox, this_to_check) {
@@ -291,15 +176,6 @@ var toggle_checking_taxa = function(pr_checkbox, this_to_check) {
   }
 };
 
-//   var checkbox = $(this),
-//     this_to_check = $(this.parentNode.parentNode).find('.custom-taxa');
-// 
-//   // if (!is_visible(this_to_check)) {
-//   if (this_to_check.not(":visible")) {
-//     this_to_check.show();
-//   }
-//   toggle_checking_taxa(checkbox, this_to_check);
-
 var toggle_children1 = function()
 {
     var children = $(this).parent('li.parent_li').find(' > ul > li');
@@ -308,26 +184,6 @@ var toggle_children1 = function()
     
     if (children.is(":visible")) {
       toggle_checking_taxa(checkbox, this_to_check);
-      // toggle_checking(this);
-        // count_checked();
-        // alert(this.checked);
-        // alert($(this).prop("checked"));
-        // $(this).prop("checked", !$(this).prop("checked"));
-        
-        // $(clicked.parentNode.parentNode).find('input').prop('checked',
-        //    function(idx, oldProp) {
-        //      return !oldProp;
-        //    });
-        
-        // a = this.checked;
-        // alert(this.type);
-        // alert(!a);
-        // $(this).prop("checked", function(idx, oldProp) {
-        //              return !oldProp;
-        //           });
-        // alert(this.checked);
-        // $(this).checked = !this.checked;
-        // count_checked();
     } else {
         show_children(checkbox);
     }
