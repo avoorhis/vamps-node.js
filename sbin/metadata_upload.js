dce562987604bebd7e15d0d6c0be5c69c4c45c0d
annaship <tetya-loshad@yandex.ru>
2014-10-08 18:16:17 -0400

    csv parser temp

--- sbin/metadata_upload.js
+++ sbin/metadata_upload.js
@@ -1,170 +1,35 @@
 // metadata_upload.js
-// 
-// /**
-// * Read csv into an object
-// * get field/column names and info into mixs_field_description
-// * Put data into required_metadata_info
-// * create a custom metadata table, using dataset_id, mixs_field_description and the additional columns info
-// * The object should have methods:
-// * get required field names from db
-// * get custom field names from csv
-// * put custom field names into db
-// * create a custom table
-// * put required info in db
-// * put custom info in db
-// */
 
-// var util = require('util')
-//   , Transform = require('stream').Transform
-//   , _ = require('lodash');
-// 
-// var COLUMNS_LINE = 'Index,Timestamp,Event Type,Player Id,Event Data';
-// var COLUMNS = COLUMNS_LINE.split(',');
-// 
-// function Parser(options) {
-//   options = options || {};
-//   // Parser expects objects coming in, and will emit objects going out
-//   options.objectMode = true;
-// 
-//   Transform.call(this, options);
-// 
-//   this._rawHeader = [];
-//   this.header = null;
-// }
-// 
-// // Parser is a `Transform` stream (readable and writable)
-// // Pipe data through it and get parsed data out of it
-// util.inherits(Parser, Transform);
-// 
-// Parser.prototype._transform = function(data, encoding, done) {
-//   if (!this.header) {
-//     this._rawHeader.push(data);
-//     if (this._isHeaderEnd(data)) {
-//       this.header = this._parseRawHeader(this._rawHeader);
-//       // Let the world know we are done parsing the header
-//       this.emit('header', this.header);
-//       this.push({header: this.header});
-//     }
-//   }
-//   // After parsing the header, push data rows
-//   else {
-//     this.push(this._parseRow(data));
-//   }
-//   done();
-// };
-// 
-// // Test if a line is the last header item
-// Parser.prototype._isHeaderEnd = function(data) {
-//   return data.join(',') === COLUMNS_LINE;
-// };
-// 
-// // Make header lines one pretty object
-// Parser.prototype._parseRawHeader = function(rawHeader) {
-//   var header = {}
-//     , self = this;
-//   _.forEach(rawHeader, function(row) {
-//     var parsedHeaderRow = self._parseHeaderRow(row);
-//     // Players are added to an array
-//     if (parsedHeaderRow['Player']) {
-//       if (!header['Players']) header['Players'] = [];
-//       header['Players'].push(parsedHeaderRow['Player']);
-//     }
-//     // The rest is just added to the header object
-//     else {
-//       _.extend(header, parsedHeaderRow);
-//     }
-//   });
-//   return header;
-// };
-// 
-// Parser.prototype._parseHeaderRow = function(row) {
-//   var result = {}
-//     , key = row[0];
-//   if (key.match(/^Game Export/)) {
-//     result['Title'] = key;
-//   }
-//   else if (key === 'Player' || key === 'Map') {
-//     result[key] = {
-//       'id': row[1],
-//       'name': row[2]
-//     };
-//   }
-//   else if (key === 'Time Range') {
-//     result[key] = {
-//       'start': row[1],
-//       'end': row[2]
-//     };
-//   }
-//   // Indicates column names line
-//   else if (key === 'Index') {
-//     result['Columns'] = row;
-//   }
-//   // Default behavior
-//   else {
-//     result[key] = row[1];
-//   }
-//   return result;
-// };
-// 
-// // Parse a data row into an object
-// Parser.prototype._parseRow = function(row) {
-//   var result = _.zipObject(COLUMNS, row);
-//   // "Expand" event data column
-//   var eventData = result['Event Data'];
-//   result['Event Data'] = {};
-//   eventData = eventData.split(', ');
-//   _.forEach(eventData, function(item) {
-//     item = item.split('=');
-//     result['Event Data'][item[0]] = item[1];
-//   });
-//   return result;
-// };
-// 
-// module.exports = function(options) {
-//   return new Parser(options);
-// };
-var Transform = require('stream').Transform
-  , csv = require('csv-streamify')
-  , JSONStream = require('JSONStream');
+/**
+* Read csv into an object
+* get field/column names and info into mixs_field_description
+* Put data into required_metadata_info
+* create a custom metadata table, using dataset_id, mixs_field_description and the additional columns info
+* The object should have methods:
+* get required field names from db
+* get custom field names from csv
+* put custom field names into db
+* create a custom table
+* put required info in db
+* put custom info in db
+*/
 
-var csvToJson = csv({objectMode: true});
+// var csv = require('csv');
+var fs = require('fs');
+var parse = require('csv-parse');
+var transform = require('stream-transform');
 
-var parser = new Transform({objectMode: true});
-// parser._transform = function(data, encoding, done) {
-//   this.push(data);
-//   done();
-// };
+var output = [];
+var parser = parse({delimiter: ','})
+var input = fs.createReadStream('./data/KCK_LSM_Bv6_qii.csv');
+var transformer = transform(function(record, callback){
+  setTimeout(function(){
+    callback(null, record.join(' ')+'\n');
+  }, 500);
+}, {parallel: 10});
+input.pipe(parser).pipe(transformer).pipe(process.stdout);
 
-parser.header = null;
-parser._rawHeader = [];
-parser._transform = function(data, encoding, done) {
-  if (!this.header) {
-    this._rawHeader.push(data);
-    if (data[0] === 'sample_name') {
-      // We reached the last line of the header
-      this.header = this._rawHeader;
-      this.push({header: this.header});
-    }
-  }
-  // After parsing the header, push data rows
-  else {
-    this.push({row: data});
-  }
-  done();
-};
-
-
-var jsonToStrings = JSONStream.stringify(false);
-
-process.stdin
-.pipe(csvToJson)
-.pipe(parser)
-.pipe(jsonToStrings)
-.pipe(process.stdout);
-
-
-// ====
-// // var csv = require('ya-csv');
+// var csv = require('ya-csv');
 // 
 // 
 // // make filePath a parameter, and use a callback function
