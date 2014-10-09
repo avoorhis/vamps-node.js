e8fc26ea19f6486580c9cc1a226fdbc15389d36e
annaship <ashipunova@as-macbook.home>
2014-10-09 11:58:10 -0400

    met data upload, get headers
    Former-commit-id: 6290537fc5127d08b0e2b008128009e3f995b8b0

--- sbin/metadata_upload.js
+++ sbin/metadata_upload.js
@@ -14,126 +14,37 @@
 * put custom info in db
 */
 
-// var csv = require('csv');
 var fs = require('fs');
-var parse = require('csv-parse');
-var transform = require('stream-transform');
 
-var output = [];
-var parser = parse({delimiter: ','})
-var input = fs.createReadStream('./data/KCK_LSM_Bv6_qii.csv');
-var transformer = transform(function(record, callback){
-  setTimeout(function(){
-    callback(null, record.join(' ')+'\n');
-  }, 500);
-}, {parallel: 10});
-input.pipe(parser).pipe(transformer).pipe(process.stdout);
+function get_line(filename, line_no, callback) {
+    var data = fs.readFileSync(filename, 'utf8');
+    var lines = data.split("\n");
 
-// var csv = require('ya-csv');
-// 
-// 
-// // make filePath a parameter, and use a callback function
-// function loadCsv(filePath, callback) {
-//   
-//   // make csv_data scoped to readFile()
-//   var csv_data = [];
-//   var IN = require('ya-csv');
-//   var reader = IN.createCsvFileReader(filePath, {
-//     separator: ',', // quotes around property name are optional
-//     columnsFromHeader: true   
-//     });
-// 
-//   // data is emitted for each processed line
-//   reader.on('data', function(item) {
-//     // console.log('777');
-//     
-//     // closure magic: csv_data is accessible because current function is nested into readFile()
-//     get_headers(JSON.stringify(item));
-//     csv_data.push(item);
-//   });
-// 
-//   // end event
-//   reader.on('end', function() {
-//     // return results to caller, simply by invoking the callback.
-//     // by convention, first argument is an error, which is null it no problem occured
-//     // console.log('888');
-//     // console.log('csv_data1:');
-//     // console.log(csv_data);
-//     // return csv_data;    
-//     callback(null, csv_data);
-//     // return csv_data;    
-//   });
-// 
-//   // error handling
-//   reader.on('error', function(err) {
-//     // stop listening on events, to avoid continuing queuing data
-//     // console.log('999');
-//     reader.removeAllListeners();
-//     // report to caller the error.
-//     // console.log('000');
-//     callback(err);
-//   });
-//   
-//   // console.log('csv_data2:');
-//   // console.log(csv_data);
-//   // console.log('reader:');
-//   // console.log(reader);
-// }
-// 
-// function do_smth_w_csv_content(err, results) {
-//   if (err) {
-//     // error handling
-//     // return ...
-//   }
-//   // nominal case: use results that contains csv_data !
-//   headers = results[0];
-//   headers1 = JSON.stringify(headers);
-//   console.log("URA 111:");
-//   console.log(util.inspect(headers, false, null)); 
-//   console.log("URA 12:");
-//   console.log(util.inspect(headers1, false, null)); 
-//   // console.log(results);
-//   console.log('headers1');
-//   console.log(typeof headers1);
-//   // console.log(headers1.keys());
-// }
-// 
-// var get_headers = function(item)
-// {
-//   // console.log('get_headers item:');
-//   // if (!item.hasOwnProperty('sample_name')) {
-//   //     console.log('sample_name property is missing');
-//   //     
-//   // }
-//   // else
-//   // {
-//     var pr = item;
-//     // var pr = item.keys();
-//     console.log('get_headers');
-//     console.log(pr);
-//     
-//   // }
-//   
-// }
-// 
-// // module.exports = loadCsv;
-// // Public
-// module.exports = csvUpload;
-// var util = require("util");
-// function csvUpload(csv_filename) {
-// 
-//   loadCsv(csv_filename, do_smth_w_csv_content);
-//   
-//   
-// }
-// 
-// 
-// // csvUpload.prototype.make_dict = function(tree_obj, key_name) 
-// // {
-// //   var i = null;
-// //   new_dict = {};
-// //   for (i = 0; tree_obj.length > i; i += 1) {
-// //     new_dict[tree_obj[i][key_name]] = tree_obj[i];
-// //   }
-// //   return new_dict;
-// // };
+    if(+line_no > lines.length){
+      throw new Error('File end reached without finding line');
+    }
+
+    return callback(null, lines[+line_no]);
+}
+
+var headers = get_line('./data/KCK_LSM_Bv6_qii.csv', 0, function(err, line){
+  // console.log('The line: ' + line);
+  return line;
+})
+
+console.log('The headers: ' + typeof headers);
+
+var headers_arr = headers.split(",");
+console.log('The headers_arr: ');
+console.log(headers_arr);
+
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+
+required_fields = ["sample_name", "ANONYMIZED_NAME", "DESCRIPTION", "TAXON_ID", "common_name", "TITLE", "altitude", "assigned_from_geo", "collection_date", "collection_time", "depth", "country", "elevation", "env_biome", "env_feature", "env_matter", "latitude", "longitude", "temp", "salinity", "diss_oxygen", "public"];
+
+console.log(headers_arr.diff( required_fields ));  
+// => [1, 2, 6]
+
+// node sbin/metadata_upload.js
