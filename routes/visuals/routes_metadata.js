c3cf5b18cdab9b9c4aec101416aecb2dd2e93159
Andrew Voorhis <avoorhis@mbl.edu>
2014-10-09 14:44:55 -0400

    pcoa
    Former-commit-id: c481a9ea2002955dcec8608dba5e9b3a1cd34abe [formerly ad1d5226df37b06c35f1db3b6f4b7ed02c46e6b4]
    
    Former-commit-id: 3382168dc6f0a9768e4a90a6da1bc99828e2c651

--- routes/visuals/routes_metadata.js
+++ routes/visuals/routes_metadata.js
@@ -8,19 +8,31 @@ module.exports = {
 
 		write_metadata_file: function(chosen_id_name_hash, post_items, sqlrows) {
 			console.log('in metadata')
-			var txt = "project_dataset\tproject\tdataset\n";
+			var test_metadata = ['blue_group','red_group'];
+			//var txt = "project_dataset\tproject\tdataset\n";
+			var txt = "project_dataset\tproject\tdataset\ttest_color_grouping\n";
 			var meta_file = '../../tmp/'+post_items.ts+'_metadata.txt';
 			var metadata = [];
+
 			for(i in chosen_id_name_hash.names) {
 				console.log(chosen_id_name_hash.names[i]);
 				var ds = chosen_id_name_hash.names[i];
 				var tmp = ds.split('--');
-				txt += ds + "\t" + tmp[0] + "\t" + tmp[1] + "\n";  // just put project and dataset in here for now
-				metadata.push({'project_dataset':ds,'project':tmp[0],'dataset':tmp[1]});
+				if(i % 2 === 0){
+					var c = test_metadata[0]
+				}else{
+					var c = test_metadata[1]
+				}
+				txt += ds + "\t" + tmp[0] + "\t" + tmp[1] +"\t"+c+ "\n";  // just put project and dataset in here for now				
+				metadata.push({'project_dataset':ds,'project':tmp[0],'dataset':tmp[1],'test_color_group':c});
+
+				//txt += ds + "\t" + tmp[0] + "\t" + tmp[1] + "\n";  // just put project and dataset in here for now				
+				//metadata.push({'project_dataset':ds,'project':tmp[0],'dataset':tmp[1]});
+
 			}
-			console.log('Writing metadata file');
- 			//COMMON.write_file( matrix_file, JSON.stringify(biome_matrix) );
- 			COMMON.write_file( meta_file, txt );
+			//console.log('Writing metadata file');
+ 			
+ 			//COMMON.write_file( meta_file, txt );
  			return metadata
 		}
 
