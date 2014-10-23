e4b1961f2a1f14a6fbc1eb02fa9a2d194d058204
annaship <tetya-loshad@yandex.ru>
2014-10-23 12:32:40 -0400

    csv metadata db
    Former-commit-id: 2804a719460a1179131c98f217bee3023107e64a

---
+++ models/csv_metadata_upload.js
@@ -0,0 +1,33 @@
+/*jshint multistr: true */
+
+/*
+1) get dataset_id
+2) add dataset_id, field_name, field_type, example into custom_metadata_fields
+3) 
+
+*/
+var taxa_query = "SELECT DISTINCT domain, phylum, klass, `order`, family, genus, species, strain \
+ , domain_id, phylum_id, klass_id, order_id, family_id, genus_id, species_id, strain_id \
+ FROM silva_taxonomy \
+ JOIN domain AS dom USING(domain_id) \
+ JOIN phylum AS phy USING(phylum_id) \
+ JOIN klass AS kla USING(klass_id) \
+ JOIN `order` AS ord USING(order_id) \
+ JOIN family AS fam USING(family_id) \
+ JOIN genus AS gen USING(genus_id) \
+ JOIN species AS spe USING(species_id) \
+ JOIN strain AS str USING(strain_id)";
+ console.log('running custom tax query short-2');
+
+module.exports = silvaTaxonomy;
+
+function silvaTaxonomy() {
+}
+
+silvaTaxonomy.prototype.get_all_taxa = function(callback) 
+{
+  connection.query(taxa_query, function (err, rows, fields) {
+    callback(err, rows);
+  });
+};
+
