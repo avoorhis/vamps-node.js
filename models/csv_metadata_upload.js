6e57bb2e4cedd126526fb893eb789533a26264b6
annaship <ashipunova@as-macbook.home>
2014-10-23 15:43:18 -0400

    metadata cdv: make_insert_custom_field_names_query

--- models/csv_metadata_upload.js
+++ models/csv_metadata_upload.js
@@ -9,16 +9,30 @@
 
 function make_dataset_id_query(project, datasets)
 {
-  var get_dataset_id_query = "SELECT DISTINCT dataset, dataset_id \
+  var get_dataset_id_query = "SELECT DISTINCT project, dataset, dataset_id \
     FROM dataset \
     JOIN project USING(project_id) \
     WHERE dataset in (" + datasets + ") \
     AND project = '" + project + "' \
   ";
-   console.log('running get_dataset_id_query:');
+   console.log('get_dataset_id_query:');
    console.log(get_dataset_id_query);
    return get_dataset_id_query;
 }
+
+function make_insert_custom_field_names_query()
+{
+  var insert_custom_field_names_query = "";
+  // "INSERT IGNORE INTO custom_metadata_fields (dataset_id, field_name, example) 
+  // VALUES ()
+  // ";
+  // console.log('insert_custom_field_names_query:');
+  // console.log(insert_custom_field_names_query);
+  // 
+  // return insert_custom_field_names_query;
+}
+
+// public
 
 module.exports = csvMetadataUpload;
 
