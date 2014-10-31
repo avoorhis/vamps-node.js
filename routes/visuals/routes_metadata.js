f4dbda4a150da4fba20a77e68bc9a8f63ddc7e1d
Andrew Voorhis <avoorhis@mbl.edu>
2014-10-31 13:17:36 -0400

    metadata
    Former-commit-id: 942febf542490ed5a51bbd494dd824c3b07bb6a7 [formerly 4dbc09e0c0b61965c122cc544f4743e3cc96cdf9]
    
    Former-commit-id: df5bc448efc557595c85423fab472fa26ce5555c

--- routes/visuals/routes_metadata.js
+++ routes/visuals/routes_metadata.js
@@ -3,37 +3,66 @@ var fs = require('fs');
 var COMMON  = require('./routes_common');
 
 
+// module.exports = {
+
+
+// 		write_metadata_file: function(chosen_id_name_hash, post_items) {
+// 			//console.log('in metadata')
+// 			var metadata_names = post_items.metadata;
+// 			//var txt = "project_dataset\tproject\tdataset\n";
+// 			var txt = "project_dataset\tproject\tdataset\ttest_color_grouping\n";
+// 			//var meta_file = '../../tmp/'+post_items.ts+'_metadata.txt';
+// 			var metadata = [];
+
+// 			for(i in chosen_id_name_hash.names) {
+// 				var ds = chosen_id_name_hash.names[i];
+// 				var tmp = ds.split('--');
+// 				if(i % 2 === 0){
+// 					var c = test_metadata[0]
+// 				}else{
+// 					var c = test_metadata[1]
+// 				}
+// 				//txt += ds + "\t" + tmp[0] + "\t" + tmp[1] +"\t"+c+ "\n";  // just put project and dataset in here for now				
+// 				metadata.push({'test_color_group':c,'project_dataset':ds,'project':tmp[0],'dataset':tmp[1]});
+
+// 			}
+// 			//console.log('Writing metadata file');
+ 			
+//  			//COMMON.write_file( meta_file, txt );
+//  			return metadata
+// 		}
+
+// }
+
 module.exports = {
 
 
 		write_metadata_file: function(chosen_id_name_hash, post_items) {
 			//console.log('in metadata')
-			var test_metadata = ['horse','donkey'];
-			//var txt = "project_dataset\tproject\tdataset\n";
-			var txt = "project_dataset\tproject\tdataset\ttest_color_grouping\n";
-			var meta_file = '../../tmp/'+post_items.ts+'_metadata.txt';
+			var metadata_names = post_items.metadata;
+			
 			var metadata = [];
 
 			for(i in chosen_id_name_hash.names) {
-				var ds = chosen_id_name_hash.names[i];
-				var tmp = ds.split('--');
-				if(i % 2 === 0){
-					var c = test_metadata[0]
-				}else{
-					var c = test_metadata[1]
+				var line = {};
+				var pjds = chosen_id_name_hash.names[i];
+				var tmp = pjds.split('--');
+				var did = chosen_id_name_hash.ids[i];
+				
+				for(n in metadata_names) {				
+					var name = metadata_names[n];
+					line[name] = MetadataValues[did][name];						
 				}
-				txt += ds + "\t" + tmp[0] + "\t" + tmp[1] +"\t"+c+ "\n";  // just put project and dataset in here for now				
-				metadata.push({'test_color_group':c,'project_dataset':ds,'project':tmp[0],'dataset':tmp[1]});
-
+				line.project_dataset = pjds;
+				line.project = tmp[0];
+				line.dataset = tmp[1]
+				metadata.push(line);
 				//txt += ds + "\t" + tmp[0] + "\t" + tmp[1] + "\n";  // just put project and dataset in here for now				
 				//metadata.push({'project_dataset':ds,'project':tmp[0],'dataset':tmp[1]});
-
+				
 			}
-			//console.log('Writing metadata file');
- 			
- 			//COMMON.write_file( meta_file, txt );
- 			return metadata
+			return metadata
+
+
 		}
-
-
 }
