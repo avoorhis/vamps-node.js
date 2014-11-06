146f159b3dfdbef48cf92b1b2bb425231289acc1
annaship <ashipunova@as-macbook.home>
2014-11-06 16:44:10 -0500

    metadata test, intermediate
    Former-commit-id: a56b9194155bc632e949cb4c59f76a2bdb427471 [formerly e6b68dd433d66e8f1386d70868e25d038ad37d6e]
    Former-commit-id: b76ae777e1d60c233f41a3c7e9c69e39ea16f682

--- test/upload_csvSpec.js
+++ test/upload_csvSpec.js
@@ -33,20 +33,31 @@ before(function() {
   console.log('before every test');
 });
 
-// describe('csv_metadata_model', function(){
-// 
-//   it('get_dataset_ids', function (done) {
-// 
-//       this.timeout(5000);
-//       async.series([
-//         function (cb) {
-//           connection.query('SELECT * FROM user WHERE username="TEST"'+
-//                 ' AND email="TEST"',function(err,results){
-//               results.length.should.not.equal(0);
-//               done();
-//             });
-//         }
-//       ], done);
-//     });
-// 
-// });
+describe('csv_metadata_model', function(){
+
+  it('get_dataset_ids', function (done) {
+    project = "KCK_LSM_Bv6";
+    datasets = "'071007st5b', 'LSM_0008_031808st6', 'LSM_0002_090407st6', '061307st4a', 'LSM_0004_110707st6', 'LSM_0005_010808st4', 'LSM_0001_090407st4', '081407st4b', '082107st7b', '061907st2', 'LSM_0006_010808st6', '080707st5b', '061907st4', '071007st6b', 'LSM_0007_031808st4', 'LSM_0003_110707st4', '082107st4', '080707st6b', '071007st4', '071007st3', '071007st2', '080707st3', '080707st4b', '010808st1', '080707st2b', '080707st7', '082107st6c', '082107st5', '082107st2', '082107st3'"
+
+    csv_metadata_db.get_dataset_ids(project, datasets, function work_with_dataset_id(err, results)
+    {
+      if (err)
+        throw err; // or return an error message, or something
+      else
+      {
+        console.log(results)
+      }
+
+      // this.timeout(5000);
+      // async.series([
+      //   function (cb) {
+      //     connection.query('SELECT * FROM user WHERE username="TEST"'+
+      //           ' AND email="TEST"',function(err,results){
+      //         results.length.should.not.equal(0);
+      //         done();
+      //       });
+      //   }
+      // ], done);
+    });
+
+});
