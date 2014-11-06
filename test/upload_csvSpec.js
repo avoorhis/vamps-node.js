2afd49cdfab3f491ec8b8edeb49815780acd1a92
annaship <ashipunova@as-macbook.home>
2014-11-06 17:22:46 -0500

    metadata csv test
    Former-commit-id: fde528d5948835d249433a8715d8fc0c723a8cb6

--- test/upload_csvSpec.js
+++ test/upload_csvSpec.js
@@ -39,25 +39,42 @@ describe('csv_metadata_model', function(){
     project = "KCK_LSM_Bv6";
     datasets = "'071007st5b', 'LSM_0008_031808st6', 'LSM_0002_090407st6', '061307st4a', 'LSM_0004_110707st6', 'LSM_0005_010808st4', 'LSM_0001_090407st4', '081407st4b', '082107st7b', '061907st2', 'LSM_0006_010808st6', '080707st5b', '061907st4', '071007st6b', 'LSM_0007_031808st4', 'LSM_0003_110707st4', '082107st4', '080707st6b', '071007st4', '071007st3', '071007st2', '080707st3', '080707st4b', '010808st1', '080707st2b', '080707st7', '082107st6c', '082107st5', '082107st2', '082107st3'"
 
-    csv_metadata_db.get_dataset_ids(project, datasets, function work_with_dataset_id(err, results)
-    {
-      if (err)
-        throw err; // or return an error message, or something
-      else
+    this.timeout(5000);
+    async.series([
+      function (cb) 
       {
-        console.log(results)
+        csv_metadata_db.get_dataset_ids(project, datasets, function work_with_dataset_id(err, results)
+        {
+          if (err)
+            throw err; // or return an error message, or something
+          else
+          {
+            console.log("RRR results")
+            console.log(results[0].project)
+            console.log(results.length)
+            results.length.should.equal(29);
+            results[0].project.should.equal("KCK_LSM_Bv6");
+          }
+          done();
+        // connection.query('SELECT * FROM user WHERE username="TEST"'+
+        //       ' AND email="TEST"', function(err,results){
+        //     results.length.should.not.equal(0);
+        //     done();
+        //   });
+        });
       }
+    ], done);
 
-      // this.timeout(5000);
-      // async.series([
-      //   function (cb) {
-      //     connection.query('SELECT * FROM user WHERE username="TEST"'+
-      //           ' AND email="TEST"',function(err,results){
-      //         results.length.should.not.equal(0);
-      //         done();
-      //       });
-      //   }
-      // ], done);
-    });
+    // csv_metadata_db.get_dataset_ids(project, datasets, function work_with_dataset_id(err, results)
+    // {
+    //   if (err)
+    //     throw err; // or return an error message, or something
+    //   else
+    //   {
+    //     console.log("RRR results")
+    //     console.log(results)
+    //   }
 
+    // });
+  });
 });
