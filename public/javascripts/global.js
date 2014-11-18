93b780d4a17c23ea966fcbb9941c302bb2e95e58
Andrew Voorhis <avoorhis@mbl.edu>
2014-11-18 16:36:42 -0500

    client side images
    Former-commit-id: 325d95b0bd5b2583fb4d991fb100aa6b22618f3e

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,11 +1,11 @@
 // global.js
 
-$(document).ready(function(){
-    $('a.back').click(function(){
-        parent.history.back();
-        return false;
-    });
-});
+// $(document).ready(function(){
+//     $('a.back').click(function(){
+//         parent.history.back();
+//         return false;
+//     });
+// });
 
 function is_checked(id_names)
 {
@@ -31,24 +31,7 @@ function show_post_array(body) {
   //}
 }
 
-//
-// view_selection page
-//
-$(document).ready(function(){
-    $('#download_fasta').click(function(){
-        alert('Not Attached yet')
-        return false;
-    });
-});
-//
-// Heatmap page
-//
-$(document).ready(function(){
-    $('#ds_save_order_btn').click(function(){
-        alert('Not Attached yet')
-        return false;
-    });
-});
+
 // $(document).ready(function(){
 //     $('#save_order_btn').click(function(){
         
