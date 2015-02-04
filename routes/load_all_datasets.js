28d55cf03a342f203e051ce15bca81203e39bb13
Andrew Voorhis <avoorhis@mbl.edu>
2015-02-04 12:34:00 -0500

    download and delete fasta

--- routes/load_all_datasets.js
+++ routes/load_all_datasets.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //
 //
 //
-var qSelectDatasets = "SELECT project, dataset_id as did, dataset";
+var qSelectDatasets = "SELECT project, title, dataset_id as did, project_id as pid, dataset, dataset_description";
 qSelectDatasets += " FROM dataset";
 qSelectDatasets += " JOIN project USING(project_id)";
 qSelectDatasets += " ORDER BY project, dataset";
@@ -13,6 +13,8 @@ qSelectDatasets += " ORDER BY project, dataset";
 module.exports.get_datasets = function(callback){
   connection.query(qSelectDatasets, function(err, rows, fields){
     var ALL_DATASETS = {};
+    var pids         = {};
+    var titles       = {};
 
       if (err)  {
         throw err;
@@ -24,13 +26,17 @@ module.exports.get_datasets = function(callback){
 
         for (var i=0; i < rows.length; i++) {
           var project = rows[i].project;
+          
           var did = rows[i].did;
+          pids[project] = rows[i].pid;
+          titles[project] = rows[i].title;
           var dataset = rows[i].dataset;
+          var dataset_description = rows[i].dataset_description;
           if (project === undefined){ continue; }
           if (project in datasetsByProject){
-              datasetsByProject[project].push({ did:did, dname:dataset });
+              datasetsByProject[project].push({ did:did, dname:dataset, ddesc: dataset_description});
           } else {
-              datasetsByProject[project] = [{ did:did, dname:dataset }];
+              datasetsByProject[project] = [{ did:did, dname:dataset, ddesc: dataset_description }];
           }
         }
 
@@ -38,11 +44,14 @@ module.exports.get_datasets = function(callback){
         for (var p in datasetsByProject){
           var tmp = {};
           tmp.name = p;
+          tmp.pid = pids[p];
+          tmp.title = titles[p];
           tmp.datasets = [];
           for (var d in datasetsByProject[p]){
             var ds = datasetsByProject[p][d].dname;
-            var dp_did = datasetsByProject[p][d].did;
-            tmp.datasets.push({ did: dp_did, dname:ds });
+            var dp_did = datasetsByProject[p][d].did;  
+            var ddesc = datasetsByProject[p][d].ddesc; 
+            tmp.datasets.push({ did:dp_did, dname:ds, ddesc:ddesc });
           }
           ALL_DATASETS.projects.push(tmp);
         }
