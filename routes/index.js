6863dbaffb07c47781f1de869632d72ea80a16a2
Andrew Voorhis <avoorhis@mbl.edu>
2015-02-04 12:34:00 -0500

    download and delete fasta
    Former-commit-id: 172595b6f5acf49ef9c55c752c0f73d511e667e2 [formerly 28d55cf03a342f203e051ce15bca81203e39bb13]
    Former-commit-id: a5413e5f3b8338cf9bcc217b051bf73d921af16a

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var fs   = require('fs');
 
 var ds = require('./load_all_datasets');
 var rs = ds.get_datasets(function(ALL_DATASETS){
@@ -8,24 +8,87 @@ var rs = ds.get_datasets(function(ALL_DATASETS){
 
   /* GET home page. */
   router.get('/', function(req, res) {
-    res.render('index', { title: 'VAMPS Home Page', user: req.user });
+    res.render('index', { title: 'VAMPS:Home', user: req.user });
   });
 
   /* GET Overview page. */
   router.get('/overview', function(req, res) {
-      res.render('overview', { title: 'Overview of the VAMPS Project', user: req.user });
+      res.render('overview', { title: 'VAMPS:Overview', user: req.user });
   });
 
   /* GET Search page. */
   router.get('/search', function(req, res) {
-      res.render('search', { title: 'Search VAMPS datasets', 
+      res.render('search', { title: 'VAMPS:Search', 
       											 user: req.user 
       											});
   });
 
+  /* GET Import Data page. */
+  router.get('/import_data', function(req, res) {
+      res.render('import_data', { title: 'VAMPS:Import Data', 
+                             user: req.user 
+                            });
+  });
 
+  /* GET Export Data page. */
+  router.get('/export_data', function(req, res) {
+      
+      var user = req.user || 'no-user';  
+      var export_dir = 'downloads';
+      var mtime = {};
+      var size = {};
+      fs.readdir(export_dir, function(err, files){   
+        
+        for(f in files){
+          
+          stat = fs.statSync(export_dir+'/'+files[f]);
+          mtime[files[f]] = stat.mtime;  // modify time
+          size[files[f]] = stat.size;
+          console.log(stat.mtime);
+        }
+        
+        res.render('export_data', { title: 'VAMPS:Export Data', 
+                             user: user,
+                             files:files,
+                             mtime:mtime,
+                             size:size
+                            });
+      });
+  });
 
+  router.get('/file_utils', function(req, res){
 
+    console.log('dnld')
+    console.log(req.query.filename)
+    var file = 'downloads/'+req.query.filename;
+    if(req.query.fxn == 'download'){
+      res.download(file); // Set disposition and send it.
+    }else if(req.query.fxn == 'delete'){
+      fs.unlink(file); // 
+      res.redirect("/export_data")
+    }
+  });
+
+  /* GET Geo-Distribution page. */
+  router.get('/geodistribution', function(req, res) {
+      res.render('geodistribution', { title: 'VAMPS:Geo_Distribution', 
+                             user: req.user 
+                            });
+  });
+
+  /* GET metadata page. */
+  router.get('/metadata', function(req, res) {
+      res.render('metadata', { title: 'VAMPS:Metadata', 
+                             user: req.user 
+                            });
+  });
+
+  /* GET Portals page. */
+  router.get('/portals', function(req, res) {
+      res.render('portals', { title: 'VAMPS:Portals', 
+                             user: req.user 
+                            });
+  });
 
 });
 
