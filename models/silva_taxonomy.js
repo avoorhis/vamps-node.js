6075387a1fda4cfb3471dcdf3328278b1de1d1d4
annaship <tetya-loshad@yandex.ru>
2015-03-23 14:04:43 -0400

    taxa count - intermediate
    Former-commit-id: c0b6f41c2bf7983cd2e7be7d98230fe45579d46d

--- models/silva_taxonomy.js
+++ models/silva_taxonomy.js
@@ -12,6 +12,27 @@ var taxa_query = "SELECT DISTINCT domain, phylum, klass, `order`, family, genus,
  JOIN species AS spe USING(species_id) \
  JOIN strain AS str USING(strain_id)";
  console.log('running custom tax query short-2');
+ 
+var dataset_taxa_counts = " \
+  SELECT distinct dataset_id, \
+    sequence_pdr_info_id, \
+    sequence_id, \
+    seq_count, \
+    silva_taxonomy_id, \
+    domain_id, \
+    phylum_id, \
+    klass_id, \
+    order_id, \
+    family_id, \
+    genus_id, \
+    species_id, \
+    strain_id \
+  FROM sequence_pdr_info \
+  JOIN sequence_uniq_info USING(sequence_id) \
+  JOIN silva_taxonomy_info_per_seq USING(silva_taxonomy_info_per_seq_id, sequence_id) \
+  JOIN silva_taxonomy USING(silva_taxonomy_id) \
+";
+console.log('running dataset_taxa_counts query');
 
 module.exports = silvaTaxonomy;
 
@@ -25,3 +46,9 @@ silvaTaxonomy.prototype.get_all_taxa = function(callback)
   });
 };
 
+silvaTaxonomy.prototype.get_dataset_taxa_counts = function(callback) 
+{
+  connection.db.query(dataset_taxa_counts, function (err, rows, fields) {
+    callback(err, rows);
+  });
+};
