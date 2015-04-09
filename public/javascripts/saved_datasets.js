f7c182bf1913cf5500491aa51febd5848fe77d2e
Andrew Voorhis <avoorhis@mbl.edu>
2015-04-09 15:52:19 -0400

    same samples2

--- public/javascripts/saved_datasets.js
+++ public/javascripts/saved_datasets.js
@@ -3,6 +3,44 @@ var view_array = document.getElementsByName('view_array[]');
 //alert(view_array[0].value)
 for(i = 0; i < view_array.length; i++){
 	val = view_array[i];
-	alert(val)
+	//alert(val)
 
 }
+
+function sendAjaxUseOrView( filename, user, fxn ){
+	
+        
+		args ='filename='+filename;
+		args += '&user='+user;
+		args += '&fxn='+fxn;
+		var xmlhttp = new XMLHttpRequest();
+	    xmlhttp.open("POST", "/visuals/useview_saved_datasets", true);
+		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	    xmlhttp.onreadystatechange = function() {
+
+	       if (xmlhttp.readyState == 4 ) {
+				var data = xmlhttp.responseText;
+				obj = JSON.parse(data);
+				dataset_ids = obj.ids;
+				var html = "<div class='auto_height200 border1'>"
+				html += "<form id='' name='' method='POST' action='unit_selection'>"
+				html += "<input type='submit' value='use in visualizations' />"
+				html += "<input type='hidden' name='search' value='1' >"
+				html += "<input type='hidden' name='dataset_ids' value='"+ JSON.stringify(dataset_ids)+"' >"
+				html += "</form>"
+				html += "<table class='small_font'>";
+			 	html += '<tr><th>SampleID</th><th>project--dataset</th></tr>';
+ 		
+ 			for( var i in obj.ids ){
+ 				var id =  obj.ids[i];
+ 				var dsname = obj.names[i];
+ 				html += '<tr><td>'+id+'</td><td>'+dsname+'</td></tr>';
+		
+ 			}
+ 			html += '</table></div>';
+			 document.getElementById(filename+"_div").innerHTML=html;
+	       }
+	    };
+	    xmlhttp.send(args);
+}
+
