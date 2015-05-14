486898c14afaa45b916deba1a0928f18f19f9bb8
Andrew Voorhis <avoorhis@mbl.edu>
2015-05-14 16:15:45 -0400

    clean
    Former-commit-id: d33499e5f0a1971cf264b02be3913d03e35e7794

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -198,7 +198,9 @@ if (typeof metadata_search_field3 !=="undefined") {
       }
   });
 }
-
+//
+//
+//
 function change_comparison(comparison, item){
     var comparison_input;  
     var minval;
@@ -232,4 +234,22 @@ function change_comparison(comparison, item){
     comparison_input.innerHTML = html;
   }
   
+}
+//
+//
+//
+function showMetadataHint(str) {
+    if (str.length == 0) {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET", "gethint/" + str, true);
+        xmlhttp.send();
+    }
 }
