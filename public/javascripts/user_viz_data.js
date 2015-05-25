e58026c5c0dabb5e87129c040bc8cfe03fd5e8e5
Andrew Voorhis <avoorhis@mbl.edu>
2015-05-25 07:53:02 -0400

    single bc formating

--- public/javascripts/user_viz_data.js
+++ public/javascripts/user_viz_data.js
@@ -100,68 +100,68 @@ $("body").delegate(".tooltipx", "mouseover mouseout mousemove", function (event)
 
 $(document).ready(function() {  
 
-	    var ts = pi_local.ts;
-		
+	    
+		create_barcharts('single')
         //var info_line = create_header('bars', pi_local);
-		var barcharts_div = document.getElementById('barcharts_div');
-		barcharts_div.style.display = 'block';
-        barcharts_table_div.innerHTML = get_single_bar_html(mtx_local);
-        //document.getElementById('pre_barcharts_table_div').style.display = 'block';
-
-
-        data = [];
-        // for (var p in mtx_local.columns){
-           tmp={};
-           tmp.datasetName = mtx_local.dataset;
-		   //tmp.alltotal = mtx_local.total
-           for (var t in mtx_local.rows){
-             tmp[mtx_local.rows[t]] = mtx_local.data[t];
-           }
-           data.push(tmp);
-         
-
-      
-        var unit_list = mtx_local.rows;
-       
-        // for (var o in mtx_local.rows){
-      //     unit_list.push(mtx_local.rows[o].name);
-      //   }
-        
-
-        var ds_count = 1;      
-        var bar_height = 20;
-        var props = get_image_properties(bar_height, ds_count); 
-        //console.log(props)
-        var color = d3.scale.ordinal()                  
-          .range( get_colors(unit_list) );
-
-        color.domain(d3.keys(data[0]).filter(function(key) { return key !== "datasetName"; }));
-
-        
-
-        data.forEach(function(d) {
-          var x0 = 0;
-          d.unitObj = color.domain().map(function(name) { 
-            return { name: name, x0: x0, x1: x0 += +d[name] }; 
-          });
-          
-          d.total = d.unitObj[d.unitObj.length - 1].x1;
-          //console.log(d.total);
-        });
-
-
-        data.forEach(function(d) {
-          // normalize to 100%
-          tot = d.total;
-          d.unitObj.forEach(function(o) {
-              //console.log(o);
-              o.x0 = (o.x0*100)/tot;
-              o.x1 = (o.x1*100)/tot;
-          });
-        });
-      
-        
-        create_svg_object(props, color, data, ts);
+// 		var barcharts_div = document.getElementById('barcharts_div');
+// 		barcharts_div.style.display = 'block';
+//         barcharts_table_div.innerHTML = get_single_bar_html(mtx_local);
+//         //document.getElementById('pre_barcharts_table_div').style.display = 'block';
+// 
+// 
+//         data = [];
+//         // for (var p in mtx_local.columns){
+//            tmp={};
+//            tmp.datasetName = mtx_local.dataset;
+// 		   //tmp.alltotal = mtx_local.total
+//            for (var t in mtx_local.rows){
+//              tmp[mtx_local.rows[t]] = mtx_local.data[t];
+//            }
+//            data.push(tmp);
+//          
+// 
+//       
+//         var unit_list = mtx_local.rows;
+//        
+//         // for (var o in mtx_local.rows){
+//       //     unit_list.push(mtx_local.rows[o].name);
+//       //   }
+//         
+// 
+//         var ds_count = 1;      
+//         var bar_height = 20;
+//         var props = get_image_properties(bar_height, ds_count); 
+//         //console.log(props)
+//         var color = d3.scale.ordinal()                  
+//           .range( get_colors(unit_list) );
+// 
+//         color.domain(d3.keys(data[0]).filter(function(key) { return key !== "datasetName"; }));
+// 
+//         
+// 
+//         data.forEach(function(d) {
+//           var x0 = 0;
+//           d.unitObj = color.domain().map(function(name) { 
+//             return { name: name, x0: x0, x1: x0 += +d[name] }; 
+//           });
+//           
+//           d.total = d.unitObj[d.unitObj.length - 1].x1;
+//           //console.log(d.total);
+//         });
+// 
+// 
+//         data.forEach(function(d) {
+//           // normalize to 100%
+//           tot = d.total;
+//           d.unitObj.forEach(function(o) {
+//               //console.log(o);
+//               o.x0 = (o.x0*100)/tot;
+//               o.x1 = (o.x1*100)/tot;
+//           });
+//         });
+//       
+//         
+//         create_svg_object(props, color, data, ts);
         
 })
 
@@ -186,98 +186,79 @@ function get_single_bar_html(obj){
 //
 //
 //
-function create_svg_object(props, color, data, ts) {
-       //d3.select('svg').remove();
-      
-      var svg = d3.select("#barcharts_div").append("svg")
-                  .attr("width",  props.width)
-                  .attr("height", props.height)
-                .append("g")
-                  .attr("transform", "translate(" + props.margin.left + "," + props.margin.top + ")");
-      
-      
-      // axis legends -- would like to rotate dataset names
-      props.y.domain(data.map(function(d) { return d.datasetName; }));
-      props.x.domain([0, 100]);
+// function create_svg_object(props, color, data, ts) {
+//        //d3.select('svg').remove();
+//       
+//       var svg = d3.select("#barcharts_div").append("svg")
+//                   .attr("width",  props.width)
+//                   .attr("height", props.height)
+//                 .append("g")
+//                   .attr("transform", "translate(" + props.margin.left + "," + props.margin.top + ")");
+//       
+//       
+//       // axis legends -- would like to rotate dataset names
+//       props.y.domain(data.map(function(d) { return d.datasetName; }));
+//       props.x.domain([0, 100]);
+// 
+// 
+//        var datasetBar = svg.selectAll(".bar")
+//           .data(data)
+//         .enter() .append("g")
+//           .attr("class", "g")
+//           .attr("transform", function(d) { return  "translate(0, " + props.y(d.datasetName) + ")"; })  
+// 	 
+// 
+//        var gnodes = datasetBar.selectAll("rect")
+//            .data(function(d) { return d.unitObj; })
+//            .enter()
+//              .append('a').attr("xlink:href",  function(d) { 
+// 			     return 'sequences?taxa='+encodeURIComponent(d.name)+'&did='+mtx_local.did;
+// 			  }).style("fill",   function(d) { return color(d.name); });
+//           
+//        gnodes.append("rect")
+//              .attr("x", function(d) { return props.x(d.x0); })
+//              .attr("y", 15)  // adjust where first bar starts on x-axis
+//              .attr("width", function(d) { return props.x(d.x1) - props.x(d.x0); })
+//              .attr("height",  25)
+// 		     
+//              .attr("id",function(d,i) { 
+//                 var cnt =  mtx_local.data[i];
+//                 var total = mtx_local.total;
+//                 var pct = (cnt * 100 / total).toFixed(2);
+//                 var id = 'barcharts-|-' + d.name + '-|-'+ cnt.toString() + '-|-' + pct;
+//                 return id;    // ip of each rectangle should be datasetname-|-unitname-|-count
+//               }) 
+// 			  .attr("class","tooltipx");
+// 
+// 
+// }
 
+// function get_image_properties(bar_height, ds_count) {
+//   var props = {};
+//   
+//   //props.margin = {top: 20, right: 20, bottom: 300, left: 50};
+//   props.margin = {top: 20, right: 0, bottom: 20, left: 0};
+//   
+//   var plot_width = 900;
+//   var gap = 2;  // gap on each side of bar
+//   props.width = plot_width + props.margin.left + props.margin.right;
+//   props.height = (ds_count * (bar_height + 2 * gap)) + 125;
+//   
+//   props.x = d3.scale.linear() .rangeRound([0, plot_width]);
+//     
+//   props.y = d3.scale.ordinal()
+//       .rangeBands([0, (bar_height + 2 * gap) * ds_count]);
+//     
+//   props.xAxis = d3.svg.axis()
+//           .scale(props.x)
+//           .orient("top");
+//   
+//   props.yAxis = d3.svg.axis()
+//           .scale(props.y)
+//           .orient("left");
+//           
+//               
+//   return props;
+// }
 
-       var datasetBar = svg.selectAll(".bar")
-          .data(data)
-        .enter() .append("g")
-          .attr("class", "g")
-          .attr("transform", function(d) { return  "translate(0, " + props.y(d.datasetName) + ")"; })  
-	 
-
-       var gnodes = datasetBar.selectAll("rect")
-           .data(function(d) { return d.unitObj; })
-           .enter()
-             .append('a').attr("xlink:href",  function(d) { 
-			     return 'sequences?taxa='+encodeURIComponent(d.name)+'&did='+mtx_local.did;
-			  }).style("fill",   function(d) { return color(d.name); });
-          
-       gnodes.append("rect")
-             .attr("x", function(d) { return props.x(d.x0); })
-             .attr("y", 15)  // adjust where first bar starts on x-axis
-             .attr("width", function(d) { return props.x(d.x1) - props.x(d.x0); })
-             .attr("height",  25)
-		     
-             .attr("id",function(d,i) { 
-                var cnt =  mtx_local.data[i];
-                var total = mtx_local.total;
-                var pct = (cnt * 100 / total).toFixed(2);
-                var id = 'barcharts-|-' + d.name + '-|-'+ cnt.toString() + '-|-' + pct;
-                return id;    // ip of each rectangle should be datasetname-|-unitname-|-count
-              }) 
-			  .attr("class","tooltipx");
-
-
-}
-
-function get_image_properties(bar_height, ds_count) {
-  var props = {};
-  
-  //props.margin = {top: 20, right: 20, bottom: 300, left: 50};
-  props.margin = {top: 20, right: 0, bottom: 20, left: 0};
-  
-  var plot_width = 900;
-  var gap = 2;  // gap on each side of bar
-  props.width = plot_width + props.margin.left + props.margin.right;
-  props.height = (ds_count * (bar_height + 2 * gap)) + 125;
-  
-  props.x = d3.scale.linear() .rangeRound([0, plot_width]);
-    
-  props.y = d3.scale.ordinal()
-      .rangeBands([0, (bar_height + 2 * gap) * ds_count]);
-    
-  props.xAxis = d3.svg.axis()
-          .scale(props.x)
-          .orient("top");
-  
-  props.yAxis = d3.svg.axis()
-          .scale(props.y)
-          .orient("left");
-          
-              
-  return props;
-}
-
-function get_colors(unit_names){
-  var colors = [];
-  for(var n in unit_names){
-    //alert(unit_names[n]);
-    col = string_to_color_code(unit_names[n]);
-    //console.log(col);
-    colors.push(col);
-  }
-  return colors;
-}
-
-function string_to_color_code(str){
-    var hash = 0;
-    for(var i=0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 3) - hash);
-    }
-    var color = Math.abs(hash).toString(16).substring(0, 6);
-    return "#" + '000000'.substring(0, 6 - color.length) + color;
-}
 
