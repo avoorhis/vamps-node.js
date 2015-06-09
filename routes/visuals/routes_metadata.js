3aa995d4ef59aa7a759ebcf0dee5ef7e44ab8fa9
Andrew Voorhis <avoorhis@mbl.edu>
2015-06-09 10:50:58 -0400

    some cleanup

--- routes/visuals/routes_metadata.js
+++ routes/visuals/routes_metadata.js
@@ -89,7 +89,58 @@ module.exports = {
 			COMMON.write_file(metadata_filename, txt);
 			return metadata2;
 		},
+		write_mapping_file: function(chosen_id_name_hash, post_items) {
+			//console.log('in metadata')
+			var metadata_names = post_items.metadata;
+			
+			var metadata = [];
+			var metadata2 = {};
+			var file_name = post_items.ts+'_metadata.txt';
+			var metadata_filename = path.join(__dirname, '../../tmp/'+file_name);
+			var txt = "#SampleID\tBarcodeSequence\tLinkerPrimerSequence";
+			for (var n in metadata_names) {				
+					var name = metadata_names[n];
+					txt += 		"\t" + name;	
+			}
+			txt += 		"\tDescription\n";
+			var txt2 = '';
+			for (var i in chosen_id_name_hash.names) {
+				var ds_row = {};
+				var pjds = chosen_id_name_hash.names[i];
+				metadata2[pjds] = {};
+				var tmp = pjds.split('--');
+				var did = chosen_id_name_hash.ids[i];
+				txt2 = pjds + "\t\t";
+				
+				for (var n in metadata_names) {				
+					var name = metadata_names[n];
+                    
+					if(did in METADATA) {
+						ds_row[name] = METADATA[did][name];
+						metadata2[pjds][name] = METADATA[did][name];
+						txt2 += "\t" + metadata2[pjds][name];
+					}else{
+						txt2 += "\tno_value";
+					}
+				}
+				
+				console.log(txt2.length)
+				if(txt2.length > pjds.length+2){  // the +2 is to account for tabs in the txt2
+					txt += txt2 + "\t"+pjds+"\n";
+				}
+				metadata2[pjds].project = tmp[0];
+				metadata2[pjds].dataset = tmp[1];
+				
+				ds_row.project_dataset = pjds;
+				ds_row.project = tmp[0];
+				ds_row.dataset = tmp[1];
 
+				metadata.push(ds_row);
+				
+			}
+			COMMON.write_file(metadata_filename, txt);
+			return metadata2;
+		},
 		create_metadata_table: function(chosen_id_name_hash, visual_post_items) {
 				var html = "<table border='1' id='metadata_table' class='single_border center_table'>";
 				html += "<thead><tr><th>Dataset (sortable)</th><th>Name (sortable)</th><th>Value (sortable)</th></tr></thead><tbody>";
