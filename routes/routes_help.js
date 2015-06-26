6de7d8e6879b2e2689800a7c5482d282b30f5c77
Andrew Voorhis <avoorhis@mbl.edu>
2015-06-26 09:36:47 -0400

    new views help
    Former-commit-id: 1a0629fc6e54038cb6c55c42010bc90c5333cab8

---
+++ routes/routes_help.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var sweetcaptcha = new require('sweetcaptcha')('233846', 'f2a70ef1df3edfaa6cf45d7c338e40b8', '720457356dc3156eb73fe316a293af2f');
+//var crypto = require('crypto');
+// These are all under /help
+
+/* GET Overview page. */
+  router.get('/overview', function(req, res) {
+      res.render('help/overview', { title: 'VAMPS:Overview', 
+      user: req.user,
+      message:'',
+   });
+  });
+
+/* GET FAQ page. */
+  router.get('/faq', function(req, res) {
+      res.render('help/faq', { title: 'VAMPS:FAQ',
+                message:'',
+                             user: req.user
+                            });
+  });
+
+  /* GET Contact Us page. */
+  router.get('/contact', function(req, res) {
+
+      //get sweetcaptcha html for the contact area
+        sweetcaptcha.api('get_html', function(err,html){
+            //Send the guts of the captcha to your template
+            res.render('help/contact', {
+              captcha : html,
+              title: 'VAMPS:Cuntact-Us',
+              user: req.user
+               });
+        });
+  });
+
+
+
+module.exports = router;
