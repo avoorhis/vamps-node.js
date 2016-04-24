a5c507e444b60bcc7720533e9ba2f44363e04bd1
annaship <tetya-loshad@yandex.ru>
2016-04-24 17:05:31 -0400

    INSERT ignore INTO required_metadata_info

--- public/scripts/maintenance_scripts/old_to_new_vamps_by_project.py
+++ public/scripts/maintenance_scripts/old_to_new_vamps_by_project.py
@@ -946,9 +946,9 @@ class Metadata:
     for project, required_metadata_dict in self.required_metadata_by_pr_dict.items():
       print "PPP"
       print dataset.all_dataset_id_by_project_dict[project]
-      temp_list = ""
-      required_metadata_dict_values = required_metadata_dict.values()
+      temp_list = []
       for dataset_id in dataset.all_dataset_id_by_project_dict[project]:
+        required_metadata_dict_values = required_metadata_dict.values()
         # print "required_metadata_dict_values"
         # print required_metadata_dict_values
         # print "dataset_id = %s" % dataset_id
@@ -956,19 +956,20 @@ class Metadata:
         # print "required_metadata_dict_values = "
         # print required_metadata_dict_values
         # all_insert_req_met_vals = "'" + "', '".join(required_metadata_dict.values()) + "'"
-        
-        temp_list = "'%s', '" % str(dataset_id) + "', '".join(required_metadata_dict_values) + "'"
+        required_metadata_dict_values.insert(0, dataset_id)
+        temp_list.append(required_metadata_dict_values)
+        # temp_list = "'%s', '" % str(dataset_id) + "', '".join(required_metadata_dict_values) + "'"
         print "temp_list 1 = "
         print temp_list 
-        a1 = ", (%s)" % temp_list
-        print "a1 1 = "
-        print a1 
-        all_insert_req_met_vals += a1
+        # a1 = ", (%s)" % temp_list
+        # print "a1 1 = "
+        # print a1
+        # all_insert_req_met_vals += a1
       #
       # print "temp_list 2 = "
       # print temp_list
 
-        # all_insert_req_met_vals += self.utils.make_insert_values(temp_list)
+    all_insert_req_met_vals += self.utils.make_insert_values(temp_list)
 
 
     # for project, dataset_id_list in dataset.all_dataset_id_by_project_dict.items():
