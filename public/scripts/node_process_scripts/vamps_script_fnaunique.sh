fa9403e828e36b68ff384396193af7a5554c0111
annaship <tetya-loshad@yandex.ru>
2016-08-05 15:21:57 -0400

    fastaunique path intermediate

--- public/scripts/node_process_scripts/vamps_script_fnaunique.sh
+++ public/scripts/node_process_scripts/vamps_script_fnaunique.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # see routes_user_data.js
-#  USAGE: $0 project_dir uniques_file_name dataset_name
+#  USAGE: $0 fastaunique_dir project_dir
 
 cd $1
 echo "Single file to unique"
 
 
-echo "for file in *.fa; do fastaunique $file; done"
-for file in *.fa; do fastaunique $file; done
+echo "for file in *.fa; do $1/fastaunique $2/$file; done"
+for file in $2/*.fa; do $1/fastaunique $2/$file; done
 
 
 #grendel
 #curr_path=`pwd`
-#curr_dir=${PWD##*/} 
+#curr_dir=${PWD##*/}
 #echo "Run on Grendel, change the reference file name accordingly:"
 #echo "cd $curr_path; run_gast_ill_qiita_sge.sh -s $curr_dir -d gast -v -e fa.unique -r refssu -f -p both"
