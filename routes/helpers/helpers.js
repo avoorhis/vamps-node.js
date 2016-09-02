49b9e66fd04852d3eea761708a782521f2541cac
annaship <tetya-loshad@yandex.ru>
2016-09-02 11:32:50 -0400

    delete status escape, works, tested by changing "new" to "delete"

--- routes/helpers/helpers.js
+++ routes/helpers/helpers.js
@@ -666,9 +666,10 @@ module.exports.update_status = function(status_params) {
   console.log(util.inspect(status_params, false, null));
 
   if (status_params.type === 'delete') {
-    statQuery = MakeDeleteStatusQ(status_params);
+    delete_status_params = [status_params.user_id, status_params.project]
+    statQuery = MakeDeleteStatusQ();
     console.log('in update_status, after delete_status');
-    connection.query(statQuery, function(err, rows) {
+    connection.query(statQuery, delete_status_params, function(err, rows) {
       if(err) { console.log('ERROR1-in status update: ' + err);
       }
       else {
@@ -705,18 +706,31 @@ module.exports.update_status = function(status_params) {
   } // Type::New
 };
 
-MakeDeleteStatusQ = function(status_params) {
+MakeDeleteStatusQ = function() {
   console.log('in delete_status');
   if (status_params.type === 'delete') {
-    var statQuery = "DELETE user_project_status";
-        statQuery += " FROM user_project_status";
-        statQuery += " JOIN project USING(project_id)";
-        statQuery += " WHERE user_id ='" + status_params.user_id + "' ";
-        statQuery += " AND   project ='" + status_params.project + "' ";
+    var statQuery = "DELETE user_project_status"
+        + " FROM user_project_status"
+        + " JOIN project USING(project_id)"
+        + " WHERE user_id = ?"
+        + " AND   project = ?";
     console.log('DELETE query: ' + statQuery);
     return statQuery;
   }
 };
+
+// MakeDeleteStatusQ = function(status_params) {
+//   console.log('in delete_status');
+//   if (status_params.type === 'delete') {
+//     var statQuery = "DELETE user_project_status";
+//         statQuery += " FROM user_project_status";
+//         statQuery += " JOIN project USING(project_id)";
+//         statQuery += " WHERE user_id ='" + status_params.user_id + "' ";
+//         statQuery += " AND   project ='" + status_params.project + "' ";
+//     console.log('DELETE query: ' + statQuery);
+//     return statQuery;
+//   }
+// };
 
 MakeUpdateStatusQ = function(status_params)
 {
