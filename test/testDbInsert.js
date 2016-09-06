88bfeecebafafa5b9858761613ed8b6791d5e1fc
annaship <tetya-loshad@yandex.ru>
2016-09-06 17:59:03 -0400

    cleaning up

--- test/testDbInsert.js
+++ test/testDbInsert.js
@@ -14,16 +14,16 @@ beforeEach(function() {
   passportStub.logout();
 });
 
- 
+
 describe('Req 1: Landing page functionality', function(){
   before(function (done) {
   // it('Insert into DB', function(done){
-    
+
     this.timeout(5000);
     async.series([
       function (cb) {
         var q1 = "INSERT INTO project (`project`, `title`, `project_description`, `rev_project_name`, `funding`, `owner_user_id`, `public`) VALUES ('test_gast_project', 'new Title', 'new description', REVERSE('test_gast_project'), '000', 1, 1);";
-        
+
         connection.query(q1, function(err){
             done();
           });
@@ -32,7 +32,7 @@ describe('Req 1: Landing page functionality', function(){
       //   connection.query('SELECT * FROM project;', function(err, results){
       //     console.log("results")
       //     console.log(util.inspect(results, false, null));
-      //   
+      //
       //     results.length.should.equal(22);
       //     results[2].project.should.containEql("test_gast_project21");
       //     done();
@@ -40,6 +40,7 @@ describe('Req 1: Landing page functionality', function(){
       // }
     ], done);
   });
+
   it('1.1 Text of landing page', function(done){
     request(app)
       .get('/')
@@ -47,21 +48,21 @@ describe('Req 1: Landing page functionality', function(){
       .end(function (err, res) {
         // console.log("EEE res");
         // console.log(util.inspect(res, false, null));
-        
+
         res.text.should.containEql('Visualizations');
         done();
       });
   });
-  
-  it('1.2 Link to the login page', function(done){
+
+  it('Check select', function(done){
       connection.query('SELECT * FROM project;', function(err, results){
         console.log("results111")
         console.log(util.inspect(results, false, null));
-      
+
         results.length.should.equal(2);
         results[1].project.should.containEql("test_gast_project");
         done();
       });
   });
 });
-// Find the rest of the test code in the source link below 
+// Find the rest of the test code in the source link below
