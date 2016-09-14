07b521277b45a6ba84e6c6dd12782ecca172f671
annaship <tetya-loshad@yandex.ru>
2016-09-14 13:52:31 -0400

    cleanup

--- routes/routes_user_data.js
+++ routes/routes_user_data.js
@@ -286,45 +286,25 @@ router.get('/import_choices', helpers.isLoggedIn, function (req, res) {
   }
 });
 
-// router.post('/import_choices/simple_fasta', [helpers.isLoggedIn],
 
 // AShipunova Aug 2016
 
-// router.get('/import_choices/simple_fasta', [helpers.isLoggedIn], function (req, res) {
-//   console.log('in import_choices/simple_fasta');
-//
-//   res.render('user_data/import_choices/simple_fasta', {
-//     title: 'Import Data',
-//     user: req.user,
-//     hostname: req.CONFIG.hostname,
-//     message: req.flash('message'),
-//     failmessage: req.flash('failMessage'),
-//     import_type: 'simple_fasta',
-//   });
-// });
-
 router.get('/import_choices/*_fasta', [helpers.isLoggedIn], function (req, res) {
-  console.log('in GET import_choices/*_fasta; req.url = ');
-  console.log(util.inspect(req.url, false, null));
-
-  // import_choices/multi_fasta
   url         = path.join('user_data', req.url);
   import_type = req.url.split("/").slice(-1)[0];
-  console.log('YYY import_type.slice(-1)[0] = ');
-  console.log(util.inspect(import_type, false, null));
+  //'/import_choices/multi_fasta', 'multi_fasta'
   
   res.render(url, {
-    title: 'Import Data',
-    user: req.user,
-    hostname: req.CONFIG.hostname,
-    message: req.flash('message'),
+    title:       'Import Data',
+    user:        req.user,
+    hostname:    req.CONFIG.hostname,
+    message:     req.flash('message'),
     failmessage: req.flash('failMessage'),
     import_type: import_type,
   });
 });
 
-
-
+// TODO: check editUploadData and uploadData for simple_fasta vs. multi_fasta etc.
 router.post('/import_choices/simple_fasta', [helpers.isLoggedIn, upload.array('upload_files', 12)],
   form(
     form.field("project", "Project Name").trim().required().is(/^[a-zA-Z_0-9]+$/, "Only letters, numbers and underscores are valid in %s").minLength(3).maxLength(20).entityEncode(),
@@ -348,19 +328,6 @@ router.post('/import_choices/simple_fasta', [helpers.isLoggedIn, upload.array('u
   }
 );
 
-// router.get('/import_choices/multi_fasta', [helpers.isLoggedIn], function (req, res) {
-//   console.log('in import_choices/multi_fasta');
-//
-//   res.render('user_data/import_choices/multi_fasta', {
-//     title: 'Import Data',
-//     user: req.user,
-//     hostname: req.CONFIG.hostname,
-//     message: req.flash('message'),
-//     failmessage: req.flash('failMessage'),
-//     import_type: 'multi_fasta',
-//   });
-// });
-//
 router.post('/import_choices/multi_fasta', [helpers.isLoggedIn, upload.array('upload_files', 12)],
   form(
     form.field("project", "Project Name").trim().required().is(/^[a-zA-Z_0-9]+$/, "Only letters, numbers and underscores are valid in %s").minLength(3).maxLength(20).entityEncode()
@@ -1975,14 +1942,14 @@ function editUploadData(req, res)
   console.log("EEE editUploadData: req.form");
   console.log(util.inspect(req.form, false, null));
   res.render('user_data/import_data', {
-    title: 'VAMPS:Import Data',
-    message: req.flash('successMessage'),
-    failmessage: req.flash('failMessage'),
-    messages: req.flash('messages'),
-    import_type: req.body.type,
-    user: req.user,
-    form_data: req.form,
-    hostname: req.CONFIG.hostname
+    title:        'VAMPS:Import Data',
+    message:      req.flash('successMessage'),
+    failmessage:  req.flash('failMessage'),
+    messages:     req.flash('messages'),
+    import_type:  req.body.type,
+    user:         req.user,
+    form_data:    req.form,
+    hostname:     req.CONFIG.hostname
   });
 }
 
