17c564f713cf4afbc2358d5d27967bbf5b356ec0
annaship <tetya-loshad@yandex.ru>
2016-09-26 12:44:31 -0400

    path.join

--- config/config_example.js
+++ config/config_example.js
@@ -1,6 +1,6 @@
 // This is an example! *** Change all paths to match your system *** 
 var path = require('path');
-var os = require("os");
+var os = require('os');
 
 var config = {};
 
@@ -16,9 +16,9 @@ config.TMP = config.PROCESS_DIR + '/tmp'
 // install rdp (http://sourceforge.net/projects/rdp-classifier/files/rdp-classifier/) 
 // on your system and provide the path to classifier.jar file here.
 // You will also need java on your system to run this.
-//config.PATH_TO_RDP = config.PROCESS_DIR + '/rdp_classifier/dist/';
+//config.PATH_TO_RDP = config.PROCESS_DIR + '/rdp_classifier/dist';
 config.PATH_TO_NODE_SCRIPTS = path.join(process.env.PWD, 'public', 'scripts', 'node_process_scripts');
-config.PATH_TO_VIZ_SCRIPTS = path.join(process.env.PWD, 'public', 'scripts', 'visualization_scripts/');
+config.PATH_TO_VIZ_SCRIPTS = path.join(process.env.PWD, 'public', 'scripts', 'visualization_scripts');
 config.PATH_TO_ADDITIONAL_SCRIPTS = path.join(process.env.PWD, 'public', 'scripts', 'bin');
 //
 // http://www.ncbi.nlm.nih.gov/books/NBK279671/
@@ -27,7 +27,7 @@ config.PATH_TO_BLAST = 'your_path';
 // http://qiime.org/
 config.PATH_TO_QIIME_BIN = '';
 config.PATH_TO_VSEARCH = 'your_path/vsearch'; // not used IF vsearch in $PATH
-config.PATH_TO_CLASSIFIER = config.PROCESS_DIR + '/rdp_classifier/dist/'; // 
+config.PATH_TO_CLASSIFIER = config.PROCESS_DIR + '/rdp_classifier/dist'; // 
 config.PATH = 'your_path:' + path.join(config.PROCESS_DIR, '/public/scripts/bin') + ':' + config.PATH_TO_NODE_SCRIPTS;
 config.LD_LIBRARY_PATH = config.PROCESS_DIR + '/public/scripts:/analysis/gast/ds/:your_path'
 config.PYTHONPATH = 'your_path';
@@ -35,7 +35,7 @@ config.LAPACK = ''
 
 config.PATH2 = '/opt/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin';
 config.LD_LIBRARY_PATH2 = 'your_path'
-config.PYTHONPATH2 = '/usr/local/lib/';
+config.PYTHONPATH2 = '/usr/local/lib';
 config.GOOGLE_EARTH_KEY = ''
 
 config.PERL5LIB = 'your_path'
