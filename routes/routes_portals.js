609e3d577c3976653d1a98c5dfa81c01f278434e
Andrew Voorhis <avoorhis@mbl.edu>
2016-12-29 14:40:21 -0500

    portal map

--- routes/routes_portals.js
+++ routes/routes_portals.js
@@ -281,7 +281,8 @@ function get_portal_metadata(req, portal){
         
         }
     }
-
+    console.log('JSON.stringify(portal_info)')
+    console.log(JSON.stringify(portal_info))
     return portal_info;
 }
 //
