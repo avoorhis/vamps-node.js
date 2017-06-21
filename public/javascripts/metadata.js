0237b72dd182498b87c26576cacc5ec9c1e448bf
annaship <tetya-loshad@yandex.ru>
2017-06-21 13:16:20 -0400

    cp select

--- public/javascripts/metadata.js
+++ public/javascripts/metadata.js
@@ -699,7 +699,9 @@ $('a.td_clone_add').on('click', function() {
 
   input_row.find('td').each(function() {
     $(this).children('input').val(first_input_value);
-      // .css('background-color','blue');
+    $(this).children('select').val(first_input_value);
+
+    // .css('background-color','blue');
   });
 
   return(false);
