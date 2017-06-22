82593ffbe8d4ad478f0e54bec7f1914d3a84da83
annaship <tetya-loshad@yandex.ru>
2017-06-22 17:48:17 -0400

    exclude readonly

--- public/javascripts/metadata.js
+++ public/javascripts/metadata.js
@@ -745,7 +745,7 @@ CopyColumn = function() {
 
     $tdsInColumnCurrent.each(function () {
       var current_val = $(this).children( ':input' ).val();
-      $(this).siblings().eq(columnNo).children( ':input' ).val(current_val).change();
+      $(this).siblings().not('.readonly_td').eq(columnNo).children( ':input' ).val(current_val).change();
     });
   });
 };
