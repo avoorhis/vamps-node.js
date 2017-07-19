cb4c57f9d468032983e85ea41e9bc64a2ddbe388
annaship <tetya-loshad@yandex.ru>
2017-07-19 11:05:17 -0400

    parses csv correctly

--- routes/routes_metadata.js
+++ routes/routes_metadata.js
@@ -165,25 +165,38 @@ router.post('/start_edit',
 
     var coopy = require('coopyhx');
 
+    // TODO: get from dir, not hardcoded
     var inputPath1 = "/Users/ashipunova/BPC/vamps-node.js/user_data/vamps2/AnnaSh/metadata-project_DCO_GAI_Bv3v5_31989.csv";
     var inputPath2 = "/Users/ashipunova/BPC/vamps-node.js/user_data/vamps2/AnnaSh/metadata-project_DCO_GAI_Bv3v5_63239.csv";
 
+    var columnDelimiter = ',';
+    var lineDelimiter = '\n';
+    var cellEscape = '"';
+
     var data1 = String(fs.readFileSync(inputPath1));
     var data2 = String(fs.readFileSync(inputPath2));
-    console.log("AAA7 data1");
-    console.log(data1);
+    // console.log("AAA7 data1");
+    // console.log(data1);
     var parse = require('csv-parse');
 
-    fs.readFile(inputPath1, function (err, data) {
-      console.log("AAA01 data");
+    var parser = parse({delimiter: columnDelimiter, columns: true, trim: true}, function(err, data){
+      console.log("AAA7 data");
       console.log(data);
-      parse(fileData, {columns: false, trim: true}, function(err, rows) {
-        console.log("AAA03 fileData");
-        console.log(fileData);
-
-        // Your CSV data is in an array of arrys passed to this callback as rows.
-      });
     });
+
+    fs.createReadStream(inputPath1).pipe(parser);
+
+    // fs.readFile(inputPath1, function (err, data) {
+    //   console.log("AAA01 data");
+    //   console.log(data);
+    //   var records = parse(string(data), {columns: true, trim: true});
+    //   // parse(data, {columns: true, trim: true}, function(err, rows) {
+    //   console.log("AAA03 records");
+    //   console.log(records);
+    //   //
+    //   //   // Your CSV data is in an array of arrys passed to this callback as rows.
+    //   // });
+    // });
     // var a = String(data1);
     // console.log("AAA8 a");
     // console.log(a);
