e494fac9adc33c06f32635d5b6e9852406c3afd7
annaship <tetya-loshad@yandex.ru>
2017-08-08 17:29:56 -0400

    grey dataset name reminder

--- public/javascripts/metadata.js
+++ public/javascripts/metadata.js
@@ -892,17 +892,20 @@ CopyColumn = function() {
 
 showDatasets = function() {
   $('#table_div_header').hide();
-  $('#firstTd').html('');
+  $('#firstTd').hide();
+    // .html('');
 
   $('#table_div').on('scroll', function () {
     if ($('#table_div').scrollTop() > 0) {
       $('#table_div_header').show();
-      $('#firstTd').html('VAMPS dataset name');
+      $('#firstTd').html('VAMPS dataset name').show();
+        // .html('VAMPS dataset name');
     }
     $('#table_div_header').scrollLeft($('#table_div').scrollLeft());
     if ($('#table_div').scrollTop() === 0) {
       $('#table_div_header').hide();
-      $('#firstTd').html('');
+      $('#firstTd').hide();
+        // .html('');
     }
 
   });
