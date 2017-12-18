a7cabbd74b0fcd7e297325219f5e731c1c34f2eb
Andrew Voorhis <avoorhis@mbl.edu>
2017-12-18 08:21:17 -0500

    alpha mg list

--- routes/routes_metagenome.js
+++ routes/routes_metagenome.js
@@ -17,32 +17,38 @@ METAGENOMIC_INFORMATION_BY_PID = {}
 
 router.get('/index', helpers.isLoggedIn, function (req, res) {
     console.log('In Metagenome index.html')
-    
+    project_list=[]
     connection.query(queries.get_metagenomic_projects_query(), function (err, rows, fields) {
         for(n in rows){
             pid = rows[n].pid
-            METAGENOMIC_INFORMATION_BY_PID[pid] = {}
+            pname = rows[n].project
+            project_list.push(rows[n]['project'])
+            METAGENOMIC_INFORMATION_BY_PID[pname] = {}
             
             for(item in rows[n]){                
-                METAGENOMIC_INFORMATION_BY_PID[pid].last            = rows[n]['last_name']
-                METAGENOMIC_INFORMATION_BY_PID[pid].first           = rows[n]['first_name']
-                METAGENOMIC_INFORMATION_BY_PID[pid].username        = rows[n]['username']
-                METAGENOMIC_INFORMATION_BY_PID[pid].oid             = rows[n]['owner_user_id']
-                METAGENOMIC_INFORMATION_BY_PID[pid].email           = rows[n]['email']
-                METAGENOMIC_INFORMATION_BY_PID[pid].institution     = rows[n]['institution']
-                METAGENOMIC_INFORMATION_BY_PID[pid].project         = rows[n]['project']
-                METAGENOMIC_INFORMATION_BY_PID[pid].pid             = pid
-                METAGENOMIC_INFORMATION_BY_PID[pid].title           = rows[n]['title']
-                METAGENOMIC_INFORMATION_BY_PID[pid].description     = rows[n]['project_description']
-                METAGENOMIC_INFORMATION_BY_PID[pid].public          = rows[n]['public']
-                METAGENOMIC_INFORMATION_BY_PID[pid].permissions     = []
+                
+                METAGENOMIC_INFORMATION_BY_PID[pname].last            = rows[n]['last_name']
+                METAGENOMIC_INFORMATION_BY_PID[pname].first           = rows[n]['first_name']
+                METAGENOMIC_INFORMATION_BY_PID[pname].username        = rows[n]['username']
+                METAGENOMIC_INFORMATION_BY_PID[pname].oid             = rows[n]['owner_user_id']
+                METAGENOMIC_INFORMATION_BY_PID[pname].email           = rows[n]['email']
+                METAGENOMIC_INFORMATION_BY_PID[pname].institution     = rows[n]['institution']
+                METAGENOMIC_INFORMATION_BY_PID[pname].project         = rows[n]['project']
+                
+                METAGENOMIC_INFORMATION_BY_PID[pname].pid             = pid
+                METAGENOMIC_INFORMATION_BY_PID[pname].title           = rows[n]['title']
+                METAGENOMIC_INFORMATION_BY_PID[pname].description     = rows[n]['project_description']
+                METAGENOMIC_INFORMATION_BY_PID[pname].public          = rows[n]['public']
+                METAGENOMIC_INFORMATION_BY_PID[pname].permissions     = []
             }
         }
+        project_list.sort()
   console.log(METAGENOMIC_INFORMATION_BY_PID)
         res.render('metagenome/metagenome_index', {
                                   title       : 'Metagenomic Project Listing',
                                   subtitle    : 'Project Selection Page',
                                   obj         : JSON.stringify(METAGENOMIC_INFORMATION_BY_PID),
+                                  name_lst    : JSON.stringify(project_list),
                                   user        : req.user,
                                   hostname    : req.CONFIG.hostname,
                                   
