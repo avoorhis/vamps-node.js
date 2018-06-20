e4c2295872018e6351bae4f9ce12c4b9bb9027c3
annaship <tetya-loshad@yandex.ru>
2018-06-20 15:45:22 -0400

    start ajax

--- public/javascripts/submissions.js
+++ public/javascripts/submissions.js
@@ -1,19 +1,34 @@
+function show_change(scode) {
+  console.log('in show_change: ' + scode);
+  table_div  = document.getElementById('sub_table');
+  var args   = {};
+  args.scode = scode;
 
-function show_change(scode){
-    console.log('in show_change: '+scode)
-    table_div = document.getElementById('sub_table')
-    var args = {}
-    args.scode = scode
-    
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", '/submissions/get_table', true);
-    xmlhttp.setRequestHeader("Content-Type", "application/json");
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 ) {
-            console.log('Were Back')
-            table_div.innerHTML = JSON.parse(xmlhttp.response)
-        }
-    };
-    xmlhttp.send(JSON.stringify(args));
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", '/submissions/get_table', true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState === 4) {
+      console.log('Were Back');
+      table_div.innerHTML = JSON.parse(xmlhttp.response);
+    }
+  };
+  xmlhttp.send(JSON.stringify(args));
 
 }
+
+$("button[name='Download as Spreadsheet']").on('click',
+  function (event) {
+    alert('TTT1');
+
+  });
+//					$.ajax({
+// 						type: 'POST',
+// 						data: JSON.stringify(data),
+// 				        contentType: 'application/json',
+//                         url: 'http://localhost:3000/endpoint',
+//                         success: function(data) {
+//                             console.log('success');
+//                             console.log(JSON.stringify(data));
+//                         }
+//                     });
