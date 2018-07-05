a26d2ba31d1c132f04c7dbeafb1bbf609193f1e1
annaship <tetya-loshad@yandex.ru>
2018-07-05 11:46:46 -0400

    CreateDataObj, add functions

--- controllers/metadataController_copy.js
+++ controllers/metadataController_copy.js
@@ -1,12 +1,12 @@
-var Project   = require(app_root + '/models/project_model');
-var Dataset   = require(app_root + '/models/dataset_model');
-var User      = require(app_root + '/models/user_model');
+// var Project   = require(app_root + '/models/project_model');
+// var Dataset   = require(app_root + '/models/dataset_model');
+// var User      = require(app_root + '/models/user_model');
 var helpers   = require(app_root + '/routes/helpers/helpers');
 var CONSTS    = require(app_root + '/public/constants');
-var validator = require('validator');
-var config    = require(app_root + '/config/config');
-var fs        = require('fs');
-var path      = require('path');
+// var validator = require('validator');
+// var config    = require(app_root + '/config/config');
+// var fs        = require('fs');
+// var path      = require('path');
 
 // 1 create data from
 // 1.1  db
@@ -22,91 +22,158 @@ var path      = require('path');
 // ===
 
 // 1 create data
-function CreateDataObj(req, res) {
+class CreateDataObj {
 
-  this.project_id   = '';
-  // Object.keys(all_metadata)[0]; // check if exists and not 0
-  this.dataset_ids  = []; // DATASET_IDS_BY_PID[pid] or from db or from save
-  this.all_metadata = prepare_empty_metadata_object(pid, all_field_names, {});
-
-  this.all_field_names = collect_field_names(dataset_ids);
-  this.project         = PROJECT_INFORMATION_BY_PID[project_id].project;
-
-}
-
-function collect_field_names(dataset_ids) {
-}
-
-function prepare_empty_metadata_object(pid, field_names_arr, all_metadata) {
-  console.time('TIME: prepare_empty_metadata_object');
-  all_metadata = all_metadata || {};
-  if (!(all_metadata.hasOwnProperty(pid))) {
-    all_metadata[pid] = {};
+  constructor (req, res, project_id, dataset_ids) {
+    this.req = req || {};
+    this.res = res || {};
+    this.pid   = project_id || '';
+    this.dataset_ids  = dataset_ids || [];
+    this.all_field_names = this.collect_field_names(this.dataset_ids);
+    this.all_metadata = this.prepare_empty_metadata_object(this.pid, this.all_field_names, {});
+    //
+    // this.project         = PROJECT_INFORMATION_BY_PID[project_id].project;
   }
 
-  for (var i = 0; i < field_names_arr.length; i++) {
-    var field_name = field_names_arr[i];
-    if (!(all_metadata[pid].hasOwnProperty(field_name))) {
-      all_metadata[pid][field_name] = [];
-    }
+  collect_field_names () {
+    var all_field_names = this.get_field_names_by_dataset_ids(this.dataset_ids);
+    all_field_names     = all_field_names.concat(CONSTS.METADATA_FORM_REQUIRED_FIELDS);
+    all_field_names     = all_field_names.concat(CONSTS.REQ_METADATA_FIELDS_wIDs);
+    all_field_names     = all_field_names.concat(CONSTS.PROJECT_INFO_FIELDS);
+    all_field_names     = all_field_names.concat(CONSTS.METADATA_NAMES_ADD);
+
+    all_field_names = helpers.unique_array(all_field_names);
+    return all_field_names;
   }
 
-  console.timeEnd('TIME: prepare_empty_metadata_object');
-  return all_metadata;
-}
+  get_field_names_by_dataset_ids() {
 
-CreateDataObj.prototype.make_metadata_object = function (req, res, pid, info) {
-  console.time('TIME: make_metadata_object');
-
-  var repeat_times = dataset_ids.length;
-
-
-  //2) all
-
-  all_metadata[pid] = info;
-
-  //3) special
-
-  // TODO: move to db creation?
-  var project_info = get_project_info(pid);
-  console.log('MMM33 all_metadata[pid]');
-  console.log(JSON.stringify(all_metadata[pid]));
-
-  for (var idx in CONSTS.PROJECT_INFO_FIELDS) {
-    var field_name = CONSTS.PROJECT_INFO_FIELDS[idx];
-
-    //todo: split if, if length == dataset_ids.length - just use as is
-    if ((typeof all_metadata[pid][field_name] !== 'undefined') && all_metadata[pid][field_name].length < 1) {
-      all_metadata[pid][field_name] = module.exports.fill_out_arr_doubles(all_metadata[pid][field_name], repeat_times);
+    var field_names_arr = [];
+    if (typeof dataset_ids === 'undefined' || this.dataset_ids.length === 0) {
+      field_names_arr = field_names_arr.concat(Object.keys(MD_CUSTOM_FIELDS_UNITS));
     }
     else {
-      all_metadata[pid][field_name] = module.exports.fill_out_arr_doubles(project_info[field_name], repeat_times);
-    }
-  }
-
-  if ((all_metadata[pid]['project_abstract'] === 'undefined') || (!all_metadata[pid].hasOwnProperty(['project_abstract']))) {
-    all_metadata[pid]['project_abstract'] = module.exports.fill_out_arr_doubles('', repeat_times);
-  }
-  else {
-
-    if ((all_metadata[pid]['project_abstract'][0] !== 'undefined') && (!Array.isArray(all_metadata[pid]['project_abstract'][0]))) {
-
-      var project_abstract_correct_form = helpers.unique_array(all_metadata[pid]['project_abstract']);
-
-      if (typeof project_abstract_correct_form[0] !== 'undefined') {
-
-        all_metadata[pid]['project_abstract'] = module.exports.fill_out_arr_doubles(project_abstract_correct_form[0].split(','), repeat_times);
-
+      for (var i = 0; i < this.dataset_ids.length; i++) {
+        var dataset_id  = this.dataset_ids[i];
+        field_names_arr = field_names_arr.concat(Object.keys(AllMetadata[dataset_id]));
       }
     }
+    field_names_arr = helpers.unique_array(field_names_arr); // one level
+    field_names_arr.sort();
+
+    return field_names_arr;
+//  [
+//   'access_point_type',
+//   'adapter_sequence',
+//   'adapter_sequence_id',
+// ...
   }
 
-  console.log('MMM9 all_metadata[pid]');
-  console.log(JSON.stringify(all_metadata[pid]));
+  prepare_empty_metadata_object(pid, field_names_arr, all_metadata) {
+    console.time('TIME: prepare_empty_metadata_object');
+    all_metadata = all_metadata || {};
+    if (!(all_metadata.hasOwnProperty(pid))) {
+      all_metadata[pid] = {};
+    }
 
-  console.timeEnd('TIME: make_metadata_object');
-  return all_metadata;
-};
+    for (var i = 0; i < field_names_arr.length; i++) {
+      var field_name = field_names_arr[i];
+      if (!(all_metadata[pid].hasOwnProperty(field_name))) {
+        all_metadata[pid][field_name] = [];
+      }
+    }
+
+    console.timeEnd('TIME: prepare_empty_metadata_object');
+    return all_metadata;
+  }
+
+
+  // prepare_empty_metadata_object (pid, all_field_names, {}) {
+
+  // }
+//   this.project_id   = '';
+//   // Object.keys(all_metadata)[0]; // check if exists and not 0
+//   this.dataset_ids  = []; // DATASET_IDS_BY_PID[pid] or from db or from save
+//   this.all_metadata = prepare_empty_metadata_object(pid, all_field_names, {});
+//
+//   this.all_field_names = collect_field_names(dataset_ids);
+//   this.project         = PROJECT_INFORMATION_BY_PID[project_id].project;
+//
+// }
+//
+// function collect_field_names(dataset_ids) {
+// }
+//
+// function prepare_empty_metadata_object(pid, field_names_arr, all_metadata) {
+//   console.time('TIME: prepare_empty_metadata_object');
+//   all_metadata = all_metadata || {};
+//   if (!(all_metadata.hasOwnProperty(pid))) {
+//     all_metadata[pid] = {};
+//   }
+//
+//   for (var i = 0; i < field_names_arr.length; i++) {
+//     var field_name = field_names_arr[i];
+//     if (!(all_metadata[pid].hasOwnProperty(field_name))) {
+//       all_metadata[pid][field_name] = [];
+//     }
+//   }
+//
+//   console.timeEnd('TIME: prepare_empty_metadata_object');
+//   return all_metadata;
+// }
+//
+// CreateDataObj.prototype.make_metadata_object = function (req, res, pid, info) {
+//   console.time('TIME: make_metadata_object');
+//
+//   var repeat_times = dataset_ids.length;
+//
+//
+//   //2) all
+//
+//   all_metadata[pid] = info;
+//
+//   //3) special
+//
+//   // TODO: move to db creation?
+//   var project_info = get_project_info(pid);
+//   console.log('MMM33 all_metadata[pid]');
+//   console.log(JSON.stringify(all_metadata[pid]));
+//
+//   for (var idx in CONSTS.PROJECT_INFO_FIELDS) {
+//     var field_name = CONSTS.PROJECT_INFO_FIELDS[idx];
+//
+//     //todo: split if, if length == dataset_ids.length - just use as is
+//     if ((typeof all_metadata[pid][field_name] !== 'undefined') && all_metadata[pid][field_name].length < 1) {
+//       all_metadata[pid][field_name] = module.exports.fill_out_arr_doubles(all_metadata[pid][field_name], repeat_times);
+//     }
+//     else {
+//       all_metadata[pid][field_name] = module.exports.fill_out_arr_doubles(project_info[field_name], repeat_times);
+//     }
+//   }
+//
+//   if ((all_metadata[pid]['project_abstract'] === 'undefined') || (!all_metadata[pid].hasOwnProperty(['project_abstract']))) {
+//     all_metadata[pid]['project_abstract'] = module.exports.fill_out_arr_doubles('', repeat_times);
+//   }
+//   else {
+//
+//     if ((all_metadata[pid]['project_abstract'][0] !== 'undefined') && (!Array.isArray(all_metadata[pid]['project_abstract'][0]))) {
+//
+//       var project_abstract_correct_form = helpers.unique_array(all_metadata[pid]['project_abstract']);
+//
+//       if (typeof project_abstract_correct_form[0] !== 'undefined') {
+//
+//         all_metadata[pid]['project_abstract'] = module.exports.fill_out_arr_doubles(project_abstract_correct_form[0].split(','), repeat_times);
+//
+//       }
+//     }
+//   }
+//
+//   console.log('MMM9 all_metadata[pid]');
+//   console.log(JSON.stringify(all_metadata[pid]));
+//
+//   console.timeEnd('TIME: make_metadata_object');
+//   return all_metadata;
+}
 
 // TODO: functions to make: all_field_names_arr, all_field_units, ordered_field_names_obj,
 
