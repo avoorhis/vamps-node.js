d39aaed83812c13479da37256dec41c8f3dd51f6
annaship <tetya-loshad@yandex.ru>
2018-07-10 15:29:42 -0400

    moved form validation to helpers

--- controllers/metadataController.js
+++ controllers/metadataController.js
@@ -25,20 +25,20 @@ function checkArray(my_arr) {
   return true;
 }
 
-function check_regexp(reg_exp, value, err_msg) {
-  var result = value.match(reg_exp);
+// function check_regexp(reg_exp, value, err_msg) {
+//   var result = value.match(reg_exp);
+//
+//   // if (value !== '' && result === null) {
+//   if (value !== '' && result !== null) {
+//     throw new Error("'" + value + "' is not allowed in '%s'" + err_msg);
+//   }
+// }
 
-  // if (value !== '' && result === null) {
-  if (value !== '' && result !== null) {
-    throw new Error("'" + value + "' is not allowed in '%s'" + err_msg);
-  }
-}
-
-function region_valid(value, region_low, region_high) {
-  if ((value !== '') && (parseInt(value) < parseInt(region_low) || parseInt(value) > parseInt(region_high))) {
-    throw new Error("'" + value + "' is not valid, %s should be between " + region_low + " and " + region_high);
-  }
-}
+// function region_valid(value, region_low, region_high) {
+//   if ((value !== '') && (parseInt(value) < parseInt(region_low) || parseInt(value) > parseInt(region_high))) {
+//     throw new Error("'" + value + "' is not valid, %s should be between " + region_low + " and " + region_high);
+//   }
+// }
 
 function new_row_field_validation(req, field_name) {
   console.time('TIME: new_row_field_validation');
@@ -225,80 +225,80 @@ exports.get_second = function (element) {
   console.timeEnd('TIME: get_second');
 };
 
-exports.env_items_validation = function (value) {
-  if (value === 'Please choose one') {
-    throw new Error('%s is required. Please choose one value from the dropdown menu');
-  }
-};
+// exports.env_items_validation = function (value) {
+//   if (value === 'Please choose one') {
+//     throw new Error('%s is required. Please choose one value from the dropdown menu');
+//   }
+// };
+//
+// exports.geo_loc_name_validation = function (value, source) {
+//   if ((!checkArray(source.geo_loc_name_marine)) && (!checkArray(source.geo_loc_name_continental))) {
+//     throw new Error("Either 'Country' or 'Longhurst Zone' are required"); // jshint ignore:line
+//   }
+// };
+//
+// exports.geo_loc_name_continental_filter = function (value) {
+//   console.time('geo_loc_name_continental_filter');
+//   for (var key in CONSTS.GAZ_SPELLING) {
+//     if (CONSTS.GAZ_SPELLING.hasOwnProperty(key)) {
+//       var curr = CONSTS.GAZ_SPELLING[key];
+//       if (curr.indexOf(value.toLowerCase()) > -1) {
+//         return key;
+//       }
+//     }
+//   }
+//   console.timeEnd('geo_loc_name_continental_filter');
+// };
+//
+// exports.geo_loc_name_marine_validation = function (value) {
+//   if (MD_ENV_LZC_vals.indexOf(value) < 0 && (value !== '')) {
+//     throw new Error("There is no Longhurst Zone '" + value + "', please check the spelling");
+//   }
+// };
+//
+// exports.geo_loc_name_continental_validation = function (value) {
+//   if (MD_ENV_CNTRY_vals.indexOf(value) < 0 && (value !== '')) {
+//     throw new Error("There is no Country '" + value + "', please check the spelling");
+//   }
+// };
 
-exports.geo_loc_name_validation = function (value, source) {
-  if ((!checkArray(source.geo_loc_name_marine)) && (!checkArray(source.geo_loc_name_continental))) {
-    throw new Error("Either 'Country' or 'Longhurst Zone' are required"); // jshint ignore:line
-  }
-};
-
-exports.geo_loc_name_continental_filter = function (value) {
-  console.time('geo_loc_name_continental_filter');
-  for (var key in CONSTS.GAZ_SPELLING) {
-    if (CONSTS.GAZ_SPELLING.hasOwnProperty(key)) {
-      var curr = CONSTS.GAZ_SPELLING[key];
-      if (curr.indexOf(value.toLowerCase()) > -1) {
-        return key;
-      }
-    }
-  }
-  console.timeEnd('geo_loc_name_continental_filter');
-};
-
-exports.geo_loc_name_marine_validation = function (value) {
-  if (MD_ENV_LZC_vals.indexOf(value) < 0 && (value !== '')) {
-    throw new Error("There is no Longhurst Zone '" + value + "', please check the spelling");
-  }
-};
-
-exports.geo_loc_name_continental_validation = function (value) {
-  if (MD_ENV_CNTRY_vals.indexOf(value) < 0 && (value !== '')) {
-    throw new Error("There is no Country '" + value + "', please check the spelling");
-  }
-};
-
-exports.numbers_n_period = function (value) {
-  // var regex = /^[0-9.]+$/;
-  //[^0-9.] faster
-  var reg_exp = /[^0-9.]/;
-  var err_msg = ', please use only numbers and periods.';
-  check_regexp(reg_exp, value, err_msg);
-};
-
-exports.numbers_n_period_n_minus = function (value) {
-  // var regex = /^[0-9.]+$/;
-  //[^0-9.] faster
-  var reg_exp = /[^0-9.-]/;
-  var err_msg = ', please use only numbers, periods and minus.';
-  check_regexp(reg_exp, value, err_msg);
-};
-
-exports.longitude_valid = function (value) {
-  region_valid(value, -180, 180);
-};
-
-exports.latitude_valid = function (value) {
-  region_valid(value, -90, 90);
-};
-
-exports.ph_valid = function (value) {
-  region_valid(value, 0, 14);
-};
-
-exports.percent_valid = function (value) {
-  region_valid(value, 0, 100);
-};
-
-exports.positive = function (value) {
-  if (value !== '' && parseInt(value) < 0) {
-    throw new Error("'" + value + "' is not valid, %s should be greater then 0.");
-  }
-};
+// exports.numbers_n_period = function (value) {
+//   // var regex = /^[0-9.]+$/;
+//   //[^0-9.] faster
+//   var reg_exp = /[^0-9.]/;
+//   var err_msg = ', please use only numbers and periods.';
+//   check_regexp(reg_exp, value, err_msg);
+// };
+//
+// exports.numbers_n_period_n_minus = function (value) {
+//   // var regex = /^[0-9.]+$/;
+//   //[^0-9.] faster
+//   var reg_exp = /[^0-9.-]/;
+//   var err_msg = ', please use only numbers, periods and minus.';
+//   check_regexp(reg_exp, value, err_msg);
+// };
+//
+// exports.longitude_valid = function (value) {
+//   region_valid(value, -180, 180);
+// };
+//
+// exports.latitude_valid = function (value) {
+//   region_valid(value, -90, 90);
+// };
+//
+// exports.ph_valid = function (value) {
+//   region_valid(value, 0, 14);
+// };
+//
+// exports.percent_valid = function (value) {
+//   region_valid(value, 0, 100);
+// };
+//
+// exports.positive = function (value) {
+//   if (value !== '' && parseInt(value) < 0) {
+//     throw new Error("'" + value + "' is not valid, %s should be greater then 0.");
+//   }
+// };
 
 exports.get_column_name = function (row_idx, req) {
   console.time('TIME: get_column_name');
