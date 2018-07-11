2b204d9013d6ac3f14ec66f719676055d20fcfbf
annaship <tetya-loshad@yandex.ru>
2018-07-11 13:46:40 -0400

    dataset model start

--- models/dataset_model.js
+++ models/dataset_model.js
@@ -1,19 +1,25 @@
-var mysql = require('mysql2');
+class Dataset {
 
-var Dataset    = {
+  constructor(req, res, pid) {
+    this.req      = req || {};
+    this.res      = res || {};
+    this.pid      = pid;
+    this.project_obj = {};
+    this.make_project_obj();
+  }
 
-  getAllDatasets: function (callback) {
+  getAllDatasets(callback) {
 
     return connection.query("Select * from dataset", callback);
 
-  },
-  getDatasetById: function (id, callback) {
-
-    return connection.query("select * from dataset where dataset_id = ?", [dataset_id], callback);
-  },
+  }
+  // getDatasetById(id, callback) {
+  //
+  //   return connection.query("select * from dataset where dataset_id = ?", [dataset_id], callback);
+  // }
   //dataset_id, dataset, dataset_description, project_id, created_at, updated_at,
 
-  addDataset: function (Dataset, callback) {
+  addDataset(Dataset, callback) {
     return connection.query("INSERT INTO dataset VALUES(?,?,?,?,?,?) ON DUPLICATE KEY UPDATE dataset = VALUES(dataset), project_id = VALUES(project_id);", [Dataset.dataset_id,
       Dataset.dataset,
       Dataset.dataset_description,
@@ -21,15 +27,15 @@ var Dataset    = {
       Dataset.created_at,
       Dataset.updated_at,
     ], callback);
-  },
-  deleteDataset: function (id, callback) {
-    return connection.query("delete from dataset where Id=?", [id], callback);
-  },
-  // updateDataset: function (id, Dataset, callback) {
+  }
+  // deleteDataset(id, callback) {
+  //   return connection.query("delete from dataset where Id=?", [id], callback);
+  // }
+  // updateDataset(id, Dataset, callback) {
   //   return connection.query("update dataset set Title=?,Status=? where Id=?", [Dataset.Title, Dataset.Status, id], callback);
   // }
 
-};
+}
+
 module.exports = Dataset;
-//  UNIQUE KEY `dataset` (`dataset`),
-//   UNIQUE KEY `rev_dataset_name` (`rev_dataset_name`),
+// UNIQUE KEY `dataset_project` (`dataset`,`project_id`)
