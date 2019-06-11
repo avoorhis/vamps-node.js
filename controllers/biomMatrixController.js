a833208be6483f02d7488b9d974efcdbdaad7b62
annaship <tetya-loshad@yandex.ru>
2019-06-11 13:05:23 -0400

    freq works

--- controllers/biomMatrixController.js
+++ controllers/biomMatrixController.js
@@ -185,15 +185,19 @@ class BiomMatrix {
 
   calculating_norm_freq(custom_count_matrix) {//TODO: very similar to calculating_norm_freq
     console.log('calculating norm FREQ');
+    console.time("TIME: calculating_norm_freq");
+
     let tmp1 = [];
 
     for (let cc1 in custom_count_matrix.data) {//TODO: change
       let new_counts = [];
       for (let kc1 in custom_count_matrix.data[cc1]) {//TODO: change
-        new_counts.push(parseFloat( (custom_count_matrix.data[cc1][kc1] / custom_count_matrix.column_totals[kc1]).toFixed(6) ) );
+        new_counts.push(parseFloat( (custom_count_matrix.data[cc1][kc1] / custom_count_matrix.column_totals[kc1]).toFixed(6) ) || 0);
       }
       tmp1.push(new_counts);
     }
+    console.timeEnd("TIME: calculating_norm_freq");
+
     return tmp1;
   }
 
@@ -208,8 +212,6 @@ class BiomMatrix {
     for (let i = 0, arr_len = arr.length; i < arr_len; i++){
       let interim_arr = [];
       for (let j = 0, arr1_len = arr[i].length; j < arr1_len; j++) {
-        // tots[j] = (tots[j] || 0) + arr[i][j];
-        // parseInt(( custom_count_matrix.data[i][j] * max_cnt ) / custom_count_matrix.column_totals[j], 10);
         let cell = custom_count_matrix.data[i][j];
         let col_total = custom_count_matrix.column_totals[j];
         let normalized_cnt = (cell * max_cnt) / col_total;
@@ -217,32 +219,6 @@ class BiomMatrix {
       }
       new_counts.push(interim_arr);
     }
-
-    // let tmp1 = [];
-    // let max_cnt = this.biom_matrix.max_dataset_count;
-    // for (var cc in custom_count_matrix.data) {//TODO: change
-    //   let new_counts = [];
-    //   for (var col_num in custom_count_matrix.data[cc]) {//TODO: change
-    //     new_counts.push(parseInt( ( custom_count_matrix.data[cc][col_num] * max_cnt ) / custom_count_matrix.column_totals[col_num], 10) );
-    //     let r1 = custom_count_matrix.data[cc][col_num] * max_cnt;
-    //     let r2 =  custom_count_matrix.column_totals[col_num];
-    //     let r3 = r1 / r2;
-    //     let r4 = parseInt(r3);
-    //
-    //   }
-      // tmp1.push(new_counts);
-    // }
-
-    // console.log('OOO1 from calculating_norm_max JSON.stringify(tmp1) = ', JSON.stringify(tmp1));
-
-    // let arr = custom_count_matrix.data;
-    // let tots = [];
-    //
-    // for (let i = 0, arr_len = arr.length; i < arr_len; i++){
-    //   for (let j = 0, arr1_len = arr[i].length; j < arr1_len; j++){
-    //     tots[j] = (tots[j] || 0) + arr[i][j];
-    //   }
-    // }
     console.timeEnd("TIME: calculating_norm_max");
 
     return new_counts;
