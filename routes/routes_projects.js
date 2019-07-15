3de975015b602dc47cdd308f8cb16cb26a846855
annaship <tetya-loshad@yandex.ru>
2019-07-15 13:08:43 -0400

    clean up

--- routes/routes_projects.js
+++ routes/routes_projects.js
@@ -2,9 +2,9 @@ let express = require('express');
 let router = express.Router();
 let fs   = require('fs-extra');
 let nodemailer = require('nodemailer');
-let transporter = nodemailer.createTransport({});
-let zlib = require('zlib');
-let Readable = require('stream').Readable;
+// let transporter = nodemailer.createTransport({});
+// let zlib = require('zlib');
+// let Readable = require('stream').Readable;
 let helpers = require('./helpers/helpers');
 let queries = require(app_root + '/routes/queries');
 let path = require('path');
@@ -12,48 +12,31 @@ let config  = require(app_root + '/config/config');
 //let crypto = require('crypto');
 // These are all under /projects
 /* GET New User page. */
+
 router.get('/projects_index', function(req, res) {
-  let db = req.db;
-
-  //console.log(ALL_DATASETS);
-  //console.log(PROJECT_INFORMATION_BY_PNAME)
-
-  // let info = PROJECT_INFORMATION_BY_PID
-  // console.log(info);
-  //let keys = Object.keys(PROJECT_INFORMATION_BY_PNAME);
-  //keys.sort();
-  //let project_list = helpers.get_public_projects(req)
   let project_list = [];
+
   for (let pid in PROJECT_INFORMATION_BY_PID){
     if(DATASET_IDS_BY_PID[pid].length > 0){
       project_list.push(PROJECT_INFORMATION_BY_PID[pid]);
     }
   }
 
-  // let pinfo = PROJECT_INFORMATION_BY_PID[prj.pid];
-  //       //let public = pinfo.public
-  //       //if(pinfo.public == 1){
-  //           projects.push(pinfo);
-  //       //}
-  //
-  // });
-
   project_list.sort(function(a, b){
     return helpers.compareStrings_alpha(a.project, b.project);
   });
+
   console.log(project_list);
   res.render('projects/projects_index', {
-    title          : 'VAMPS Projects',
-    projects  : JSON.stringify(project_list),
-    user: req.user,hostname: req.CONFIG.hostname
+    title: 'VAMPS Projects',
+    projects: JSON.stringify(project_list),
+    user: req.user, hostname: req.CONFIG.hostname
   });
-
-
 });
 
 //TODO: JSHint: This function's cyclomatic complexity is too high. (16) (W074)
 router.get('/:id', helpers.isLoggedIn, function(req, res) {
-  let db = req.db;
+  // let db = req.db;
   let dsinfo = [];
   let mdata = {};
   let dscounts = {};
@@ -73,69 +56,56 @@ router.get('/:id', helpers.isLoggedIn, function(req, res) {
 //  MD_ENV_LZC     longhurst zone code
 
     let ProjectProfileFinishRequest = function (req, arg_obj) {
-          console.log('arg_obj.project_file_names = ');
-          console.log(arg_obj.project_file_names);
-//             console.log('info')
-//             console.log(info)
         res.render('projects/profile', {
-            title  : 'VAMPS Project',
+            title: 'VAMPS Project',
             info: JSON.stringify(arg_obj.info),
             dsinfo: arg_obj.dsinfo,
             dscounts: JSON.stringify(arg_obj.dscounts),
             pid: req.params.id,
             mdata: JSON.stringify(arg_obj.mdata),
             pcount: arg_obj.project_count,
-            portal :    JSON.stringify(arg_obj.member_of_portal),
-            publish_info : JSON.stringify(arg_obj.publish_data),
+            portal: JSON.stringify(arg_obj.member_of_portal),
+            publish_info: JSON.stringify(arg_obj.publish_data),
             pnotes: arg_obj.pnotes,
             dco_file: arg_obj.best_file,
             finfo: JSON.stringify(arg_obj.project_file_names),
-            user   : req.user,
+            user: req.user,
             hostname: req.CONFIG.hostname
        });
-
-
       };
+    
     if(req.params.id in PROJECT_INFORMATION_BY_PID){
       let info = PROJECT_INFORMATION_BY_PID[req.params.id];
       let project_count = ALL_PCOUNTS_BY_PID[req.params.id];
-      console.log(info);
-      // console.log("ALL_PCOUNTS_BY_PID 2: ");
-      // console.log(ALL_PCOUNTS_BY_PID);
 
-      let dataset_counts = {};
       for (let n0 in ALL_DATASETS.projects){
-        if(ALL_DATASETS.projects[n0].pid == req.params.id){
+        if(ALL_DATASETS.projects[n0].pid === req.params.id){
           dsinfo = ALL_DATASETS.projects[n0].datasets;
         }
       }
+
       for (let n in dsinfo){
         let did = dsinfo[n].did;
         dscounts[did] = ALL_DCOUNTS_BY_DID[did];
         mdata[dsinfo[n].dname] = {};
 
-
-          for (let name in AllMetadata[did]){
-            let data;
-            if(name === 'primer_suite_id'){
-              data = helpers.required_metadata_names_from_ids(AllMetadata[did], 'primer_ids');
-              mdata[dsinfo[n].dname][data.name] = data.value;
-            }
-            data = helpers.required_metadata_names_from_ids(AllMetadata[did], name);
+        for (let name in AllMetadata[did]){
+          let data;
+          if(name === 'primer_suite_id'){
+            data = helpers.required_metadata_names_from_ids(AllMetadata[did], 'primer_ids');
             mdata[dsinfo[n].dname][data.name] = data.value;
-
           }
-
-
-
+          data = helpers.required_metadata_names_from_ids(AllMetadata[did], name);
+          mdata[dsinfo[n].dname][data.name] = data.value;
+        }
       }
 
       let project_parts = info.project.split('_');
-      let project_prefix = info.project;
-
-      if(project_parts.length >= 2 ){
-        project_prefix = project_parts[0]+'_'+project_parts[1];
-      }
+      // let project_prefix = info.project;
+      //
+      // if(project_parts.length >= 2 ){
+      //   project_prefix = project_parts[0]+'_'+project_parts[1];
+      // }
       let member_of_portal = {};
       for (let p in req.CONSTS.PORTALS){
         //console.log(p +' -- '+project_parts[0])
@@ -154,45 +124,41 @@ router.get('/:id', helpers.isLoggedIn, function(req, res) {
 
       let info_file = '';
       let publish_data = {};
-      let best_file_path = '';
+      // let best_file_path = '';
       let best_file = '';
       if(info.project.substring(0,3) === 'DCO'){
-
-          try{
+          try {
               info_file = path.join(req.CONFIG.PATH_TO_STATIC_DOWNLOADS,'DCO_INFO.json');
               publish_data = JSON.parse(fs.readFileSync(info_file, 'utf8'));
-          }catch(e){
+          } catch(e){
               publish_data = {};
           }
-          let dco_all_metadata_file = '';
+          // let dco_all_metadata_file = '';
           let best_date = Date.parse('2000-01-01');
 
           fs.readdirSync(req.CONFIG.PATH_TO_DCO_DOWNLOADS).forEach(file => {
-              if(file.substring(0,16) == 'dco_all_metadata'){
+              if (file.substring(0,16) === 'dco_all_metadata'){
                   //console.log('file '+file)
-                  let file_date = file.substring(17,file.length - 7);
+                  let file_date = file.substring(17, file.length - 7);
                   //console.log('file_date: '+file_date)
                   let d = Date.parse(file_date);
-                  if(d > best_date){
-                      best_file = file;
+                  if (d > best_date){
+                    best_file = file;
                   }
               }
           });
           //best_file =  'dco_all_metadata_'+yyyy+'-'+mm+'-'+dd+'.tsv.gz'
           //console.log('best_file '+best_file)
-          best_file_path = path.join(req.CONFIG.PATH_TO_STATIC_DOWNLOADS,best_file)
-
-
+          // best_file_path = path.join(req.CONFIG.PATH_TO_STATIC_DOWNLOADS, best_file);
       }
-      if(info.project.substring(0,3) == 'CMP'){
 
-          try{
-              info_file = path.join(req.CONFIG.PATH_TO_STATIC_DOWNLOADS,'CMP_INFO.json');
-              publish_data = JSON.parse(fs.readFileSync(info_file, 'utf8'));
-          }catch(e){
-              publish_data = {};
-          }
-
+      if(info.project.substring(0,3) === 'CMP'){
+        try {
+            info_file = path.join(req.CONFIG.PATH_TO_STATIC_DOWNLOADS,'CMP_INFO.json');
+            publish_data = JSON.parse(fs.readFileSync(info_file, 'utf8'));
+        } catch(e) {
+            publish_data = {};
+        }
       }
       let user_metadata_csv_files = get_csv_files(req);
       let project_file_names = filter_metadata_csv_files_by_project(user_metadata_csv_files, info.project, req.user.username);
@@ -283,7 +249,8 @@ function filter_metadata_csv_files_by_project(file_names, project_name, username
 
   let file_name_template = "metadata-project_" + project_name + "_" + username;
 
-  let project_metadata_file_names = file_names.filter(function(obj) {return (obj.filename.startsWith(file_name_template))});
+  let project_metadata_file_names = file_names.filter(function(obj)
+  {return (obj.filename.startsWith(file_name_template))});
   console.timeEnd("TIME: filter_metadata_csv_files_by_project");
 
   return project_metadata_file_names;
