985d5302172880d4f84aebd8b993db46592cfe63
annaship <tetya-loshad@yandex.ru>
2019-09-18 14:29:40 -0400

    Update biomMatrixController.js
    add TaxonomyGeneric to factory

--- controllers/biomMatrixController.js
+++ controllers/biomMatrixController.js
@@ -455,12 +455,14 @@ class TaxonomyFactory {
     this.units       = visual_post_items.unit_choice;
     this.taxa_counts_module = taxa_counts;
     this.chosen_dids = chosen_dids;
-    this.chosen_taxonomy = this.choose_simple_or_custom_lookup_module(visual_post_items, this.taxa_counts_module, this.chosen_dids);
+    this.chosen_taxonomy = this.choose_taxonomy_lookup_module(visual_post_items, this.taxa_counts_module, this.chosen_dids);
   }
 
-  choose_simple_or_custom_lookup_module(visual_post_items, taxa_counts_module, chosen_dids) {
+  choose_taxonomy_lookup_module(visual_post_items, taxa_counts_module, chosen_dids) {
     let unit_choice_simple = (this.units.substr(this.units.length - 6) === 'simple');
     let unit_choice_custom = (this.units === 'tax_' + C.default_taxonomy.name + '_custom');
+    let unit_choice_generic_simple = (this.units === 'tax_generic_simple');
+
     //TODO: args object send to whatever module is chosen
     if (unit_choice_simple) {
       return new module.exports.TaxonomySimple(visual_post_items, taxa_counts_module, chosen_dids);
@@ -468,11 +470,13 @@ class TaxonomyFactory {
     else if (unit_choice_custom) {
       return new module.exports.TaxonomyCustom(visual_post_items, taxa_counts_module, chosen_dids);
     }
+    else if (unit_choice_generic_simple) {
+      return new module.exports.TaxonomyGeneric(visual_post_items, taxa_counts_module, chosen_dids);
+    }
     else {
-      console.log("ERROR: Can't choose simple or custom taxonomy");
+      console.log("ERROR: Can't choose the correct taxonomy");
     }
   }
-
 }
 
 class Taxonomy {
