149052279ceaf8454282cde4680d32687b229606
annaship <tetya-loshad@yandex.ru>
2019-09-21 10:50:31 -0400

    Update visualizationController.js
    strategy + factory

--- controllers/visualizationController.js
+++ controllers/visualizationController.js
@@ -1,61 +1,63 @@
-class viewSelection {
-  setGetData(get_data) {
-    this.get_data = get_data;
-  }
+const COMMON = require(app_root + '/routes/visuals/routes_common');
 
-  getGetData() {
-    return this.get_data.get_get_data();
-  }
-
-}
-// https://itnext.io/how-we-avoided-if-else-and-wrote-extendable-code-with-strategy-pattern-256e34b90caf
 class viewSelectionGetData {
-
-  constructor(func) {
-    // it’s a function reference and not an actual call.
-    this.get_data = func;
+  // const default_data = new viewSelectionGetData(() => 'this is the get_data for gold');
+// const from_upload_configuration_file = new viewSelectionGetData(() => 'this is the get_data for gold');
+// const from_directory_configuration_file = new viewSelectionGetData(() => 'this is the get_data for goldAndInternational');
+// const resorted = new viewSelectionGetData(() => 'this is the get_data for goldAndDataHeavy');
+// const update_data = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const cancel_resort = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const restore_image = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const api = new viewSelectionGetData(() => 'this is the get_data for silver');
+//
+  constructor(req) {
+    this.image_to_open = {};
+    this.dataset_ids = [];
+    this.new_dataset_ids = [];
+    this.visual_post_items = {};
+    this.req = req;
+    this.domains = ['Archaea', 'Bacteria', 'Eukarya', 'Organelle', 'Unknown'];
   }
 
-  get_get_data() {
-    return this.get_data();
+  default_data() {
+    console.log('DEFAULT req.body');
+    this.visual_post_items = COMMON.save_post_items(this.req);
+    this.dataset_ids = this.req.session.chosen_id_order;
+    this.req.session.no_of_datasets = this.dataset_ids.length;
+    this.req.session.metadata = this.visual_post_items.metadata;
+    this.req.session.normalization = "none";
+    this.req.session.selected_distance = "morisita_horn";
+    this.req.session.tax_depth = this.req.body.tax_depth;
+    this.req.session.domains = this.req.body.domains || this.domains;
+    this.req.session.include_nas = "yes";
+    this.req.session.min_range = '0';
+    this.req.session.max_range = '100';
+    this.req.session.unit_choice = this.req.body.unit_choice;
+    this.req.session.custom_taxa = this.visual_post_items.custom_taxa;
+  }
+
+  from_upload_configuration_file(){
   }
 }
 
-const default_data = new viewSelectionGetData(() => 'this is the get_data for gold');
-const from_upload_configuration_file = new viewSelectionGetData(() => 'this is the get_data for gold');
-const from_directory_configuration_file = new viewSelectionGetData(() => 'this is the get_data for goldAndInternational');
-const resorted = new viewSelectionGetData(() => 'this is the get_data for goldAndDataHeavy');
-const update_data = new viewSelectionGetData(() => 'this is the get_data for silver');
-const cancel_resort = new viewSelectionGetData(() => 'this is the get_data for silver');
-const restore_image = new viewSelectionGetData(() => 'this is the get_data for silver');
-const api = new viewSelectionGetData(() => 'this is the get_data for silver');
+module.exports = function () {
 
-module.exports = {
-  viewSelection,
-  default_data, from_upload_configuration_file, from_directory_configuration_file, resorted, update_data, cancel_resort, restore_image, api,
-};
+  var serviceDescriptions: [
+    { name: 'a', service:  StrategyA},
+    {name: 'b', service:  StrategyB}
+  ];
 
-//   function StrategyA(){
-//
-//   }
-//   StrategyA.prototype = {
-//     execute: function() {
-//       //custom behavior here
-//     }
-//   }
-//
-//   function StrategyB(){
-//   }
-//   StrategyB.prototype = {
-//     execute: function() {
-//       //custom behavior here
-//     }
-//   }
-// }
-//
+  var getStrategy: function (name) {
+    //asuming you have underscore, otherwise, just iterate the array to look for the proper service
+    return _.find(this.serviceDescriptions, {name: name}).service;
+  };
+
+}
+
+
 // class viewSelectionFactory {
-//   let  StrategyA = require('./strategyA.js'),
-//   StrategyB = require('./strategyB.js');
+//   // let  StrategyA = require('./strategyA.js'),
+//   // StrategyB = require('./strategyB.js');
 //
 //   module.exports = function () {
 //
@@ -71,8 +73,47 @@ module.exports = {
 //
 //   }
 // }
-//
+
+
 // module.exports = {
 //   StrategyA: StrategyA,
 //   StrategyB: StrategyB
 // }
+
+// class viewSelection {
+//   setGetData(get_data) {
+//     this.get_data = get_data;
+//   }
+//
+//   getGetData() {
+//     return this.get_data.get_get_data();
+//   }
+//
+// }
+// // https://itnext.io/how-we-avoided-if-else-and-wrote-extendable-code-with-strategy-pattern-256e34b90caf
+// class viewSelectionGetData {
+//
+//   constructor(func) {
+//     // it’s a function reference and not an actual call.
+//     this.get_data = func;
+//   }
+//
+//   get_get_data() {
+//     return this.get_data();
+//   }
+// }
+//
+// const default_data = new viewSelectionGetData(() => 'this is the get_data for gold');
+// const from_upload_configuration_file = new viewSelectionGetData(() => 'this is the get_data for gold');
+// const from_directory_configuration_file = new viewSelectionGetData(() => 'this is the get_data for goldAndInternational');
+// const resorted = new viewSelectionGetData(() => 'this is the get_data for goldAndDataHeavy');
+// const update_data = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const cancel_resort = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const restore_image = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const api = new viewSelectionGetData(() => 'this is the get_data for silver');
+//
+// module.exports = {
+//   // viewSelection,
+//   viewSelectionGetData
+// };
+
