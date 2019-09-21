011b89d0064cd43c0fbe60870b45d6bf0064718d
annaship <tetya-loshad@yandex.ru>
2019-09-21 12:13:06 -0400

    Update visualizationController.js
    add all ifs to viewSelectionGetData

--- controllers/visualizationController.js
+++ controllers/visualizationController.js
@@ -1,22 +1,38 @@
 const COMMON = require(app_root + '/routes/visuals/routes_common');
+const helpers = require(app_root + '/routes/helpers/helpers');
+const fs = require('fs-extra');
 
 class viewSelectionGetData {
   // const default_data = new viewSelectionGetData(() => 'this is the get_data for gold');
 // const from_upload_configuration_file = new viewSelectionGetData(() => 'this is the get_data for gold');
 // const from_directory_configuration_file = new viewSelectionGetData(() => 'this is the get_data for goldAndInternational');
-// const resorted = new viewSelectionGetData(() => 'this is the get_data for goldAndDataHeavy');
-// const update_data = new viewSelectionGetData(() => 'this is the get_data for silver');
-// const cancel_resort = new viewSelectionGetData(() => 'this is the get_data for silver');
-// const restore_image = new viewSelectionGetData(() => 'this is the get_data for silver');
-// const api = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const from_resorted = new viewSelectionGetData(() => 'this is the get_data for goldAndDataHeavy');
+// const from_update_data = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const from_cancel_resort = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const from_restore_image = new viewSelectionGetData(() => 'this is the get_data for silver');
+// const from_api = new viewSelectionGetData(() => 'this is the get_data for silver');
 //
   constructor(req) {
     // this.image_to_open = {};
     this.dataset_ids = [];
-    // this.new_dataset_ids = [];
+    this.new_dataset_ids = [];
     this.visual_post_items = {};
     this.req = req;
     this.domains = ['Archaea', 'Bacteria', 'Eukarya', 'Organelle', 'Unknown'];
+  }
+
+  get_visual_post_items_common() {
+    this.visual_post_items.custom_taxa = this.req.session.custom_taxa;
+    this.visual_post_items.domains = this.req.session.domains;
+    this.visual_post_items.include_nas = this.req.session.include_nas;
+    this.visual_post_items.max_range = this.req.session.max_range;
+    this.visual_post_items.metadata = this.req.session.metadata;
+    this.visual_post_items.min_range = this.req.session.min_range;
+    this.visual_post_items.no_of_datasets = this.dataset_ids.length;
+    this.visual_post_items.normalization = this.req.session.normalization;
+    this.visual_post_items.selected_distance = this.req.session.selected_distance;
+    this.visual_post_items.tax_depth = this.req.session.tax_depth;
+    this.visual_post_items.unit_choice = this.req.session.unit_choice;
   }
 
   default_data() {
@@ -36,8 +52,138 @@ class viewSelectionGetData {
     this.req.session.custom_taxa = this.visual_post_items.custom_taxa;
   }
 
-  from_upload_configuration_file(){
+  from_api() {
+    console.log('From: API-API-API');
+    this.visual_post_items = COMMON.default_post_items();
+    // Change defaults:
+    this.req.session.normalization = this.visual_post_items.normalization = this.req.body.normalization          || "none";
+    this.req.session.selected_distance = this.visual_post_items.selected_distance = this.req.body.selected_distance  || "morisita_horn";
+    this.req.session.tax_depth   = this.visual_post_items.tax_depth = this.req.body.tax_depth                  || "phylum";
+    this.req.session.domains     = this.visual_post_items.domains = this.req.body.domains                      || ["Archaea", "Bacteria", "Eukarya", "Organelle", "Unknown"];
+    this.req.session.include_nas = this.visual_post_items.include_nas = this.req.body.include_nas              || "yes";
+    this.req.session.min_range   = this.visual_post_items.min_range = this.req.body.min_range                  || '0';
+    this.req.session.max_range   = this.visual_post_items.max_range = this.req.body.max_range                  || '100';
+
+    if ((this.req.body).hasOwnProperty('ds_order') && this.req.body.ds_order.length !== 0){
+      console.log('Found api dids ',this.req.body.ds_order);
+      try {
+        this.dataset_ids = JSON.parse(this.req.body.ds_order);
+      }
+      catch(e){
+        this.dataset_ids = this.req.body.ds_order;
+      }
+      this.new_dataset_ids = helpers.screen_dids_for_permissions(this.req, this.dataset_ids);
+      this.dataset_ids = this.new_dataset_ids;
+      this.req.session.chosen_id_order = this.visual_post_items.ds_order = this.dataset_ids;
+    }
+    else if ( (this.req.body).hasOwnProperty('project') && PROJECT_INFORMATION_BY_PNAME.hasOwnProperty(this.req.body.project) ){
+      console.log('Found api project ',this.req.body.project);
+      let pid = PROJECT_INFORMATION_BY_PNAME[this.req.body.project].pid;
+      this.new_dataset_ids = helpers.screen_dids_for_permissions(this.req, this.dataset_ids_BY_PID[pid.toString()]);
+      this.visual_post_items.ds_order = this.new_dataset_ids;
+      console.log(PROJECT_INFORMATION_BY_PNAME[this.req.body.project]);
+      console.log(this.visual_post_items.ds_order);
+      this.req.session.chosen_id_order = this.dataset_ids = this.visual_post_items.ds_order;
+      //console.log('dids',this.dataset_ids)
+    } else {
+      console.log('API ALERT - no dids or project');
+      return;
+    }
+
+    this.visual_post_items.update_data = this.req.body.update_data              || '1';   // fires changes
+
+    this.req.session.no_of_datasets  = this.visual_post_items.no_of_datasets = this.dataset_ids.length;
+
+    // for API select ALL metadata with these datasets
+    let md = {}; // hash lookup unique
+    for (let n in this.dataset_ids){
+      let did = this.dataset_ids[n];
+      for (let item in AllMetadata[did]){
+        md[item] =1;
+      }
+    }
+    this.req.session.metadata  = this.visual_post_items.metadata = Object.keys(md);
+
   }
+
+  from_restore_image() {
+    console.log('in view_selection RESTORE IMAGE');
+  }
+
+  from_cancel_resort() {
+    console.log('resorted canceled');
+    this.req.flash('success','Canceled Resort.');
+    this.dataset_ids = JSON.parse(this.req.body.ds_order);
+  }
+
+  from_update_data() {  // from 'Update' button on view_selection.html
+    console.log('Update Data');
+    // populate this.req.session and this.visual_post_items from this.req.body(post)
+    this.dataset_ids = this.req.session.chosen_id_order;
+    this.visual_post_items = COMMON.save_post_items(this.req);
+    for (let item in this.visual_post_items){
+      this.req.session[item] = this.visual_post_items[item];
+    }
+  }
+
+  from_resorted() {
+    console.log('resorted === 1');
+    // populate this.visual_post_items from this.req.session (except new ds_order)
+    this.req.flash('success','The dataset order has been updated.');
+    this.dataset_ids = this.req.body.ds_order;
+    this.req.session.chosen_id_order = this.dataset_ids;
+    this.get_visual_post_items_common();
+  }
+
+  from_directory_configuration_file() {
+    // ALL Config files now loaded through GET (see router.get('/view_selection/:filename/:from_configuration_file')
+    console.log('from_directory_configuration_file-POST');
+    // populate this.visual_post_items from ?????
+    let config_file_path = path.join(this.req.CONFIG.USER_FILES_BASE, this.req.user.username, this.req.body.filename);
+    let upld_obj = JSON.parse(fs.readFileSync(config_file_path, 'utf8'));
+    //console.log(upld_obj)
+    let config_file_data = create_clean_config(this.req, upld_obj); // put into this.req.session
+    if (Object.keys(config_file_data).length === 0){
+      //error
+      res.redirect('saved_elements');
+      return;
+    }
+    for (let item in config_file_data) {// TODO: copy the object faster
+      this.req.session[item] = config_file_data[item];
+    }
+    this.dataset_ids = this.req.session.chosen_id_order;
+    this.get_visual_post_items_common();
+    this.visual_post_items.update_data = 1;
+  }
+  // not in resorted: this.visual_post_items.update_data = 1;
+
+  from_upload_configuration_file() {
+    // UPLOAD Config file
+    console.log('from_upload_configuration_file-POST');
+    // populate this.visual_post_items from ????
+    // For this we need the upload.single('upload_files', 12) in the post definition
+    let upload_file = this.req.file.path;
+    let upld_obj = JSON.parse(fs.readFileSync(upload_file, 'utf8'));//,function(err, data){
+    let config_file_data = create_clean_config(this.req, upld_obj); // put into this.req.session
+    if (Object.keys(config_file_data).length === 0){
+      //error
+      res.redirect('saved_elements');
+      return;
+    }
+    for (let item in config_file_data) {//TODO: already done above - DRY
+      this.req.session[item] = config_file_data[item];
+    }
+    this.dataset_ids = this.req.session.chosen_id_order;
+    this.get_visual_post_items_common();
+    this.visual_post_items.update_data = 1;
+    // same as from_directory_configuration_file this.visual_post_items...
+
+    //let image_to_open = load_configuration_file(this.req, res, config_file_data)
+    // FIXME need datasets from config file
+
+
+  }
+  
 }
 // module.exports = function () {
 //
@@ -66,82 +212,37 @@ class viewSelectionFactory {
     // module.exports.viewSelectionGetData
   select_get_data_strategy() {
     let req_body = this.req.body;
+
     if (req_body.api === '1') {
-      console.log("api");
+      this.get_data.from_api();
     }
-    // else if (req_body.cancel_resort === '1') {}
-    // else if (req_body.from_directory_configuration_file === '1') {}
-    // else if (req_body.from_upload_configuration_file === '1') {}
-    // else if (req_body.resorted === '1') {}
-    // else if (req_body.restore_image === '1') {}
-    // else if (req_body.update_data === '1') {}  // from 'Update' button on view_selection.html
+    else if (req_body.cancel_resort === '1') {
+      this.get_data.from_cancel_resort();
+    }
+    else if (req_body.from_directory_configuration_file === '1') {
+      this.get_data.from_directory_configuration_file();
+    }
+    else if (req_body.from_upload_configuration_file === '1') {
+      this.get_data.from_upload_configuration_file();
+    }
+    else if (req_body.resorted === '1') {
+      this.get_data.from_resorted();
+    }
+    else if (req_body.restore_image === '1') {
+      this.get_data.from_restore_image();
+    }
+    else if (req_body.update_data === '1') {
+      this.get_data.from_update_data();
+    }  // from 'Update' button on view_selection.html
     else {
       this.get_data.default_data();
     }
 
     this.dataset_ids = this.get_data.dataset_ids;
     this.visual_post_items = this.get_data.visual_post_items;
-
   }
-
-
 }
-//   // let  StrategyA = require('./strategyA.js'),
-//   // StrategyB = require('./strategyB.js');
-//
-//   module.exports = function () {
-//
-//     var serviceDescriptions: [
-//       { name: 'a', service:  StrategyA },
-//       { name: 'b', service:  StrategyB }
-//     ];
-//
-//     var getStrategy: function (name) {
-//       //asuming you have underscore, otherwise, just iterate the array to look for the proper service
-//       return _.find(this.serviceDescriptions, {name: name}).service;
-//     };
-//
-//   }
-// }
 
-
-// module.exports = {
-//   StrategyA: StrategyA,
-//   StrategyB: StrategyB
-// }
-
-// class viewSelection {
-//   setGetData(get_data) {
-//     this.get_data = get_data;
-//   }
-//
-//   getGetData() {
-//     return this.get_data.get_get_data();
-//   }
-//
-// }
-// // https://itnext.io/how-we-avoided-if-else-and-wrote-extendable-code-with-strategy-pattern-256e34b90caf
-// class viewSelectionGetData {
-//
-//   constructor(func) {
-//     // it’s a function reference and not an actual call.
-//     this.get_data = func;
-//   }
-//
-//   get_get_data() {
-//     return this.get_data();
-//   }
-// }
-//
-// const default_data = new viewSelectionGetData(() => 'this is the get_data for gold');
-// const from_upload_configuration_file = new viewSelectionGetData(() => 'this is the get_data for gold');
-// const from_directory_configuration_file = new viewSelectionGetData(() => 'this is the get_data for goldAndInternational');
-// const resorted = new viewSelectionGetData(() => 'this is the get_data for goldAndDataHeavy');
-// const update_data = new viewSelectionGetData(() => 'this is the get_data for silver');
-// const cancel_resort = new viewSelectionGetData(() => 'this is the get_data for silver');
-// const restore_image = new viewSelectionGetData(() => 'this is the get_data for silver');
-// const api = new viewSelectionGetData(() => 'this is the get_data for silver');
-//
 module.exports = {
   viewSelectionGetData,
   viewSelectionFactory
