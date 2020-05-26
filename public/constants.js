813401329a82a7e7ff6696a7efab3e6a0e3f88aa
Andrew Voorhis <avoorhis@mbl.edu>
2020-05-26 17:50:28 -0400

    one more plastisphere

--- public/constants.js
+++ public/constants.js
@@ -446,10 +446,9 @@ constants.PORTALS = {
     projects: [
     'LAZ_SEA_Bv6', 'LAZ_SEA_Ev9', 'LAZ_SEA_Bv6v4', 'LAZ_DET_Bv3v4',
     'AD_2019_Bv3v4','AM_2014_Bv4','AM_2016_Bv4','AST_2019_Bv4','CD_2018a_Bv3v5','ES_2017_Bv3','GEC_2019_Bv4v5',
-    'IVK_2019_Bv3v4','KK_2019_Bv4','KPNLF_2019_Bv3v4',
+    'IVK_2018_Bv3v4','IVK_2019_Bv3v4','KK_2019_Bv4','KPNLF_2019_Bv3v4',
     'LCW_2018_Bv4','MAA_2018_Bv4','MO_2018_Bv3v4','MP_2019_Bv3v4','MR_2019_Bv3','SO_2018_Bv4',
     'SW_2018_Bv4','TH_2014_Bv4','XX_2019_Bv3v4'
-    
     ],
    
 
